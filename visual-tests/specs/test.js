a6a341eec5ebe69095c12657079c79d20bc03262
Jonas Nordstrand <jonas.nordstrand@sparebank1.no>
2016-03-03 16:10:39 +0100

    + visual regression tests

---
+++ visual-tests/specs/test.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var gemini = require('gemini');
+
+
+
+var HTML_PATTERN = /\.html$/
+
+fs.readdirSync('./examples')
+  .filter(function(f) { return f.match(HTML_PATTERN) &&  f !== 'index.html' })
+  .forEach(function(f) {
+    gemini.suite(f.replace(HTML_PATTERN, ""), function(suite) {
+      suite.setUrl('/' + f)
+           .setCaptureElements('body')
+           .before(function(actions, find) {
+                actions.executeJS(function(window) {
+                  Array.prototype.forEach.call(document.querySelectorAll(".loading-spinner,  .action-button__label-spinner, .primary-button__label-spinner, .secondary-button__label-spinner"), function(el) {
+                     // Removes spinning/animation elements that impair test stability
+                     el && el.parentNode.removeChild(el);
+                  });
+                })
+                .wait(500);  // for checkbox animations to reach steady state
+
+           })
+           .capture('plain');
+    })
+  });
+
