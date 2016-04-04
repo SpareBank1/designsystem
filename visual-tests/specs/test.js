4f6570f79c9fe516f24dd66b8b5cf56a60588bf1
Jonas Nordstrand <jonas.nordstrand@sparebank1.no>
2016-04-04 15:21:30 +0200

    Implement automatic publicering and visual regression test

---
+++ visual-tests/specs/test.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var gemini = require('gemini');
+
+
+
+var HTML_PATTERN = /\.html$/
+
+fs.readdirSync('./examples')
+  .filter(function(f) { return f.match(HTML_PATTERN) })
+  .forEach(function(f) {
+    gemini.suite(f.replace(HTML_PATTERN, ""), function(suite) {
+      suite.setUrl('/' + f)
+           .setCaptureElements('body')
+           .before(function(actions, find) {
+                actions.wait(1000);  // for card animations to reach steady state
+
+           })
+           .capture('plain');
+    })
+  });
+
