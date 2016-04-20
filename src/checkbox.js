92959f8ee9c2e50c2d3c40552e9939e958b9d0c0
Erik Wendel <erik.wendel@sparebank1.no>
2016-04-20 11:21:16 +0200

    1.0.0 of ffe-checkbox-react

---
+++ src/checkbox.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react';
+import hash from 'nfe-hash';
+
+export default function CheckBox({ name, label, onChange, checked, children }) {
+    const id = `checkbox-${hash(name)}`;
+
+    return  <span>
+                <input 
+                    onChange={() => onChange(!checked) }
+                    className="ffe-hidden-checkbox"
+                    type="checkbox"
+                    checked={checked}
+                    name={name}
+                    id={id}
+                />
+                <label className="ffe-check-box ffe-check-box--inline" htmlFor={id}>
+                    {label || children}
+                </label>
+            </span>;
+}
+
+CheckBox.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    checked: PropTypes.bool
+};
