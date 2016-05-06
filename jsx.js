1be2281294025b63f997c5d3c0d36352a6dc1714
Ørjan Helstrøm <orjan.helstrom@sparebank1.no>
2016-05-06 13:01:04 +0200

    Added ignoreStateless for 'react/no-multi-comp': [2, {'ignoreStateless': true}], - The original rule was too restrictive. Could be set to false on a per project basis.

--- jsx.js
+++ jsx.js
@@ -50,7 +50,7 @@ module.exports = {
 
         // Prevent multiple component definition per file
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
-        'react/no-multi-comp': 2,
+        'react/no-multi-comp': [2, {'ignoreStateless': true}],
 
         // Prevent usage of unknown DOM property
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
