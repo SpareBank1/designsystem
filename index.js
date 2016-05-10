557df5cf95640fd7e02683d93d99911d3271b59c
Sigurd Fosseng <sigurd.fosseng@sparebank1.no>
2016-05-10 15:30:32 +0200

    use eslint-config-ffe-base

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 module.exports = {
     'extends': [
-        './errors',
-        './regex',
-        './bestPractices',
-        './es6.js'
+        'eslint-config-ffe-base',
+        './rules/jsx'
     ].map(require.resolve),
-    'env': {
-        'es6': true,
-        'browser': true,
-        'node': true
-    },
-    'parserOptions': {
-        'ecmaVersion': 6,
-        'ecmaFeatures': {
-            'arrowFunctions': true,
-            'experimentalObjectRestSpread': true
-        },
-        'sourceType': 'module'
-    },
     'plugins': [
-        'import'
-    ]
+        'react'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+    },
+    'ecmaFeatures': {
+        'jsx': true
+    },
+    'rules': {}
 };
-
