bc633eec69bd182ef00b2e12b43a06ae2ed1dbc9
Erik Wendel <erik.wendel@sparebank1.no>
2016-05-31 09:24:41 +0200

    Docs ready for autopublishing

--- docs/example-component.js
+++ docs/example-component.js
@@ -1,8 +1,24 @@
-import { SuccessMessage } from '../lib/index';
+import { SuccessMessage, ErrorMessage, TipsMessage, InfoMessage } from '../lib/index';
 import React from 'react';
 
-export const Example = (
+export const SuccessExample = (
     <SuccessMessage title="Betalingen er gjennomført" content="Her står det en lengre melding, kanskje med HTML." />
 );
 
-export default Example;
+export const ErrorExample = (
+    <ErrorMessage title="Noe gikk galt" content="Noe gikk galt. Vennligst prøv igjen senere!" />
+);
+
+export const TipsExample = (
+    <TipsMessage title="Et tips til deg" content="Følger du dette tipset i denne boksen kan du spare mange penger." />
+);
+
+export const InfoExample = (
+    <InfoMessage title="Du har ingen kort" content="Denne tjenesten er kun tilgjengelig for deg med et debitkort eller kredittkort fra SpareBank1" />
+);
+
+export const MainExample = (
+    <SuccessMessage title="Betalingen er gjennomført" content="Her står det en lengre melding, kanskje med HTML." />
+);
+
+export default MainExample;
