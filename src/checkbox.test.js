2cdddc955dce0fea3a186214c84ed07c6782d348
Erik Wendel <erik.wendel@sparebank1.no>
2016-06-01 10:50:07 +0200

    v1.2.0 - with automated docs

--- src/checkbox.test.js
+++ src/checkbox.test.js
@@ -1,20 +1,18 @@
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import { assert } from 'chai';
-import React from 'react';
-import Checkbox from './checkbox';
-
+import CreateCheckbox from '../docs/example-component';
 
 describe('<Checkbox />', () => {
    
     it('should render a input', () => {
-        const wrapper = shallow(<Checkbox />);
+        const wrapper = shallow(CreateCheckbox({}));
         assert.equal(wrapper.find('input').length, 1);
     });
 
     it('should call onChange when clicked', () => {
         const spy = sinon.spy();
-        const wrapper = shallow(<Checkbox onChange={spy} />);
+        const wrapper = shallow(CreateCheckbox({ onChange: spy }));
 
         wrapper.find('input').simulate('change');
         assert.equal(spy.calledOnce, true);
@@ -23,13 +21,13 @@ describe('<Checkbox />', () => {
     it('should call onChange with the correct parameter', () => {
         let spy = sinon.spy();
         
-        let wrapper = shallow(<Checkbox onChange={spy} />);
+        let wrapper = shallow(CreateCheckbox({ onChange: spy }));
 
         wrapper.find('input').simulate('change');
         assert.equal(spy.args[0][0], true);
 
         spy = sinon.spy();
-        wrapper = shallow(<Checkbox checked={true} onChange={spy} />);
+        wrapper = shallow(CreateCheckbox({ onChange: spy, checked: true }));
         
         wrapper.find('input').simulate('change');
         assert.equal(spy.args[0][0], false);
@@ -37,15 +35,15 @@ describe('<Checkbox />', () => {
     });
 
     it('should render a default value if passed', () => {
-        let wrapper = shallow(<Checkbox />);
+        let wrapper = shallow(CreateCheckbox({}));
         assert.equal(wrapper.find('input').prop('checked'), undefined);
 
-        wrapper = shallow(<Checkbox checked={true} />);
+        wrapper = shallow(CreateCheckbox({ checked: true }));
         assert.equal(wrapper.find('input').prop('checked'), true);
     });
 
     it('should apply the same id to <label> and <input>', () => {
-        const wrapper = shallow(<Checkbox name="Some text goes here" />);
+        const wrapper = shallow(CreateCheckbox({ name: 'Some text goes here' }));
 
         assert.equal(
             wrapper.find('label').prop('htmlFor'),
