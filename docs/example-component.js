bfac1ba5b1c6173a3039362417599b3646ef96fc
Erik Wendel <erik.wendel@sparebank1.no>
2016-06-01 10:50:07 +0200

    v1.2.0 - with automated docs

---
+++ docs/example-component.js
@@ -0,0 +1,18 @@
+import Checkbox from '../lib/checkbox';
+import React from 'react';
+
+export const MainExample = (
+    <Checkbox
+        onChange={ () => {} }
+        checked={false}
+        name="Yellow"
+        label="Yellow"
+    />
+);
+
+export default props => <Checkbox
+                            onChange={props.onChange}
+                            checked={props.checked}
+                            name={props.name}
+                            label={props.label}
+                        />;
