992aab24c3c77a982d9120caa3770344306da396
Terje Røstum <terje.rostum@sparebank1.no>
2016-06-17 14:59:03 +0200

    initial commit

---
+++ src/i18n/nb.js
@@ -0,0 +1,5 @@
+export default {
+  ACCOUNTSLIST_OPEN: 'Vis kontoliste',
+  ACCOUNTSLIST_CLOSE: 'Skjul kontoliste',
+  RESET_SEARCH: 'Nullstill',
+};
---
+++ src/util/account-formatter.test.js
@@ -0,0 +1,33 @@
+import { assert } from 'chai';
+import accountFormatter, { isValidNorwegianAccountNumber } from './account-formatter';
+
+const validNorwegianAccountNumber = '42000231376';
+const inValidNorwegianAccountNumber = '42000231377';
+
+describe('accountFormatter', () => {
+  it('should return unformatted int for int value', () => {
+    assert.strictEqual(accountFormatter(12345678912), 12345678912);
+  });
+  it('should return empty string for empty string', () => {
+    assert.strictEqual(accountFormatter(''), '');
+  });
+  it('should return formatted norwegian account number', () => {
+    assert.strictEqual(accountFormatter(validNorwegianAccountNumber), '4200 02 31376');
+  });
+});
+
+describe('valid norwegian account number', () => {
+  it('should return false for empty string', () => {
+    assert.isFalse(isValidNorwegianAccountNumber(''));
+  });
+  it('should return false for value with less than 11 chars', () => {
+    assert.isFalse(isValidNorwegianAccountNumber('123456789'));
+  });
+  it('should return formatted account number', () => {
+    assert.strictEqual(accountFormatter(validNorwegianAccountNumber), '4200 02 31376');
+  });
+  it('should return false for invalid norwegian account number', () => {
+    assert.isFalse(isValidNorwegianAccountNumber(inValidNorwegianAccountNumber));
+  });
+});
+
