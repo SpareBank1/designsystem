de5fcc0f229ffebb763010ef99131b5a1790a5f3
William Killerud <william.killerud@sparebank1.no>
2016-08-30 08:32:09 +0200

    Moved should_publish to buildCI.sh, added target/ to gitignore
    This lets users run build.sh locally without worrying about publishing, even with an updated package.json
    
    Added the generated target/ folder to .gitignore
    
    Also removed an unused baseline image committed when there was only one screen size.

--- buildCI.sh
+++ buildCI.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -e
-# Alias for build.sh to make package bob compatible
+# Runs build.sh before checking should_publish
+
+function should_publish() {
+    [[ $GIT_BRANCH =~ ^(origin/)?master$ ]]
+}
 
 source build.sh
+
+if should_publish; then
+    npm run has-published -s || npm publish
+    bob ci job build --jobname ffe-design-system_build_deploy
+fi
