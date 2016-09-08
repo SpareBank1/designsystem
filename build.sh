63815f4e88400e2f650b60dd81b106633126523a
Ørjan Helstrøm <orjan.helstrom@sparebank1.no>
2016-09-08 13:41:27 +0200

    Add example files Add visual tests Update buildsteps

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash
 
-npm install
+main() {
+    npm install
+    npm run compile
+
+    rm -rf target/
+    mkdir -p target/archive
+
+    ./run_visual-tests.sh
+
+    cp -R examples target/archive
+}
+
+_move_gemini_files() {
+    testRes=$?
+
+    cp -R gemini-report/ target/archive
+
+    exit ${testRes}
+}
+
+trap "_move_gemini_files" INT TERM EXIT
+
+main "$@"
