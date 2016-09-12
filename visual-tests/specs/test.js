b63e9666d911d68ff8f837d6dadb3a90c379e12f
William Killerud <william.killerud@sparebank1.no>
2016-09-12 11:40:28 +0200

    SFO-4335 Add remaining icons needed to upgrade ffe-icons

--- visual-tests/specs/test.js
+++ visual-tests/specs/test.js
@@ -1,20 +1,8 @@
-var fs = require('fs');
-
-
-
-var HTML_PATTERN = /\.html$/
-
-fs.readdirSync('./dist/symbol')
-  .filter(function(f) { return f.match(HTML_PATTERN) })
-  .forEach(function(f) {
-    gemini.suite(f.replace(HTML_PATTERN, ""), function(suite) {
-      suite.setUrl('/' + f)
-           .setCaptureElements('section ul')
-           .before(function(actions, find) {
-                actions.wait(1000);
-
-           })
-           .capture('plain');
-    })
-  });
-
+gemini.suite('dist', function (suite) {
+    suite.setUrl('/sprite.symbol.html')
+        .setCaptureElements('section ul')
+        .before(function (actions) {
+            actions.wait(1000);
+        })
+        .capture('plain');
+});
