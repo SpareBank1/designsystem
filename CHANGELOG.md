52de95a3d432089c33115b35428225f77da48634
Henrik Hermansen <henrik.hermansen@sparebank1.no>
2016-09-14 13:40:28 +0200

    DIG-12181 Added tests for AccountSelector (only covering changes in this DIG) Streamlined CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,29 +1,29 @@
 # Changelog
-#v2.1.0
+# v2.1.0
 * Added optional prop `noMatches` on `AccountSelector`
 
-#v2.0.0
+## v2.0.0
 * Bumped dependency on `ffe-core` to `^8.0.2`
 * Added mixins.less containing `.ffe-clearfix`, which was removed from `ffe-core`
 * Added `@import account-suggestions.less` in `account-selector.less`, to simplify import in other projects
 
-#v1.4.3
+## v1.4.3
 * Upgrade react dependencies to ^15.2.0 due to a bug with getNodeFromInstance in 15.1.0
 * Throw on blur only when leaving component, not when selecting account from dropdown or pressing reset-button
 
-# v.1.4.2
+## v1.4.2
 * Bugfix: React warning when no selectedAccount or value props are given
 * Bugfix: Missing X (reset) when selectedAccount or value are given
 * UX: Focus will remain in the input when account is selected (except when tab is pressed)
 
-## v.1.4.1
+## v1.4.1
 * Added callback to onAccountSelected with null account when the input is reset by user
 
-## v.1.4.0
+## v1.4.0
 * Added optional prop onAccountSelected
 * Bugfix: onBlur will no longer select the highlighted account if value doesn't match an account
 
-## v.1.3.0
+## v1.3.0
 * Bugfix: Select account on tab
 * Fire onChange with null on AccountSelector reset
 * Added optional prop onFocus
---
+++ src/account-selector.test.js
@@ -0,0 +1,55 @@
+/* We stick to require() rather than imports in this file because imports are hoisted, and thus ran before any other code.
+ * This is a problem because we need to set up document, window and navigator on global before we can import/require React.
+ * More info on
+ * https://github.com/airbnb/enzyme/issues/58
+ * http://stackoverflow.com/questions/26867535/calling-setstate-in-jsdom-based-tests-causing-cannot-render-markup-in-a-worker
+ */
+const jsdom = require('jsdom');
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
+global.navigator = global.window.navigator;
+
+const AccountSelector = require('./account-selector').default;
+const accountFormatter = require('./util/account-formatter').default;
+const amountFormatter = require('nfe-amount-formatter').default;
+
+const React = require('react');
+const assert = require('chai').assert;
+const mount = require('enzyme').mount;
+
+const accounts = [
+  { name: 'Matvarerkonto', accountNumber: '42011841044', balance: 24566546.21 },
+  { name: 'Sparekonto', accountNumber: '42011841045', balance: 124566546.21 }
+];
+const noMatches = 'Ingen treff';
+
+describe('<AccountSelector>', () => {
+  it('should show match when match is found', () => {
+    const el = <AccountSelector accounts={ accounts } onChange={ () => {} } noMatches={ noMatches } value="Spare" id="custom-id1" />;
+    const wrapper = mount(el);
+    wrapper.find('input').simulate('focus');
+
+    assert.equal(wrapper.find('.nfe-account-suggestions__name').length, 1);
+    assert.equal(wrapper.find('.nfe-account-suggestions__name').text(), accounts[1].name);
+    assert.include(wrapper.find('.nfe-account-suggestions__details').text(), accountFormatter(accounts[1].accountNumber));
+    assert.include(wrapper.find('.nfe-account-suggestions__details').text(), amountFormatter(accounts[1].balance));
+  });
+
+  it('should show all accounts when nothing is searched for', () => {
+    const el = <AccountSelector accounts={ accounts } onChange={ () => {} } noMatches={ noMatches } id="custom-id2" />;
+    const wrapper = mount(el);
+    wrapper.find('input').simulate('focus');
+
+    assert.equal(wrapper.find('.nfe-account-suggestions__name').length, accounts.length);
+  });
+
+  it('should show no matches when no matches are found', () => {
+    const el = <AccountSelector accounts={ accounts } onChange={ () => {} } noMatches={ noMatches } value="Trololol" id="custom-id3" />;
+    const wrapper = mount(el);
+    wrapper.find('input').simulate('focus');
+
+    assert.equal(wrapper.find('.nfe-account-suggestions__empty').length, 1);
+    assert.equal(wrapper.find('.nfe-account-suggestions__empty').text(), noMatches);
+  });
+});
