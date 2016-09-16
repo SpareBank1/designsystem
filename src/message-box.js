173f8c01b5608489b2e1638d940b754f8fd70f5c
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2016-09-16 10:39:01 +0200

    Stop rendering children inside paragraph
    This commit provides a solution for consumers who wants to send in
    custom HTML to a message box.
    
    Previously, children was put inside a `<p />` tag, which would
    produce a `validateDOMNesting` warning if you sent in any block-
    level elements (like other `<p />` tags, if you wanted two
    paragraphs of texts instead of one) as children.
    
    This commit renders `children` without this `<p />` tag, so that
    the consumer can send in whatever it pleases.
    
    If you want the current (old) behavior, it's still available by
    sending your content in through the `content` property instead.
    
    This is a potentially breaking change for consumers that are
    expecting children to be wrapped in a `<p />` tag. Those consumers
    can migrate to the new version either by adding the tag directly
    or using the `content` property.

--- src/message-box.js
+++ src/message-box.js
@@ -1,18 +1,44 @@
 import React from 'react';
 
-export default function MessageBox({ type, title, icon, content, children }) {
-  
-  return (
-   <div className="ffe-message-box">
-        <span className={`ffe-message-box__icon ffe-message-box__icon--${type}`}>
-            { icon }
-        </span>
-        <div className={`ffe-message-box__box ffe-message-box__box--${type}`}>
-            { title ? <div className={`ffe-h4 ffe-message-box__title--${type}`}>{ title }</div> : null }
-            <p className="ffe-body-text">
-                 { content || children }
-            </p>
+function MessageBox(props) {
+    const {
+        type,
+        title,
+        icon,
+        content,
+        children
+    } = props;
+
+    return (
+        <div className="ffe-message-box">
+            <span
+                className={`
+                    ffe-message-box__icon
+                    ffe-message-box__icon--${type}
+                `}
+            >
+                { icon }
+            </span>
+            <div
+                className={`
+                    ffe-message-box__box
+                    ffe-message-box__box--${type}
+                    `}
+            >
+                { title &&
+                    <div className={`ffe-h4 ffe-message-box__title--${type}`}>
+                        { title }
+                    </div>
+                }
+                { content &&
+                    <p className="ffe-body-text">
+                        { content }
+                    </p>
+                }
+                { !content && children }
+            </div>
         </div>
-    </div>
     );
 }
+
+export default MessageBox;
