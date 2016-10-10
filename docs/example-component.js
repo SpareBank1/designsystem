23b49b2e850898cc07f6c712a9a2c382b7190a03
Terje Røstum <terje.rostum@sparebank1.no>
2016-10-10 10:04:27 +0200

    Make it possible to override tabindex on input element

--- docs/example-component.js
+++ docs/example-component.js
@@ -16,4 +16,5 @@ export default props => <Checkbox
                             name={props.name}
                             label={props.label}
                             noMargins={props.noMargins}
+                            tabIndex={props.tabIndex}
                         />;
