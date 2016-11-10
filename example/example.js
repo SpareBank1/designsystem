b9707728c1caed0f2291f7a1de94399c9cd16ffb
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2016-11-10 12:12:51 +0100

    FFE-31 Create docs

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {renderToStaticMarkup} from 'react-dom/server';
-
+import ThumbUpIcon from 'ffe-icons-react/tommel-opp-ikon';
 import {
     ContextInfoMessage,
     ContextTipMessage,
@@ -9,43 +9,49 @@ import {
 const example = (
     <div>
         <div className="component-intro">
-            <h2 className="component-intro__header">System Message</h2>
-            <div className="lozenge lozenge-success">I PRODUKSJON</div>
+            <h2 className="component-intro__header">Kontekst Meldinger</h2>
+            <div className="lozenge lozenge--wip">UNDER ARBEID</div>
             <p className="ffe-micro-text ffe-external-docs-text">
-                HENTET FRA <a href="***REMOVED***">STASH</a>
+                RELATERTE PAKKER PÅ STASH:
+                <a href="***REMOVED***">LESS</a>
+                <a href="***REMOVED***">REACT</a>
             </p>
-            <p>Meldinger</p>
+            <p>Gir side eller produktspesifikk informasjon av ikke kritisk art</p>
         </div>
 
         <h3 className="ffe-h4">Eksempler</h3>
 
         <div className="example-container">
             <h4 className="example-container__header">Info</h4>
-            <ContextInfoMessage>
-                Informasjon
+            <ContextInfoMessage
+                header='Info'
+            >
+                Info om en spennede side eller produkt.
             </ContextInfoMessage>
         </div>
 
         <div className="example-container">
-            <h4 className="example-container__header">News</h4>
-            <ContextTipMessage>
-                Nyheter
+            <h4 className="example-container__header">Tips</h4>
+            <ContextTipMessage
+                header='Tips'
+                icon={<ThumbUpIcon />}
+            >
+                Tips om en spenndee side eller produkt.
             </ContextTipMessage>
         </div>
 
-
-
         <div className="expanding-field">
-            <button className="expanding-field__toggle">Vis kode</button>
+            <button className="expanding-field__toggle">Vis HTML</button>
             <span className="expanding-field__icon"></span>
             <div className="expanding-field__content">
                 <pre>
                     <code className="html hljs xml">
                         {
                             renderToStaticMarkup(
-                                <ContextInfoMessage>
-                                    Send innholdet her. Alt pakkes i et &lt;p&gt; element så unngå bruk av
-                                    feks &lt;div&gt;, &lt;p&gt; og &lt;ul&gt;.
+                                <ContextInfoMessage
+                                    header='Info'
+                                >
+                                    Info om en spennede side eller produkt.
                                 </ContextInfoMessage>
                             ).replace(/</, '\n<')
                         }
@@ -53,7 +59,6 @@ const example = (
                 </pre>
             </div>
         </div>
-
     </div>
 );
 
