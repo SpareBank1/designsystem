5ca44965b1db46161c046485bd49013967b7c8ed
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2016-11-10 13:17:09 +0100

    FFE-31 Visual test and baseline screenshots

--- visual-tests/spec/test.js
+++ visual-tests/spec/test.js
@@ -1,31 +1,15 @@
 var fs = require('fs');
 
-var HTML_PATTERN = /\.html$/
+var HTML_PATTERN = /\.html$/;
 
 fs.readdirSync('./examples')
-  .filter(function(f) { return f.match(HTML_PATTERN) && f.match(HTML_PATTERN)})
-  .forEach(function(f) {
-    gemini.suite(f.replace(HTML_PATTERN, ''), function(suite) {
-      suite.setUrl('/' + f)
-           .setCaptureElements('body')
-           .before(function(actions, find) {
-                actions.executeJS(function() {
-                      // Removes spinning/animation elements that impair test stability
-                      Array.prototype.forEach.call(
-                          document.querySelectorAll(
-                              '.ffe-loading-spinner, ' +
-                              '.ffe-action-button__label-spinner, ' +
-                              '.ffe-primary-button__label-spinner, ' +
-                              '.ffe-secondary-button__label-spinner, ' +
-                              '.visual-test-hidden'
-                          ),
-                          function(el) {
-                              el && el.parentNode.removeChild(el);
-                          }
-                      );
-                  })
-                  .wait(500);  // for checkbox animations to reach steady state
-           })
-           .capture('plain');
+    .filter(function (f) {
+        return f.match(HTML_PATTERN) && f.match(HTML_PATTERN)
     })
-  });
+    .forEach(function (f) {
+        gemini.suite(f.replace(HTML_PATTERN, ''), function (suite) {
+            suite.setUrl('/' + f)
+                .setCaptureElements('body')
+                .capture('plain');
+        })
+    });
