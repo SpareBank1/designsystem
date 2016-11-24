2d85fb41ca3639bd9828eb19688479814318b0d7
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2016-11-24 16:49:44 +0100

    FIX-1. Add name property support
    This commit adds support for specifying the name property of the
    <select /> element.
    
    This commit also cleans up indentation and white space usage.

--- src/dropdown.js
+++ src/dropdown.js
@@ -7,64 +7,72 @@ const createID = (label, options) => {
     return `Dropdown-${hash(label + stringifiedOptions)}`;
 };
 
-function Dropdown({
-    id,
-    className = '',
-    containerClassName = '',
-    label,
-    onChange,
-    onBlur,
-    selectedValue,
-    defaultValue,
-    children,
-    invalid,
-    isLoading,
-    errorMessage
-}) {
+function Dropdown(props) {
+    const {
+        id,
+        children,
+        className = '',
+        containerClassName = '',
+        defaultValue,
+        errorMessage,
+        invalid,
+        isLoading,
+        label,
+        name,
+        onChange,
+        onBlur,
+        selectedValue
+    } = props;
+
     const domId = id || createID(label, children);
 
-    return  <div className={ `ffe-input-group ${containerClassName}` } aria-live="polite">
-                { label &&
-                        <label className="ffe-form-label ffe-form-label--block" htmlFor={ domId }>
-                            { label }
-                        </label>
-                }
-                <select
-                        className={ `ffe-dropdown ${className}` }
-                        id={ domId }
-                        onChange={ onChange }
-                        onBlur={ onBlur }
-                        value={ selectedValue }
-                        defaultValue={ defaultValue }
-                        aria-invalid={ invalid }
-                >
-                    { children }
-                </select>
+    return (
+        <div className={`ffe-input-group ${containerClassName}`} aria-live="polite">
+            {label &&
+                <label className="ffe-form-label ffe-form-label--block" htmlFor={domId}>
+                    {label}
+                </label>
+            }
+            <select
+                className={`ffe-dropdown ${className}`}
+                id={domId}
+                name={name}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={selectedValue}
+                defaultValue={defaultValue}
+                aria-invalid={invalid}
+            >
+                {children}
+            </select>
 
-                {isLoading &&
-                    <div className="ffe-loading-spinner"></div>
-                }
+            {isLoading &&
+                <div className="ffe-loading-spinner"></div>
+            }
 
-                {!errorMessage ? null :
-                    <div className="ffe-field-error-message">
-                        { errorMessage }
-                    </div>
-                }
-            </div>;
+            {errorMessage &&
+                <div className="ffe-field-error-message">
+                    {errorMessage}
+                </div>
+            }
+        </div>
+    );
 }
 
 Dropdown.propTypes = {
     id: PropTypes.string,
+    children: PropTypes.array,
     className: PropTypes.string,
     containerClassName: PropTypes.string,
+    defaultValue: PropTypes.string,
+    errorMessage: PropTypes.string,
+    invalid: PropTypes.bool,
+    isLoading: PropTypes.bool,
     label: PropTypes.string,
+    name: PropTypes.string,
     onChange: PropTypes.func.isRequired,
     onBlur: PropTypes.func,
-    selectedValue: PropTypes.string,
-    defaultValue: PropTypes.string,
-    children: PropTypes.array,
-    invalid: PropTypes.bool,
-    isLoading: PropTypes.bool
+    selectedValue: PropTypes.string
 };
 
 export default Dropdown;
