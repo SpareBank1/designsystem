35f095b2ad864d4afe55042b09994b1827707e9a
Morten Laugerud <morten.laugerud@sparebank1.no>
2016-11-28 13:06:09 +0100

    indentation, example data

--- examples/tables.data.js
+++ examples/tables.data.js
@@ -2,8 +2,9 @@ export const SIMPLE_TABLE = {
   headers: [
     { key: 'name', content: 'Navn' },
     { key: 'address', content: 'Adresse' },
+    { key: 'age', content: 'Alder', alignRight: true },
     { key: 'phone', content: 'Telefon' },
-    { key: 'email', content: 'E-post', alignRight: true},
+    { key: 'email', content: 'E-post'},
   ],
   data: [
     [ 'Ola Normann', 'Gateveien 2', '912 34 567', 'ola@normann.no' ],
@@ -15,6 +16,7 @@ export const SIMPLE_TABLE = {
     {
       name: 'Ola Normann',
       address: 'Gateveien 2',
+      age: 23,
       phone: '912 34 567',
       email: 'ola@normann.no',
       username: 'olli22'
@@ -22,6 +24,7 @@ export const SIMPLE_TABLE = {
     {
       name: 'Sivert Svenska',
       address: 'Gatuveio 7',
+      age: 45,
       phone: '+46 123 456 789',
       email: 'sivert@svenska.se',
       username: 'zlatan_2000'
@@ -29,6 +32,7 @@ export const SIMPLE_TABLE = {
     {
       name: 'Daniel Dansk',
       address: 'Kartoffel Fjers',
+      age: 67,
       phone: '+45 018 456 789',
       email: 'daniel@dansk.dk',
       username: 'kartoffel88'
@@ -36,6 +40,7 @@ export const SIMPLE_TABLE = {
     {
       name: 'Anders Amerikaner',
       address: 'Road Street 52',
+      age: 78,
       phone: '+1 555 555 5555',
       email: 'anders@amerikaner.biz',
       username: 'andy-mandy'
--- src/ResponsiveTable/ResponsiveTable.js
+++ src/ResponsiveTable/ResponsiveTable.js
@@ -3,76 +3,76 @@ import TableHeaders from '../TableHeaders.jsx';
 import TableRow from '../TableRow.jsx';
 
 class ResponsiveTable extends Component {
-  renderTableCaption() {
-    const {
-      caption,
-    } = this.props;
+    renderTableCaption() {
+        const {
+            caption,
+        } = this.props;
 
-    if (!caption) {
-      return null;
+        if (!caption) {
+            return null;
+        }
+
+        return (
+            <caption className="ffe-responsive-table__caption">
+                {caption}
+            </caption>
+        );
     }
 
-    return (
-      <caption className="ffe-responsive-table__caption">
-        {caption}
-      </caption>
-    );
-  }
+    renderTableHeaders() {
+        const {
+            headers,
+        } = this.props;
 
-  renderTableHeaders() {
-    const {
-      headers,
-    } = this.props;
+        if (!headers || !headers.length) {
+            return null;
+        }
 
-    if (!headers || !headers.length) {
-      return null;
+        return <TableHeaders headers={ headers } />;
     }
 
-    return <TableHeaders headers={ headers } />;
-  }
+    renderTableBody() {
+        const {
+            data,
+            headers,
+            headerData
+        } = this.props;
 
-  renderTableBody() {
-    const {
-      data,
-      headers,
-      headerData
-    } = this.props;
+        if (!data || !data.length) {
+            return null;
+        }
 
-    if (!data || !data.length) {
-      return null;
+        return (
+            <tbody>
+                {data.map((row, index) => (
+                    <TableRow cells={ row } headers={ headerData || headers } key={ index } />
+                ))}
+            </tbody>
+        );
     }
 
-    return (
-      <tbody>
-        {data.map((row, index) => (
-          <TableRow cells={ row } headers={ headerData || headers } key={ index } />
-        ))}
-      </tbody>
-    );
-  }
-
-  render() {
-    return (
-      <table className="ffe-responsive-table">
-        {this.renderTableCaption()}
-        {this.renderTableHeaders()}
-        {this.renderTableBody()}
-      </table>
-    );
-  }
+    render() {
+        return (
+            <table className="ffe-responsive-table">
+                {this.renderTableCaption()}
+                {this.renderTableHeaders()}
+                {this.renderTableBody()}
+            </table>
+        );
+    }
 }
 
 ResponsiveTable.propTypes = {
-  caption: PropTypes.string,
-  data: PropTypes.arrayOf(
-    PropTypes.object
-  ).isRequired,
-  headers: PropTypes.arrayOf(
-    PropTypes.shape({
-      content: PropTypes.node.isRequired,
-      key: PropTypes.string.isRequired,
-    }).isRequired
-  ),
+    caption: PropTypes.string,
+    data: PropTypes.arrayOf(
+        PropTypes.object
+    ).isRequired,
+    headers: PropTypes.arrayOf(
+        PropTypes.shape({
+            content: PropTypes.node.isRequired,
+            key: PropTypes.string.isRequired,
+        }).isRequired
+    ),
 };
 
 export default ResponsiveTable;
