6a65248f9192903029d74429ddd2580fd030bc74
Ivar Nilsen <ivar.nilsen@sparebank1.no>
2016-11-30 07:23:16 +0100

    Introduce stylelint for .less files

---
+++ .stylelintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "stylelint-config-standard",
+    "rules": {
+        "at-rule-empty-line-before": [
+            "always", { "except": [ "blockless-after-same-name-blockless" ] }
+        ],
+        "at-rule-no-vendor-prefix": true,
+        "color-hex-case": "upper",
+        "declaration-block-properties-order": "alphabetical",
+        "indentation": [4, { "ignore": ["inside-parens"] }],
+        "max-nesting-depth": 3,
+        "media-feature-name-no-vendor-prefix": true,
+        "property-no-vendor-prefix": true,
+        "selector-max-specificity": "0,3,0",
+        "selector-no-vendor-prefix": true,
+        "value-no-vendor-prefix": true,
+    }
+}
