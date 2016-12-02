bf7aba32bd10c6a9780d826a0543efc17628fb50
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2016-12-02 15:35:52 +0100

    Remove redundant code with object rest and spread

--- src/context-info-message.js
+++ src/context-info-message.js
@@ -1,17 +1,11 @@
 import React, { PropTypes } from 'react';
 import Base from './base';
-import acceptedLocales from './locale/accepted-locales';
 
-export default function ContextInfoMessage({ style, children, header, icon, onClose, locale, showCloseButton }) {
+export default function ContextInfoMessage({children, ...rest}) {
     return (
         <Base
             messageType="info"
-            locale={locale}
-            icon={icon}
-            header={header}
-            style={style}
-            onClose={onClose}
-            showCloseButton={showCloseButton}
+            {...rest}
         >
             {children}
         </Base>
@@ -20,10 +14,4 @@ export default function ContextInfoMessage({ style, children, header, icon, onCl
 
 ContextInfoMessage.propTypes = {
     children: PropTypes.node.isRequired,
-    showCloseButton : PropTypes.bool.isRequired,
-    locale : PropTypes.oneOf(acceptedLocales),
-    header: PropTypes.string,
-    style: PropTypes.object,
-    onClose: PropTypes.func,
-    icon: PropTypes.element,
 };
