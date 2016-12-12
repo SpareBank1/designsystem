9f4e9ed3e6a462fa1731c83ae8ad80cb384a56ca
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2016-12-12 16:10:58 +0100

    Revert "Fix hover for no-matches list item"
    This reverts commit be69911d0c62e9f13a4209d24c37a000004f1312.

--- src/suggestion/account-suggestions-empty.js
+++ src/suggestion/account-suggestions-empty.js
@@ -1,11 +1,13 @@
 import React, { PropTypes } from 'react';
 
-const AccountSuggestionsEmpty = ({ value }) =>
-  <ul className="nfe-account-suggestions" role="listbox">
-    <li className="nfe-account-suggestions__item-no-matches">
-      <span>{ value }</span>
-    </li>
-  </ul>;
+const AccountSuggestionsEmpty = ({value}) =>
+    <ul className="nfe-account-suggestions" role="listbox">
+      <li className="nfe-account-suggestions__item">
+        <div className="nfe-account-suggestions__empty">
+          <span className="nfe-account-suggestions__name">{ value }</span>
+        </div>
+      </li>
+    </ul>;
 
 AccountSuggestionsEmpty.propTypes = {
   value: PropTypes.string
