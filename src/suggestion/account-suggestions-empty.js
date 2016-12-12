2b4a3c03c8fc8bd8bdc7321162499a1a63eb4924
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2016-12-12 10:53:59 +0100

    Fix hover for no-matches list item

--- src/suggestion/account-suggestions-empty.js
+++ src/suggestion/account-suggestions-empty.js
@@ -1,13 +1,11 @@
 import React, { PropTypes } from 'react';
 
-const AccountSuggestionsEmpty = ({value}) =>
-    <ul className="nfe-account-suggestions" role="listbox">
-      <li className="nfe-account-suggestions__item">
-        <div className="nfe-account-suggestions__empty">
-          <span className="nfe-account-suggestions__name">{ value }</span>
-        </div>
-      </li>
-    </ul>;
+const AccountSuggestionsEmpty = ({ value }) =>
+  <ul className="nfe-account-suggestions" role="listbox">
+    <li className="nfe-account-suggestions__item-no-matches">
+      <span>{ value }</span>
+    </li>
+  </ul>;
 
 AccountSuggestionsEmpty.propTypes = {
   value: PropTypes.string
