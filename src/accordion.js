4c0cb8b3323be4e6e2e6bbf4a8bb98fecafcfafd
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2016-12-16 10:15:55 +0100

    FIX-1. Refactor and reformat code
    This commit rewrites and refactors the code, to make it easier to
    maintain for future developers. In addition, all the lint errors
    from the upgrade in eslint-config-ffe are now fixed.

--- src/accordion.js
+++ src/accordion.js
@@ -1,14 +1,23 @@
 import React, { PropTypes, cloneElement } from 'react';
 
-export default function Accordion(props) {
-    return <ul {...props} className="ffe-accordion" role="tablist" aria-multiselectable="true">
-                { React.Children.map(props.children, 
-                    (ele, index) => cloneElement(ele, { type: props.type, index })) }
-            </ul>;
+function Accordion(props) {
+    return (
+        <ul
+            {...props}
+            className="ffe-accordion"
+            role="tablist"
+            aria-multiselectable="true"
+        >
+            {React.Children.map(props.children, (ele, index) =>
+                cloneElement(ele, { type: props.type, index }))
+            }
+        </ul>
+    );
 }
 
-
 Accordion.propTypes = {
-    children: PropTypes.array.isRequired,
+    children: PropTypes.node.isRequired,
     type: PropTypes.oneOf(['white', 'blue']).isRequired
 };
+
+export default Accordion;
