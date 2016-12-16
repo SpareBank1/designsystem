792a0b28b2775ca329cfe970f483c5ea124ceae1
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2016-12-16 10:15:55 +0100

    FIX-1. Refactor and reformat code
    This commit rewrites and refactors the code, to make it easier to
    maintain for future developers. In addition, all the lint errors
    from the upgrade in eslint-config-ffe are now fixed.

--- src/accordion-item.js
+++ src/accordion-item.js
@@ -1,5 +1,5 @@
+import React, { Component, PropTypes } from 'react';
 import FFEExpandable from 'ffe-expandable-react';
-import React, { PropTypes } from 'react';
 import Chevron from 'ffe-icons-react/chevron-ikon';
 import classNames from 'classnames';
 
@@ -8,68 +8,109 @@ const createClasses = (baseClass, isOpen, type) => classNames(baseClass, {
     [`${baseClass}--${type}`]: true
 });
 
-const isIgnoredNode = (node, ignoredNodes) => ignoredNodes.some(name => name === node.nodeName);
+class AccordionItem extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            isOpen: props.isOpen,
+        };
 
-export default React.createClass({
-    propTypes: {
-        isOpen: PropTypes.bool,
-        onOpen: PropTypes.func,
-        onClose: PropTypes.func,
-        type: PropTypes.oneOf(['white', 'blue']),
-        index: PropTypes.number,
-        ignoredNodeNames: PropTypes.array,
-        children: PropTypes.node,
-        expandedContent: PropTypes.node,
-        ariaLabel: PropTypes.string
-    },
-
-    getInitialState() {
-        return { isOpen: this.props.isOpen || false };
-    },
-
-    toggle() {
-        if (this.props.onClose && this.state.isOpen) this.props.onClose();
-        else if (this.props.onOpen && !this.state.isOpen) this.props.onOpen();
-        this.setState({ isOpen: !this.state.isOpen });
-    },
+        this.onClick = this.onClick.bind(this);
+    }
 
     onClick(e) {
-        const ignoredNodes = this.props.ignoredNodeNames;
+        const {
+            ignoredNodeNames,
+        } = this.props;
 
-        if (ignoredNodes && isIgnoredNode(e.target, ignoredNodes)) return;
+        if (ignoredNodeNames.some(name => name === e.target.nodeName)) {
+            return;
+        }
         this.toggle();
-    },
+    }
+
+    toggle() {
+        const { isOpen } = this.state;
+
+        if (isOpen) {
+            this.props.onClose();
+        } else {
+            this.props.onOpen();
+        }
+
+        this.setState({ isOpen: !isOpen });
+    }
+
+    renderExpandedContent() {
+        const { isOpen } = this.state;
+        const {
+            type,
+            index,
+            expandedContent,
+        } = this.props;
+
+        return (
+            <div
+                className={ createClasses('ffe-accordion-item__content', isOpen, type) }
+                role="tabpanel"
+                id={ `panel-${index}` }
+                aria-hidden={ !isOpen }
+                aria-labelledby={ `tab-${index}` }
+            >
+                { expandedContent }
+            </div>
+        );
+    }
 
     render(props) {
         const { isOpen } = this.state;
-        const { type, index, ariaLabel } = this.props;
+        const {
+            children,
+            type,
+            index,
+            ariaLabel
+        } = this.props;
 
-        return <li
-                    className={ createClasses('ffe-accordion-item', isOpen, type) } >
-                    <a
-                        id={`tab-${index}`}
-                        href="javascript:;"
-                        className={ createClasses('ffe-accordion-item__toggler', isOpen, type) }
-                        onClick={ this.onClick }
-                        aria-expanded={ isOpen }
-                        aria-label={ ariaLabel }
-                        role="tab"
-                        aria-controls={`panel-${index}`}>
-                        <Chevron className={ createClasses('ffe-accordion-item__icon', isOpen, type) } />
-                        { this.props.children }
-                    </a>
-                    <FFEExpandable
-                                isOpen={ isOpen }
-                                folded=""
-                                expanded={
-                                    <div className={ createClasses('ffe-accordion-item__content', isOpen, type) }
-                                         role="tabpanel"
-                                         id={`panel-${index}`}
-                                         aria-hidden={ !isOpen }
-                                         aria-labelledby={`tab-${index}`}>
-                                        { this.props.expandedContent }
-                                    </div> }
-                    />
-                </li>;
+        return (
+            <li className={ createClasses('ffe-accordion-item', isOpen, type) }>
+                <a
+                    aria-controls={ `panel-${index}` }
+                    aria-expanded={ isOpen }
+                    aria-label={ ariaLabel }
+                    className={ createClasses('ffe-accordion-item__toggler', isOpen, type) }
+                    href="javascript:;"
+                    id={`tab-${index}`}
+                    onClick={ this.onClick }
+                    role="tab"
+                >
+                    <Chevron className={ createClasses('ffe-accordion-item__icon', isOpen, type) } />
+                    { children }
+                </a>
+                <FFEExpandable
+                    expanded={ this.renderExpandedContent() }
+                    isOpen={ isOpen }
+                />
+            </li>
+        );
     }
-});
+}
+
+AccordionItem.propTypes = {
+    ariaLabel: PropTypes.string,
+    children: PropTypes.node,
+    expandedContent: PropTypes.node,
+    ignoredNodeNames: PropTypes.arrayOf(PropTypes.string),
+    index: PropTypes.number,
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func,
+    onOpen: PropTypes.func,
+    type: PropTypes.oneOf(['white', 'blue'])
+};
+
+AccordionItem.defaultProps = {
+    ignoredNodeNames: [],
+    onClose: f => f,
+    onOpen: f => f,
+};
+
+export default AccordionItem;
