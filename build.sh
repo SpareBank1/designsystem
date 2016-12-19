46ec563b6e46d843a2c8263c78dbf9f241000221
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2016-12-19 16:06:09 +0100

    Initial commit (boilerplate)
    This commit adds boilerplate project code created with generator-ffe.

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ex
+
+main() {
+    yarn install
+    npm run compile
+
+    rm -rf target/
+    mkdir -p target/archive
+
+    ./run_visual-tests.sh
+
+    cp -R examples target/archive
+}
+
+_move_gemini_files() {
+    testRes=$?
+
+    cp -R gemini-report/ target/archive
+
+    exit ${testRes}
+}
+
+trap "_move_gemini_files" INT TERM EXIT
+
+main "$@"
