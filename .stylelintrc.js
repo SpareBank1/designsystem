615a0e5d5c765c329bb7d0949752d31fb971afa7
Kristofer Walters <kristofer.walters@sparebank1.no>
2016-12-27 13:03:43 +0100

    FFE-130 use stylelint-config-ffe

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-    "extends": "stylelint-config-standard",
-    "rules": {
-        "at-rule-empty-line-before": [
-            "always", { "except": [ "blockless-after-same-name-blockless" ] }
-        ],
-        "at-rule-no-vendor-prefix": true,
-        "color-hex-case": "upper",
-        "indentation": [4, { "ignore": ["inside-parens"] }],
-        "max-nesting-depth": 3,
-        "media-feature-name-no-vendor-prefix": true,
-        "property-no-vendor-prefix": true,
-        "selector-max-specificity": "0,3,0",
-        "selector-no-vendor-prefix": true,
-        "value-no-vendor-prefix": true,
-    }
+    "extends": "stylelint-config-ffe"
 }
