acc3bc97ed8e965851e0c14babfbf306b52cae75
Morten Laugerud <morten.laugerud@sparebank1.no>
2017-01-09 09:01:33 +0100

    minor changes, lint fixes

--- src/TableRowExpandable/TableRowExpandable.js
+++ src/TableRowExpandable/TableRowExpandable.js
@@ -1,73 +1,76 @@
 import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
-import TableCell from '../TableCell.jsx';
 import TableRow from '../TableRow.jsx';
 import Chevron from 'ffe-icons-react/chevron-ikon';
 
 class TableRowExpandable extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false
-    };
-  }
-
-  toggleExpand() {
-    this.setState({ expanded: !this.state.expanded});
-  }
-
-  handleKeyPress(event) {
-    if ( event.key === 'Enter' && event.target.tagName === 'TBODY') {
-      this.setState({ expanded: !this.state.expanded});
-      event.preventDefault();
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: false
+        };
     }
-  }
 
-  render() {
-    var { cells, headers, children } = this.props;
-    let tabIndex = { tabIndex:'0' };
+    toggleExpand() {
+        this.setState({ expanded: !this.state.expanded});
+    }
 
-    cells.expandIcon = <Chevron className='expand-icon'/>;
+    handleKeyPress(event) {
+        if ( event.key === 'Enter' && event.target.tagName === 'TBODY') {
+            this.setState({ expanded: !this.state.expanded});
+            event.preventDefault();
+        }
+    }
 
-    let clickableRow = (
-      <TableRow
-        cells={cells}
-        trClasses={'ffe-responsive-table__row-expandable'}
-        headers={headers}
-        onClick={this.toggleExpand.bind(this)}
-        expanded={ this.state.expanded } />
-    );
-    let expandedRow = (
-      <tr role="presentation" className="ffe-responsive-table__row-expanded-content">
-        <td
-          colSpan={headers.length}
-        >
-          <div>
-            { children }
-          </div>
-        </td>
-      </tr>
-    );
+    render() {
+        const { cells, headers, children } = this.props;
+        const tabIndex = { tabIndex:'0' };
 
-    let expandedContent = this.state.expanded ? expandedRow : 
-      <tr role="presentation" className="ffe-responsive-table__row-expanded-content collapsed">
-        <td colSpan={headers.length}>
-          <div>
-          </div>
-        </td>
-      </tr>;
+        cells.expandIcon = <Chevron className='expand-icon'/>;
 
-    return <tbody {...tabIndex} onKeyDown={this.handleKeyPress.bind(this)}>
-      {clickableRow}
-      {expandedContent}
-      </tbody>;
-  }
+        const clickableRow = (
+            <TableRow
+                cells={ cells }
+                trClasses={ 'ffe-responsive-table__row-expandable' }
+                headers={ headers }
+                onClick={ this.toggleExpand.bind(this) }
+                expanded={ this.state.expanded }
+            />
+        );
+
+        console.log(children);
+
+        const expandedRow = (
+            <tr
+                role="presentation"
+                aria-expanded={ this.state.expanded ? 'true' : 'false' }
+                className={ classNames(
+                    'ffe-responsive-table__row-expanded-content',
+                     { 'collapsed' : !this.state.expanded }
+                 )}
+            >
+                <td colSpan={ headers.length }>
+                    <div>
+                        { this.state.expanded && children }
+                    </div>
+                </td>
+            </tr>
+        );
+
+        return (
+            <tbody { ...tabIndex } onKeyDown={ this.handleKeyPress.bind(this) }>
+                { clickableRow }
+                { expandedRow }
+            </tbody>
+        );
+    }
 }
 
 TableRowExpandable.propTypes = {
-  cells: React.PropTypes.object.isRequired,
-  headers: React.PropTypes.array.isRequired
+    children: PropTypes.node.isRequired,
+    cells: PropTypes.object.isRequired,
+    headers: PropTypes.array.isRequired
 };
 
 export default TableRowExpandable;
