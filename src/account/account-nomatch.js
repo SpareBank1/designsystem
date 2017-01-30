b582c23739832660960b6ebf20f3d220ab76b504
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2017-01-30 09:19:16 +0100

    Styling for suggestion-nomatches

--- src/account/account-nomatch.js
+++ src/account/account-nomatch.js
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 
 const AccountNoMatch = ({value}) =>
-  <div className='nfe-account-suggestions__empty'>
-    <span className='nfe-account-suggestions__name'>{ value }</span>
+  <div className='account-suggestion__nomatches'>
+    <span>{ value }</span>
   </div>;
 
 AccountNoMatch.propTypes = {
