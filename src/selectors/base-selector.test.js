8e2117e4d59d1660cd24a1a24dd14a2ca603820f
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2017-02-22 15:36:44 +0100

    Tests for base-selector

---
+++ src/selectors/base-selector.test.js
@@ -0,0 +1,296 @@
+import {shallow} from 'enzyme';
+import {assert} from 'chai';
+import sinon from 'sinon';
+import React from 'react';
+import BaseSelector from './base-selector';
+import {KeyCodes} from '../util/types';
+
+function shallowBaseSelector(props) {
+  return shallow(
+    <BaseSelector
+      {...{...propsBaseSelector(), ...props}}
+    />);
+}
+
+function suggestions() {
+  return [
+    {header: '1'},
+    {header: '2'},
+    {header: '3'}
+  ];
+}
+
+function propsBaseSelector(_suggestions = suggestions()) {
+  return {
+    suggestions: _suggestions,
+    suggestionFilter: ()=> ()=> true,
+    onSelect: () => {},
+    value: '',
+    shouldHideSuggestionsOnSelect: true,
+    shouldSelectHighlightedOnTab: true,
+  };
+}
+
+function assertHomeEnd(keyCode, stubMethodName) {
+  const component = shallowBaseSelector().instance();
+  const setLastHighlighted = sinon.stub(component, stubMethodName);
+  const preventDefaultSpy = sinon.spy();
+  component.state.showSuggestions = true;
+
+  component.onInputKeyDown({which: keyCode, preventDefault: preventDefaultSpy});
+  assert.isTrue(setLastHighlighted.calledOnce);
+  assert.isTrue(preventDefaultSpy.calledOnce);
+}
+
+function assertSuggestionSelected(event) {
+  const component = shallowBaseSelector().instance();
+  const onSuggestionSelectSpy = sinon.stub(component, 'onSuggestionSelect');
+  component.state = {highlightedSuggestionIndex: 0, showSuggestions: true};
+
+  component.onInputKeyDown(event);
+  assert.isTrue(onSuggestionSelectSpy.calledWith(suggestions()[0]));
+}
+
+
+describe('<BaseSelector> methods', () => {
+
+  it('should show suggestions on input change', () => {
+    const value = 'test';
+    const onChangeSpy = sinon.spy();
+    const component = shallowBaseSelector({onChange: onChangeSpy}).instance();
+
+    component.onInputChange(value);
+    assert.isTrue(component.state.showSuggestions);
+    assert.isTrue(onChangeSpy.calledWith(value));
+  });
+
+  it('should show suggestions on input focus', () => {
+    const onFocusSpy = sinon.spy();
+    const component = shallowBaseSelector({onFocus: onFocusSpy}).instance();
+
+    component.onFocus();
+    assert.isTrue(component.state.showSuggestions);
+    assert.isTrue(onFocusSpy.calledOnce);
+  });
+
+  it('should not show suggestions on input focus if clickAction was performed', () => {
+    const onFocusSpy = sinon.spy();
+    const component = shallowBaseSelector({onFocus: onFocusSpy}).instance();
+    component.didPerformClickAction = true;
+
+    component.onFocus();
+    assert.isFalse(component.state.showSuggestions);
+    assert.isFalse(onFocusSpy.called);
+    assert.isFalse(component.didPerformClickAction);
+  });
+
+  it('should hide suggestions on input blur', () => {
+    const onBlurSpy = sinon.spy();
+    const component = shallowBaseSelector({onBlur: onBlurSpy}).instance();
+
+    component.onBlur();
+    assert.isFalse(component.state.showSuggestions);
+    assert.isTrue(onBlurSpy.calledOnce);
+  });
+
+  it('should not hide suggestions on input blur if clickAction was performed', () => {
+    const onBlurSpy = sinon.spy();
+    const inputFocusSpy = sinon.spy();
+    const component = shallowBaseSelector({onBlur: onBlurSpy}).instance();
+    component.didPerformClickAction = true;
+    component.input = {focus: inputFocusSpy};
+
+    component.onBlur();
+    assert.isFalse(component.state.showSuggestions);
+    assert.isFalse(onBlurSpy.called);
+    assert.isTrue(inputFocusSpy.calledOnce);
+  });
+
+  it('should hide suggestions on suggestionSelect', () => {
+    const onSelectSpy = sinon.spy();
+    const suggestion = {suggestion: 'suggestion'};
+    const component = shallowBaseSelector({onSelect: onSelectSpy}).instance();
+    component.state.showSuggestions = true;
+
+    component.onSuggestionSelect(suggestion);
+    assert.isTrue(onSelectSpy.calledOnce);
+    assert.isFalse(component.state.showSuggestions);
+  });
+
+  it('should not hide suggestions when props.shouldHideSuggestionsOnSelect = false', ()=> {
+    const onSelectSpy = sinon.spy();
+    const suggestion = {suggestion: 'suggestion'};
+    const component = shallowBaseSelector({onSelect: onSelectSpy, shouldHideSuggestionsOnSelect: false}).instance();
+
+    component.onSuggestionSelect(suggestion);
+    assert.isTrue(onSelectSpy.calledOnce);
+    assert.isFalse(component.state.showSuggestions);
+
+  });
+
+  it('should not hide suggestions when suggestion is empty', ()=> {
+    const onSelectSpy = sinon.spy();
+    const component = shallowBaseSelector({onSelect: onSelectSpy, shouldHideSuggestionsOnSelect: false}).instance();
+
+    component.onSuggestionSelect(null);
+    assert.isFalse(onSelectSpy.called);
+    assert.isFalse(component.state.showSuggestions);
+  });
+
+  it('should set didPerformClickAction on suggestionClick', () => {
+    const component = shallowBaseSelector().instance();
+
+    component.onSuggestionClick();
+    assert.isTrue(component.didPerformClickAction);
+  });
+
+  it('should set didPerformClickAction on onInputResetClick', () => {
+    const component = shallowBaseSelector().instance();
+
+    component.onInputResetClick();
+    assert.isTrue(component.didPerformClickAction);
+  });
+
+  it('should hide suggestions on input reset', () => {
+    const onResetSpy = sinon.spy();
+    const component = shallowBaseSelector({onReset: onResetSpy}).instance();
+
+    component.onInputReset();
+    assert.isFalse(component.state.showSuggestions);
+    assert.isTrue(onResetSpy.calledOnce);
+  });
+
+  it('should increment highlightedSuggestionIndex on nextHighlightedIndex', ()=> {
+    const component = shallowBaseSelector().instance();
+    const scollPosSpy = sinon.spy();
+    component.state.highlightedSuggestionIndex = 1;
+    component.suggestionList = {setScrollPosNext: scollPosSpy};
+
+    component.setNextHighlighted();
+    assert.equal(component.state.highlightedSuggestionIndex, 2);
+    assert.isTrue(scollPosSpy.calledOnce);
+  });
+
+  it('should wrap highlightedSuggestionIndex on nextHighlightedIndex', ()=> {
+    const component = shallowBaseSelector().instance();
+    const scollPosSpy = sinon.spy();
+    component.state.highlightedSuggestionIndex = 2;
+    component.suggestionList = {setScrollPosStart: scollPosSpy};
+
+    component.setNextHighlighted();
+    assert.equal(component.state.highlightedSuggestionIndex, 0);
+    assert.isTrue(scollPosSpy.calledOnce);
+  });
+
+  it('should decrement highlightedSuggestionIndex on previousHighlightedIndex', ()=> {
+    const component = shallowBaseSelector().instance();
+    const scollPosSpy = sinon.spy();
+    component.state.highlightedSuggestionIndex = 1;
+    component.suggestionList = {setScrollPosPrevious: scollPosSpy};
+
+    component.setPreviousHighlighted();
+    assert.equal(component.state.highlightedSuggestionIndex, 0);
+    assert.isTrue(scollPosSpy.calledOnce);
+  });
+
+
+  it('should wrap highlightedSuggestionIndex on previousHighlightedIndex', ()=> {
+    const component = shallowBaseSelector().instance();
+    const scollPosSpy = sinon.spy();
+    component.state.highlightedSuggestionIndex = 0;
+    component.suggestionList = {setScrollPosEnd: scollPosSpy};
+
+    component.setPreviousHighlighted();
+    assert.equal(component.state.highlightedSuggestionIndex, 2);
+    assert.isTrue(scollPosSpy.calledOnce);
+  });
+
+  it('should setFirstHighlighted ', () => {
+    const component = shallowBaseSelector().instance();
+    const scollPosSpy = sinon.spy();
+    component.suggestionList = {setScrollPosStart: scollPosSpy};
+
+    component.setFirstHighlighted();
+    assert.equal(component.state.highlightedSuggestionIndex, 0);
+    assert.isTrue(scollPosSpy.calledOnce);
+  });
+
+  it('should setLastHighlighted ', () => {
+    const component = shallowBaseSelector().instance();
+    const scollPosSpy = sinon.spy();
+    component.suggestionList = {setScrollPosEnd: scollPosSpy};
+
+    component.setLastHighlighted();
+    assert.equal(component.state.highlightedSuggestionIndex, 2);
+    assert.isTrue(scollPosSpy.calledOnce);
+  });
+});
+
+describe('<BaseSelector> keyboard navigation', () => {
+  it('should show suggestions on ALT + DOWN', ()=> {
+    const component = shallowBaseSelector().instance();
+    const showHideSuggestionsSpy = sinon.spy(component, 'showHideSuggestions');
+
+    component.onInputKeyDown({which: KeyCodes.DOWN, altKey: true});
+    assert.isTrue(showHideSuggestionsSpy.calledWith(true));
+  });
+
+  it('should highlight next if suggestion are showing on DOWN', ()=> {
+    const component = shallowBaseSelector().instance();
+    const setNextHighlightedStub = sinon.stub(component, 'setNextHighlighted');
+    component.state.showSuggestions = true;
+
+    component.onInputKeyDown({which: KeyCodes.DOWN});
+    assert.isTrue(setNextHighlightedStub.called);
+  });
+
+  it('should hide suggestions on ALT + UP', ()=> {
+    const component = shallowBaseSelector().instance();
+    const showHideSuggestionsSpy = sinon.spy(component, 'showHideSuggestions');
+    component.state.showSuggestions = true;
+
+    component.onInputKeyDown({which: KeyCodes.UP, altKey: true});
+    assert.isTrue(showHideSuggestionsSpy.calledWith(false));
+  });
+
+  it('should highlight previous suggestions on UP', ()=> {
+    const component = shallowBaseSelector().instance();
+    const showHideSuggestionsStub = sinon.stub(component, 'setPreviousHighlighted');
+    component.state.showSuggestions = true;
+
+    component.onInputKeyDown({which: KeyCodes.UP});
+    assert.isTrue(showHideSuggestionsStub.called);
+  });
+
+  it('should reset input on ESC', () => {
+    const component = shallowBaseSelector().instance();
+    const onInputResetSpy = sinon.spy(component, 'onInputReset');
+
+    component.onInputKeyDown({which: KeyCodes.ESC});
+    assert.isTrue(onInputResetSpy.calledOnce);
+  });
+
+  it('should move to first suggestion on HOME', () => {
+    assertHomeEnd(KeyCodes.HOME, 'setFirstHighlighted');
+  });
+
+  it('should move to last suggestion on END', () => {
+    assertHomeEnd(KeyCodes.END, 'setLastHighlighted');
+  });
+
+  it('should select highlighted suggestion on ENTER', ()=> {
+    assertSuggestionSelected({which: KeyCodes.ENTER});
+  });
+
+  it('should select highlighted index on TAB', () => {
+    assertSuggestionSelected({which: KeyCodes.TAB})
+  });
+
+  it('should not select highlighted index on TAB when shouldSelectHighlightedOnTab', () => {
+    const component = shallowBaseSelector({shouldSelectHighlightedOnTab: false}).instance();
+    const onSuggestionSelectSpy = sinon.stub(component, 'onSuggestionSelect');
+
+    component.onInputKeyDown({which: KeyCodes.TAB});
+    assert.isFalse(onSuggestionSelectSpy.called);
+  });
+});
