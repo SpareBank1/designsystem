0d9e23be51dd79dfa4d8e3f6b39598a0e9850d5a
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2017-02-22 15:52:53 +0100

    Tests for account-details

--- src/account/account-details.test.js
+++ src/account/account-details.test.js
@@ -1,9 +1,9 @@
 import { shallow } from 'enzyme';
-import { assert, expect } from 'chai';
+import { assert } from 'chai';
 import React from 'react';
 import AccountDetails from './account-details';
 
-describe.skip('<AccountDetails />', () => {
+describe('<AccountDetails />', () => {
 
   const account = {
     id: 1,
@@ -12,26 +12,13 @@ describe.skip('<AccountDetails />', () => {
     balance: 1234567,
   };
 
-  it('should render root element', () => {
-    const wrapper = shallow(<AccountDetails account={account} locale="nb" />);
-    assert.equal(wrapper.find('.nfe-account-selector__selected-account-details').length, 1);
+  it('should render balance', () => {
+    const wrapper = shallow(<AccountDetails account={account} locale='nb' />);
+    assert.equal(wrapper.find('.account-details--right').length, 1);
   });
 
-  it('should render the accountnumber if passed', () => {
-    const wrapper = shallow(<AccountDetails account={account} locale="nb" />);
-    assert.equal(wrapper.find('.nfe-account-selector__selected-account-details--left').length, 1);
-    assert.equal(wrapper.find('.nfe-account-selector__selected-account-details--left').text(), '4201 02 05683');
+  it('should not render balance', () => {
+    const wrapper = shallow(<AccountDetails account={{...account, balance : null}} locale='nb' />);
+    assert.equal(wrapper.find('.account-details--right').length, 0);
   });
-
-  it('should render NOK as default currencyCode', () => {
-    const wrapper = shallow(<AccountDetails account={account} locale="nb" />);
-    expect(wrapper.find('.nfe-account-selector__selected-account-details--right').text()).to.contain('NOK');
-  });
-
-  it('should render currencyCode if supplied', () => {
-    const accountSek = {...account, currencyCode: 'SEK'};
-    const wrapper = shallow(<AccountDetails account={accountSek} locale="nb" />);
-    expect(wrapper.find('.nfe-account-selector__selected-account-details--right').text()).to.contain('SEK');
-  });
-
 });
