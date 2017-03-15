78d67131f1ce0ee129b603b82b92dacccd14b7f0
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2017-03-15 10:29:23 +0100

    Added index styles

--- src/account-selector.js
+++ src/account-selector.js
@@ -1,4 +1,4 @@
-import AccountSelector from './selectors/base-selector';
+import AccountSelector from './selectors/account-selector';
 import AccountSelectorMulti from './selectors/account-selector-multi';
 
 export {AccountSelectorMulti};
