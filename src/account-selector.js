7f4022687025c71003e0595051e670ab5b0cb951
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2017-03-15 10:43:22 +0100

    Rmoved account-selecor-multi

--- src/account-selector.js
+++ src/account-selector.js
@@ -1,5 +1,3 @@
 import AccountSelector from './selectors/account-selector';
-import AccountSelectorMulti from './selectors/account-selector-multi';
 
-export {AccountSelectorMulti};
 export default AccountSelector;
