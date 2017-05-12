fefa24fc27b61977b2e1064ed8f6b79e1864a0ae
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2017-05-12 09:30:46 +0200

    Babyproof the Heading class
    This commit makes sure you can't do stuff like
    `<Heading2 level=3 />`, and confuse yourself.

--- src/typography/Heading.js
+++ src/typography/Heading.js
@@ -3,50 +3,52 @@ import classNames from 'classnames';
 
 import { bool, oneOf, node, string } from 'prop-types';
 
-function Heading(props) {
-    const {
-        children,
-        className,
-        error,
-        noMargin,
-        withBorder,
-        lookLike,
-        level,
-        ...rest
-    } = props;
+const makeHeading = level => {
+    const Heading = (props) => {
+        const {
+            children,
+            className,
+            error,
+            noMargin,
+            withBorder,
+            lookLike,
+            ...rest
+        } = props;
 
-    const H = `h${level}`;
-    const headingClass = `ffe-h${lookLike ? lookLike : level}`;
+        const H = `h${level}`;
+        const headingClass = `ffe-h${lookLike ? lookLike : level}`;
 
-    return (
-        <H
-            className={classNames(
-                headingClass,
-                [`${headingClass}--error`]: error,
-                [`${headingClass}--no-margin`]: noMargin,
-                [`${headingClass}--with-border`]: withBorder,
-                className
-            )}
-            {...rest}
-        >
-            {children}
-        </H>
-    );
-}
+        return (
+            <H
+                {...rest}
+                className={classNames(
+                    headingClass,
+                    [`${headingClass}--error`]: error,
+                    [`${headingClass}--no-margin`]: noMargin,
+                    [`${headingClass}--with-border`]: withBorder,
+                    className
+                )}
+            >
+                {children}
+            </H>
+        );
+    };
 
-Heading.propTypes = {
-    children: node.isRequired,
-    className: string,
-    error: bool,
-    lookLike: oneOf([1, 2, 3, 4, 5, 6]),
-    level: oneOf([1, 2, 3, 4, 5, 6]),
-    noMargin: bool,
-    withBorder: bool,
+    Heading.propTypes = {
+        children: node.isRequired,
+        className: string,
+        error: bool,
+        lookLike: oneOf([1, 2, 3, 4, 5, 6]),
+        noMargin: bool,
+        withBorder: bool,
+    };
+
+    return Heading;
 };
 
-export const Heading1 = props => <Heading level={1} {...props} />;
-export const Heading2 = props => <Heading level={2} {...props} />;
-export const Heading3 = props => <Heading level={3} {...props} />;
-export const Heading4 = props => <Heading level={4} {...props} />;
-export const Heading5 = props => <Heading level={5} {...props} />;
-export const Heading6 = props => <Heading level={6} {...props} />;
+export const Heading1 = makeHeading(1);
+export const Heading2 = makeHeading(2);
+export const Heading3 = makeHeading(3);
+export const Heading4 = makeHeading(4);
+export const Heading5 = makeHeading(5);
+export const Heading6 = makeHeading(6);
