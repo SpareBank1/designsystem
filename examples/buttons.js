dc8d036d20cb1aea3c20c000dd25dcc9de5d84a3
Martin A. Midtsund <martin.midtsund@sparebank1.no>
2017-05-12 15:58:59 +0200

    Add npm start to run some live examples of the buttons

---
+++ examples/buttons.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './buttons.less';
+import {
+  ActionButton,
+  BackButton,
+  PrimaryButton,
+  SecondaryButton,
+  ShortcutButton,
+  TertiaryButton,
+} from '../src/index';
+
+const container = document.createElement('section');
+document.body.appendChild(container);
+
+function ButtonsExample() {
+  return (
+    <div>
+      <div className="ffe-content-container ffe-content-container--with-border">
+        <h1 className="ffe-h1">FFE Buttons</h1>
+      </div>
+      <div className="ffe-content-container">
+        <ActionButton>I'm an action button</ActionButton>
+      </div>
+      <div className="ffe-content-container">
+        <PrimaryButton>I'm a primary button</PrimaryButton>
+      </div>
+      <div className="ffe-content-container">
+        <SecondaryButton>I'm a secondary button</SecondaryButton>
+      </div>
+      <div className="ffe-content-container">
+        <ShortcutButton>I'm a shortcut button</ShortcutButton>
+      </div>
+      <div className="ffe-content-container">
+        <TertiaryButton>I'm a tertiary button</TertiaryButton>
+      </div>
+      <div className="ffe-content-container">
+        <BackButton>I'm a back button</BackButton>
+      </div>
+    </div>
+  );
+}
+
+render(<ButtonsExample />, container);
