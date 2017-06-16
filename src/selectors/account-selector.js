6d727d5752221d220250c3cdc31df98ce149793c
Torgeir Pedersen Cook <torgeir.pedersen.cook@sparebank1.no>
2017-06-16 10:17:01 +0200

    Disable account filter when account is selected. This makes it easier to change accounts after an account is selected because the whole list of suggestions is show.

--- src/selectors/account-selector.js
+++ src/selectors/account-selector.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { func, string, arrayOf} from 'prop-types';
+import { func, string, arrayOf } from 'prop-types';
 import BaseSelector from './base-selector';
 import AccountSuggestionItem from '../account/account-suggestion';
 import AccountNoMatch from '../account/account-nomatch';
 import AccountDetails from '../account/account-details';
-import {Account, Locale} from '../util/types';
-import {accountFilter} from '../filter/filters';
+import { Account, Locale } from '../util/types';
+import { createAccountFilter } from '../filter/filters';
 import classNames from 'classnames';
 
 function AccountSelector(props) {
@@ -30,7 +30,7 @@ function AccountSelector(props) {
         shouldSelectHighlightedOnTab={true}
         shouldHideSuggestionsOnBlur={true}
         shouldHideSuggestionsOnReset={false}
-        suggestionFilter={accountFilter}
+        suggestionFilter={createAccountFilter(selectedAccount)}
         onSelect={onAccountSelected}
         suggestions={accounts}
         {...props}
