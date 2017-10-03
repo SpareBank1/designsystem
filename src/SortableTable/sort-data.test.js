582964c624773c0e0d3504c3ef3eb2e5647ce866
Nicolai Hagen <nicolai.hagen@sparebank1.no>
2017-10-03 09:38:12 +0200

    added test-case for sorting on undefined attributes

--- src/SortableTable/sort-data.test.js
+++ src/SortableTable/sort-data.test.js
@@ -12,7 +12,8 @@ const data = [
     { name: 'Zombie Mountain', age: 36, id: 2, rand: '69 873' },
     { name: 'Daenerys Targaryen', age: 16, id: 4, rand: '9 156' },
     { name: 'Ned Stark', age: 48, id: 3, rand: '3 863 914' },
-    { name: 'Jon Snow', age: 20, id: 1, rand: '145 678' }
+    { name: 'Jon Snow', age: 20, id: 1, rand: '145 678' },
+    { name: 'Reek', age: 25, id: 19, rand:'322 222'}
 ];
 
 describe('sortData', () => {
@@ -35,8 +36,19 @@ describe('sortData', () => {
         const tableData = sortData(columns, data, 'age', true);
         expect(tableData[0].age).to.equal(48);
         expect(tableData[1].age).to.equal(36);
-        expect(tableData[2].age).to.equal(20);
-        expect(tableData[3].age).to.equal(16);
+        expect(tableData[2].age).to.equal(25);
+        expect(tableData[3].age).to.equal(20);
+        expect(tableData[4].age).to.equal(16);
+    });
+
+    it('should sort data even though an attribute is not set' , () => {
+        data[4] = { name: 'Reek', id: 19, rand:'322 222'}; // no age key
+        const tableData = sortData(columns, data, 'age', false);
+        expect(tableData[0].age).to.equal(16);
+        expect(tableData[1].age).to.equal(20);
+        expect(tableData[2].age).to.equal(36);
+        expect(tableData[3].age).to.equal(48);
+        expect(tableData[4].age).to.equal(undefined);
     });
 
     it('should sort data with a custom compare function when provided' , () => {
@@ -44,7 +56,8 @@ describe('sortData', () => {
         expect(tableData[0].rand).to.equal('9 156');
         expect(tableData[1].rand).to.equal('69 873');
         expect(tableData[2].rand).to.equal('145 678');
-        expect(tableData[3].rand).to.equal('3 863 914');
+        expect(tableData[3].rand).to.equal('322 222');
+        expect(tableData[4].rand).to.equal('3 863 914');
     });
 
 });
