064340569d07982a98a1589718468141e6b9f31c
Henrik Hermansen <henrik.hermansen@sparebank1.no>
2017-10-11 17:37:05 +0200

    Replace flow.sh with buildCI.sh.

---
+++ buildCI.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+main() {
+    npm install
+    npm test
+    npm run build
+
+    if should_publish; then
+        npm run has-published -s || npm publish
+    fi
+}
+
+function should_publish() {
+    [[ $GIT_BRANCH =~ ^(origin/)?master$ ]]
+}
+
+main "$@"
