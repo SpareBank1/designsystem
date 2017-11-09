d6abefe329d7f001508e771a3dce616ecdbc32e8
Ole Espen Lundsør <ole.espen.lundsor@sparebank1.no>
2017-11-09 09:17:09 +0100

    - Upgraded to react 16. - Upgraded Enzyme to v.3 to support react 16. - Added repository text to remove warning. - Upgraded ffe-accordian to v.3

--- src/accordion.test.js
+++ src/accordion.test.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-duplicate-imports */
 /*eslint-env mocha*/
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
 import { assert } from 'chai';
 import { whiteAccordion, blueAccordion } from '../docs/example-component';
 import { AccordionItem, WhiteAccordion } from '../src/';
@@ -6,6 +9,7 @@ import { shallow, render } from 'enzyme';
 import React from 'react';
 import sinon from 'sinon';
 
+Enzyme.configure({ adapter: new Adapter() });
 const wrapperHasContent = (wrapper, content) => new RegExp(content).test(wrapper.html());
 
 describe('ffe-accordion-react', () => {
