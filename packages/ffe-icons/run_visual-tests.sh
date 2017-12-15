fcf2b02bf5fc2ec69b8f2ab1e5e530b50dca082a
Kristofer Walters <kristofer.walters@sparebank1.no>
2017-12-15 15:34:45 +0100

    chore(ffe-icons): import ffe-icons into monorepo

--- packages/ffe-icons/run_visual-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-source node_modules/ffe-visual-tests-support/run_gemini_command.lib
-
-rm -rf gemini-report/
-
-run_gemini_command test  --reporter html --reporter flat
--- packages/ffe-icons/visual-tests/specs/test.js
+++
@@ -1,8 +0,0 @@
-gemini.suite('dist', function (suite) {
-    suite.setUrl('/sprite.symbol.html')
-        .setCaptureElements('section ul')
-        .before(function (actions) {
-            actions.wait(1000);
-        })
-        .capture('plain');
-});
