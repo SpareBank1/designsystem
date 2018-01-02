82b18480bebc23d2647034b50ded397b12fd1cb7
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-01-02 14:27:47 +0100

    chore(ffe-cards): Import ffe-cards

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,6 +8,7 @@ module.exports = {
           'stylelint-config-ffe',
           'ffe-buttons',
           'ffe-buttons-react',
+          'ffe-cards',
           'ffe-core',
           'ffe-core-react',
           'ffe-form',
--- packages/ffe-cards/update_visual-tests-baselines.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-source node_modules/ffe-visual-tests-support/run_gemini_command.lib
-
-
-run_gemini_command update
