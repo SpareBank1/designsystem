d528d8972ee0a5cdb6c6e55ac3ea6d420ee8f75d
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-01-03 13:06:44 +0100

    chore(ffe-chart-donut-react): Use Jest for testing
    This commit fixes the broken test setup for ffe-chart-donut-react
    and switches it over to use Jest for testing

---
+++ packages/ffe-chart-donut-react/src/ChartDonut.spec.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ChartDonut from '../src';
+
+const defaultProps = {
+    firstLabel: 'Foo',
+    lastLabel: 'Bar',
+    name: 'Baz',
+    percentage: 42,
+};
+
+const renderShallow = (props = {}) => shallow(<ChartDonut {...defaultProps} {...props} />);
+
+describe('ChartDonut', () => {
+    it('renders two SVGs', () => {
+        const el = renderShallow();
+
+        expect(el.find('svg')).toHaveLength(2);
+    });
+
+    it('renders html for the name of the circle', () => {
+        const el = renderShallow();
+
+        expect(el.find('.ffe-chart-donut__name')).toHaveLength(1);
+        expect(el.find('.ffe-chart-donut__name').text()).toBe('Baz');
+    });
+
+    it('renders html for the labels and their percentages', () => {
+        const el = renderShallow();
+
+        expect(el.find('.ffe-chart-donut__type')).toHaveLength(2);
+        expect(el.find('.ffe-chart-donut__amount')).toHaveLength(2);
+
+        expect(el.find('.ffe-chart-donut__type').at(0).text()).toBe('Foo');
+        expect(el.find('.ffe-chart-donut__amount').at(0).text()).toMatch(/58/);
+
+        expect(el.find('.ffe-chart-donut__type').at(1).text()).toBe('Bar');
+        expect(el.find('.ffe-chart-donut__amount').at(1).text()).toMatch(/42/);
+    });
+});
