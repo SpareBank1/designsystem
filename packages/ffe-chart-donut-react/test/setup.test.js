08eefaeca46615ad4d842d2e39e0ff0140e53f4f
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-01-03 13:06:44 +0100

    chore(ffe-chart-donut-react): Use Jest for testing
    This commit fixes the broken test setup for ffe-chart-donut-react
    and switches it over to use Jest for testing

--- packages/ffe-chart-donut-react/test/setup.test.js
+++
@@ -1,8 +0,0 @@
-import 'babel-polyfill';
-import jsdom from 'jsdom';
-import expectjs from 'expect.js';
-
-global.document = jsdom.jsdom('');
-global.window = document.defaultView;
-global.navigator = window.navigator;
-global.expect = expectjs;
