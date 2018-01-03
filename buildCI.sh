b703da4bbf9aaa5f7fd4654c43dfb1c03146ab53
Kristofer Walters <kristofer.walters@sparebank1.no>
2018-01-03 16:16:14 +0100

    chore: Don't run lerna publish when not on master
    Lerna is configured to fail if the current branch is anything other than
    master (`allowBranch` setting in lerna.json). This causes CI jobs on
    feature branches to fail.

--- buildCI.sh
+++ buildCI.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 
+function is_master_branch() {
+    [[ $GIT_BRANCH =~ ^(origin/)?master$ ]]
+}
+
 git clean -f -x -d
 npm install
 npm run build
 npm test
-./node_modules/.bin/lerna publish --yes \
-	--registry https://nexus.intern.sparebank1.no/nexus/content/repositories/npm-internal
+
+if is_master_branch; then
+    ./node_modules/.bin/lerna publish --yes \
+        --registry https://nexus.intern.sparebank1.no/nexus/content/repositories/npm-internal
+fi
