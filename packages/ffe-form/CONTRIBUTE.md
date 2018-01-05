93b39b531a44d73e7e1a8bcc43381ee6ea088c33
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-01-05 13:59:01 +0100

    chore(ffe-form): Import ffe-form

--- packages/ffe-form/CONTRIBUTE.md
+++
@@ -1,3 +0,0 @@
-# Contribute
-
-Se [ffe-core/CONTRIBUTE.md](https://stash.intern.sparebank1.no/projects/FFE/repos/ffe-core/browse/CONTRIBUTE.md)
--- packages/ffe-form/run_visual-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-source node_modules/ffe-visual-tests-support/run_gemini_command.lib
-
-rm -rf gemini-report/
-
-run_gemini_command test  --reporter html --reporter flat
