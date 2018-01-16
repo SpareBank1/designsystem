46898a47f6288f2cad6f61ffc5cb2fc7c1ab1131
Kristofer Walters <kristofer.walters@sparebank1.no>
2018-01-16 10:49:10 +0100

    fix: Build doesn't test for remote git changes before publishing anymore
    This check seems not to work on our jenkins.

--- buildCI.sh
+++ buildCI.sh
@@ -4,17 +4,12 @@ function is_master_branch() {
     [[ $GIT_BRANCH =~ ^(origin/)?master$ ]]
 }
 
-function is_uptodate() {
-    git fetch --quiet
-    ! [[ $(git log HEAD..origin/master --oneline) ]]
-}
-
 git clean -f -x -d
 npm install
 npm run build
 npm test
 
-if is_master_branch && is_uptodate; then
+if is_master_branch; then
     ./node_modules/.bin/lerna publish --yes \
         --registry ***REMOVED***
 fi
