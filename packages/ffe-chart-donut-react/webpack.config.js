66525aef450b2884682a6540478c030dc6e2011e
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-01-18 18:31:40 +0100

    chore: Remove package examples
    This commit removes the examples and dev servers found for each of the
    packages in the monorepo. All development will be done inside of
    styleguidist in the future, and these files can therefore be deleted
    safely.

--- packages/ffe-chart-donut-react/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const getConfig = require('hjs-webpack');
-
-module.exports = getConfig({
-    in: 'dev/app.js',
-
-    out: 'public',
-
-    clearBeforeBuild: true,
-});
--- packages/ffe-checkbox-react/docs/example-component.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Checkbox from '../src/Checkbox';
-
-export const MainExample = (
-    <Checkbox checked={false} name="Yellow" label="Yellow" />
-);
-
-export default props => <Checkbox {...props} />;
