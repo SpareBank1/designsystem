2c164280428791ae97c37ea8462417282a4dba0c
Kristofer Walters <kristofer.walters@sparebank1.no>
2018-01-19 16:05:24 +0100

    chore(ffe-decorators-react): Use hoisted dependencies

--- packages/ffe-decorators-react/src/ease-properties/easeProperties.spec.js
+++ packages/ffe-decorators-react/src/ease-properties/easeProperties.spec.js
@@ -15,8 +15,8 @@ describe('easeProperties decorator', () => {
     beforeEach(() => {
         window.requestAnimationFrame = f => f;
         window.cancelAnimationFrame = f => f;
-        sinon.stub(window, 'requestAnimationFrame', rafMock.raf);
-        sinon.stub(window, 'cancelAnimationFrame', rafMock.cancel);
+        sinon.stub(window, 'requestAnimationFrame').callsFake(rafMock.raf);
+        sinon.stub(window, 'cancelAnimationFrame').callsFake(rafMock.cancel);
     });
 
     afterEach(() => {
