d84187eae9110bdfe5a4dc7d736ae843e102879d
Ann Katrin Gagnat <ann.katrin.gagnat@sparebank1.no>
2018-02-01 10:33:08 +0100

    feat(ffe-searchable-dropdown-react): Add support for limiting number of rendered dropdown elements Set property `maxRenderedDropdownElements` to the maximum number of elements you wish to render at any time. Useful when `dropdownList` is large in order to avoid lag.

--- packages/ffe-searchable-dropdown-react/src/ListItem.test.js
+++ packages/ffe-searchable-dropdown-react/src/ListItem.test.js
@@ -6,76 +6,93 @@ const i = t => t;
 
 const renderItem = company => (
     <div>
-        <div className="custom-render-function">
-            {company.organizationName}
-        </div>
+        <div className="custom-render-function">{company.organizationName}</div>
     </div>
 );
 
-const mountListItemProps = props => shallow(<ListItem {...props}/>);
+const mountListItemProps = props => shallow(<ListItem {...props} />);
 
 describe('<Input>', () => {
     const props = {
-        id:1,
-        key:1,
+        id: 1,
+        key: 1,
         isHighlighted: false,
-        item:{
-            organizationName: "Bedriften",
-            organizationNumber: "912602370",
+        item: {
+            organizationName: 'Bedriften',
+            organizationNumber: '912602370',
         },
-        onSelect:i,
-        dropdownAttributes:["organizationName", "organizationNumber"],
+        onSelect: i,
+        dropdownAttributes: ['organizationName', 'organizationNumber'],
         refHighlightedListItem: i,
     };
 
     const component = mountListItemProps(props);
 
-    test('renders', () =>
-        expect(component.find('.ffe-searchable-dropdown__item')).toHaveLength(1));
+    it('renders', () =>
+        expect(component.find('.ffe-searchable-dropdown__item')).toHaveLength(
+            1,
+        ));
 
-    test('renders custom renderElement function when defined', () => {
+    it('renders custom renderElement function when defined', () => {
         const customRenderProps = {
-            id:1,
-            key:1,
+            id: 1,
+            key: 1,
             isHighlighted: false,
-            item:{
-                organizationName: "Bedriften",
-                organizationNumber: "912602370",
+            item: {
+                organizationName: 'Bedriften',
+                organizationNumber: '912602370',
             },
-            onSelect:i,
+            onSelect: i,
             renderElement: renderItem,
-            dropdownAttributes:["organizationName", "organizationNumber"],
+            dropdownAttributes: ['organizationName', 'organizationNumber'],
             refHighlightedListItem: i,
         };
         const customRenderItem = mountListItemProps(customRenderProps);
 
-        expect(customRenderItem.find('.ffe-searchable-dropdown__item--header')).toHaveLength(0);
-        expect(customRenderItem.find('.custom-render-function')).toHaveLength(1);
+        expect(
+            customRenderItem.find('.ffe-searchable-dropdown__item--header'),
+        ).toHaveLength(0);
+        expect(customRenderItem.find('.custom-render-function')).toHaveLength(
+            1,
+        );
     });
 
     describe('renders default case when renderElement is not defined', () => {
-
-        test('including dropdown details when dropdownAttrs > 1', () => {
-            expect(component.find('.ffe-searchable-dropdown__item--header')).toHaveLength(1);
-            expect(component.find('.ffe-searchable-dropdown__item--details')).toHaveLength(1);
+        it('including dropdown details when dropdownAttrs > 1', () => {
+            expect(
+                component.find('.ffe-searchable-dropdown__item--header'),
+            ).toHaveLength(1);
+            expect(
+                component.find('.ffe-searchable-dropdown__item--details'),
+            ).toHaveLength(1);
         });
 
-        test('does not render dropdown details when dropdownAttrs === 1', () => {
+        it('does not render dropdown details when dropdownAttrs === 1', () => {
             const singleDropdownAttrProps = {
-                id:1,
-                key:1,
+                id: 1,
+                key: 1,
                 isHighlighted: false,
-                item:{
-                    organizationName: "Bedriften",
-                    organizationNumber: "912602370",
+                item: {
+                    organizationName: 'Bedriften',
+                    organizationNumber: '912602370',
                 },
-                onSelect:i,
-                dropdownAttributes:["organizationName"],
+                onSelect: i,
+                dropdownAttributes: ['organizationName'],
                 refHighlightedListItem: i,
             };
-            const componentWithSingelDropdownAttrsItem = mountListItemProps(singleDropdownAttrProps);
-            expect(componentWithSingelDropdownAttrsItem.find('.ffe-searchable-dropdown__item--header')).toHaveLength(1);
-            expect(componentWithSingelDropdownAttrsItem.find('.ffe-searchable-dropdown__item--details')).toHaveLength(0);
+            const componentWithSingelDropdownAttrsItem = mountListItemProps(
+                singleDropdownAttrProps,
+            );
+            expect(
+                componentWithSingelDropdownAttrsItem.find(
+                    '.ffe-searchable-dropdown__item--header',
+                ),
+            ).toHaveLength(1);
+            expect(
+                componentWithSingelDropdownAttrsItem.find(
+                    '.ffe-searchable-dropdown__item--details',
+                ),
+            ).toHaveLength(0);
         });
     });
 });
