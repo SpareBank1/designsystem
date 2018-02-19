354f4984eea5454d3b2f1ee7aeced436685ddfff
Anders Johnsen <anders.johnsen@sparebank1.no>
2018-02-19 10:24:01 +0100

    feat: Restructure styleguide contents in order to avoid using styleguidist to render everything

---
+++ src/templates/helpers.js
@@ -0,0 +1,10 @@
+const Handlebars = require('handlebars');
+
+Handlebars.registerHelper('increment', value => value + 1);
+
+Handlebars.registerHelper('preformat', obj => {
+    const str = JSON.stringify(obj, null, 2);
+    return new Handlebars.SafeString(
+        `<pre>${Handlebars.escapeExpression(str)}</pre>`
+    );
+});
