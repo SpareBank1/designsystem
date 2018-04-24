5c9b4861ba3f7d39877848755f21df4e1a499124
Henrik Hermansen <henrik.hermansen@sparebank1.no>
2018-04-24 13:08:03 +0200

    feat(ffe-tabs-react): add props thin and className to TabGroup
    This commit adds two new props to the `TabGroup` component.
    Bool `thin` adds the `--thin` modifier to remove top and bottom padding.
    String `className` adds given css classes to the tab group.

--- packages/ffe-tabs-react/src/TabGroup.spec.js
+++ packages/ffe-tabs-react/src/TabGroup.spec.js
@@ -24,4 +24,22 @@ describe('TabGroup', () => {
         expect(wrapper.contains(<Tab>En tab</Tab>)).toBe(true);
         expect(wrapper.contains(<Tab>En annen tab</Tab>)).toBe(true);
     });
+
+    it('should apply thin modifier class when the thin prop is true', () => {
+        const wrapper = shallow(
+            <TabGroup thin={true}>
+                <Tab>En tab</Tab>
+            </TabGroup>,
+        );
+        expect(wrapper.hasClass('ffe-tab-button-group--thin')).toBe(true);
+    });
+
+    it('should accept custom classes', () => {
+        const wrapper = shallow(
+            <TabGroup className="some-custom-class">
+                <Tab>En tab</Tab>
+            </TabGroup>,
+        );
+        expect(wrapper.hasClass('some-custom-class')).toBe(true);
+    });
 });
