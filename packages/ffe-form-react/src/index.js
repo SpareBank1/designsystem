98620cebb1891aff02b41d576eeff2320cdef7c3
Ivar Nilsen <ivar.nilsen@sparebank1.no>
2018-11-29 13:37:42 +0100

    feat(ffe-form-react): New components
    The following components are now available from this package:
    
    - Checkbox
    - RadioBlock
    - RadioButton
    - RadioButtonInputGroup
    - RadioSwitch

--- packages/ffe-form-react/src/index.js
+++ packages/ffe-form-react/src/index.js
@@ -1,14 +1,20 @@
+import Checkbox from './Checkbox';
 import ErrorFieldMessage from './ErrorFieldMessage';
 import InfoFieldMessage from './InfoFieldMessage';
 import SuccessFieldMessage from './SuccessFieldMessage';
 import Input from './Input';
 import InputGroup from './InputGroup';
 import Label from './Label';
+import PhoneNumber from './PhoneNumber';
+import RadioBlock from './RadioBlock';
+import RadioButton from './RadioButton';
+import RadioButtonInputGroup from './RadioButtonInputGroup';
+import RadioSwitch from './RadioSwitch';
 import TextArea from './TextArea';
 import Tooltip from './Tooltip';
-import PhoneNumber from './PhoneNumber';
 
 export {
+    Checkbox,
     ErrorFieldMessage,
     InfoFieldMessage,
     SuccessFieldMessage,
@@ -16,6 +22,10 @@ export {
     PhoneNumber,
     InputGroup,
     Label,
+    RadioBlock,
+    RadioButton,
+    RadioButtonInputGroup,
+    RadioSwitch,
     TextArea,
     Tooltip,
 };
