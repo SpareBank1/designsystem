e3aeaef8c1786b1fd993eed61f164bc4648360be
Torkjel Skjetne <3830142+torkjels@users.noreply.github.com>
2020-09-28 11:45:12 +0200

    feat(ffe-context-message-react): add role="alert" to error message
    This allows screen reader users to see an error message immediately when it appears, in the same way that a user not using a screen reader would see it immediately.

--- packages/ffe-context-message-react/src/ContextErrorMessage.js
+++ packages/ffe-context-message-react/src/ContextErrorMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ContextMessage from './ContextMessage';
 
 const ContextErrorMessage = props => (
-    <ContextMessage messageType="error" {...props} />
+    <ContextMessage messageType="error" role="alert" {...props} />
 );
 
 export default ContextErrorMessage;
