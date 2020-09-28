ff6ea6cd367c377eb538e959f9ba6fff88230ac7
Torkjel Skjetne <3830142+torkjels@users.noreply.github.com>
2020-09-28 08:56:36 +0200

    feat(ffe-form-react): add role="alert" to ErrorFieldMessage
    This allows screen reader users to see a form error message immediately when it appears, in the same way that a user not using a screen reader would see it immediately.

--- packages/ffe-form-react/src/ErrorFieldMessage.js
+++ packages/ffe-form-react/src/ErrorFieldMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BaseFieldMessage from './BaseFieldMessage';
 
 const ErrorFieldMessage = props => {
-    return <BaseFieldMessage {...props} type="error" />;
+    return <BaseFieldMessage {...props} type="error" role="alert" />;
 };
 
 export default ErrorFieldMessage;
