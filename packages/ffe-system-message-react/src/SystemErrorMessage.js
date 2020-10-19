0469f5e2290c4f8fdfcaad00e182d6cf83399610
Hege Daljord Morken <hege.daljord.morken@sparebank1.no>
2020-10-19 14:29:09 +0200

    fix(ffe-system-message-react): optout param for alert for error message
    Last release added alert role for all error messages. This release adds an opt-out parameter alert.

--- packages/ffe-system-message-react/src/SystemErrorMessage.js
+++ packages/ffe-system-message-react/src/SystemErrorMessage.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import { func, string, number, node, oneOf, bool } from 'prop-types';
 import UtropstegnIkon from '@sb1/ffe-icons-react/lib/utropstegn-ikon';
 
 import SystemMessage from './SystemMessage';
 
 export default function SystemErrorMessage(props) {
+    const { alert, ...rest } = props;
+
     return (
         <SystemMessage
             modifier="error"
             icon={<UtropstegnIkon aria-hidden="true" />}
-            role="alert"
-            {...props}
+            role={alert ? 'alert' : false}
+            {...rest}
         />
     );
 }
+
+SystemErrorMessage.defaultProps = {
+    animationLengthMs: 300,
+    locale: 'nb',
+    onClose: f => f,
+    alert: true,
+};
+
+SystemErrorMessage.propTypes = {
+    animationLengthMs: number,
+    /** The content of the system message */
+    children: node.isRequired,
+    /** Additional classes added to the surrounding div */
+    className: string,
+    /** Override the default icon - use with caution! */
+    icon: node.isRequired,
+    /** 'nb', 'nn', or 'en' */
+    locale: oneOf(['en', 'nb', 'nn']),
+    /**
+     * The type of system message. Used internally only-
+     * @ignore
+     **/
+    modifier: oneOf(['error', 'info', 'success', 'news']),
+    /** Callback for when the system message has been closed (after animation ends) */
+    onClose: func,
+    /** When false, role is not set to alert, avoids message from being read up immediately after page load. Default value is true. */
+    alert: bool,
+};
