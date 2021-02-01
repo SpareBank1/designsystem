b3c3fd994c119d4d0e7da21f2c601c128fe14acb
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2021-02-01 16:49:26 +0100

    fix(ffe-datepicker-react): chrashes without language

--- packages/ffe-datepicker-react/src/input/Input.js
+++ packages/ffe-datepicker-react/src/input/Input.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { bool, func, oneOfType, string, shape } from 'prop-types';
+import { bool, func, oneOfType, string, shape, oneOf } from 'prop-types';
 import classNames from 'classnames';
 import i18n from '../i18n/i18n';
 
@@ -20,7 +20,7 @@ export default class Input extends Component {
             onChange,
             onKeyDown,
             value,
-            language,
+            language = 'nb',
         } = this.props;
 
         return (
@@ -53,5 +53,5 @@ Input.propTypes = {
     onKeyDown: func,
     value: string.isRequired,
     fullWidth: bool,
-    language: string,
+    language: oneOf(['nb', 'nn', 'en']),
 };
