cacf08a2e4ab5b9fa65f6e6eb55ae3e7b63ddee5
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2021-02-05 09:04:57 +0100

    fix(ffe-collapse-react): fix ts

--- packages/ffe-collapse-react/src/index.d.ts
+++ packages/ffe-collapse-react/src/index.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-interface CollapseProps extends React.HTMLProps<HTMLDivElement> {
+export interface CollapseProps extends React.HTMLProps<HTMLDivElement> {
     isOpen: boolean;
     onRest?: () => void;
 }
