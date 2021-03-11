17c9da94a0cb9442e6b07c65de643146dae32c13
Kristofer Walters <kristofer.walters@sparebank1.no>
2021-03-11 10:11:13 +0100

    feat(designsystem): set up build system
    I'm kind of proud of this Makefile.

---
+++ designsystem/scripts/lib/transformExampleCode.js
@@ -0,0 +1,78 @@
+const acorn = require('acorn');
+const acornJsx = require('acorn-jsx');
+const walk = require('estree-walker').walk;
+
+const getAst = src =>
+    acorn.Parser.extend(acornJsx()).parse(src, {
+        ecmaVersion: '2020',
+        sourceType: 'module',
+    });
+
+const getImportSpecifiers = node => node.specifiers.map(n => n.local.name);
+
+const removePositions = (sourceCode, importPositions) => {
+    const compare = (a, b) => b[0] - a[0];
+    const reducer = (code, pos) => code.slice(0, pos[0]) + code.slice(pos[1]);
+
+    return importPositions
+        .sort(compare)
+        .reduce(reducer, sourceCode)
+        .trim();
+};
+
+const generateModuleCode = ({
+    code,
+    imports,
+    importSpecifiers,
+    sourceCode,
+    sourceFileName,
+    dependencies,
+}) =>
+    `${imports.join('\n')}
+export const scope = {${importSpecifiers.join(',')}};
+export const code = ${JSON.stringify(code)};
+export const sourceCode = ${JSON.stringify(sourceCode)};
+export const sourceImports = ${JSON.stringify(imports.join('\n'))};
+export const dependencies = ${JSON.stringify(dependencies)};
+export const sourceFileName = ${JSON.stringify(sourceFileName)};`;
+
+module.exports = function(sourceCode, sourceFileName) {
+    let importSpecifiers = [];
+    const importPositions = [];
+    const imports = [];
+    const dependencies = [];
+
+    let ast;
+    try {
+        ast = getAst(sourceCode);
+    } catch (err) {
+        if (err instanceof SyntaxError) {
+            console.error(
+                `\nSyntaxError: ${sourceFileName}: ${err.message}\n\n`,
+            );
+        }
+        throw err;
+    }
+
+    walk(ast, {
+        enter(node) {
+            if (node.type === 'ImportDeclaration') {
+                importSpecifiers = importSpecifiers.concat(
+                    getImportSpecifiers(node),
+                );
+                imports.push(sourceCode.slice(node.start, node.end));
+                importPositions.push([node.start, node.end]);
+                dependencies.push(node.source.value);
+            }
+        },
+    });
+
+    return generateModuleCode({
+        code: removePositions(sourceCode, importPositions),
+        imports,
+        importSpecifiers,
+        sourceCode,
+        sourceFileName,
+        dependencies,
+    });
+};
