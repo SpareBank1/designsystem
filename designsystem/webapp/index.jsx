8e08102f881e66f344fa65ae31883cf083ba412e
Kristofer Walters <kristofer.walters@sparebank1.no>
2021-03-26 15:03:34 +0100

    feat(designsystem): add a11y checking with axe to examples

--- designsystem/webapp/index.jsx
+++ designsystem/webapp/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import axe from '@axe-core/react';
 
 import '@designsystem/style.css';
 import '@sb1/ffe-webfonts/sb1-fonts.css';
 import './index.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+axe(React, ReactDOM, 1000);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
