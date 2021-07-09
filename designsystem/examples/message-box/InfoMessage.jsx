b79fd21ce2e7da8afe5df5d5ea51c54a87672359
HeleneKassandra <hjuterud@gmail.com>
2021-07-09 15:31:21 +0200

    fix(designsystem): remove paragraph tag from messagebox examples

--- designsystem/examples/message-box/InfoMessage.jsx
+++ designsystem/examples/message-box/InfoMessage.jsx
@@ -1,9 +1,6 @@
 import { InfoMessage } from '@sb1/ffe-message-box-react';
-import { Paragraph } from '@sb1/ffe-core-react';
 
 <InfoMessage title="Du har ingen kort">
-    <Paragraph>
-        Denne tjenesten er kun tilgjengelig for deg med et debitkort eller
-        kredittkort fra SpareBank 1.
-    </Paragraph>
+    Denne tjenesten er kun tilgjengelig for deg med et debitkort eller
+    kredittkort fra SpareBank 1.
 </InfoMessage>
