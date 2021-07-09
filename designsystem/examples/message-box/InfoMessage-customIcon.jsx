1d612982879184837b379bb698c9593d2066ee55
HeleneKassandra <hjuterud@gmail.com>
2021-07-09 15:31:21 +0200

    fix(designsystem): remove paragraph tag from messagebox examples

--- designsystem/examples/message-box/InfoMessage-customIcon.jsx
+++ designsystem/examples/message-box/InfoMessage-customIcon.jsx
@@ -3,8 +3,6 @@ import { Paragraph } from '@sb1/ffe-core-react';
 import { HandlevognIkon } from '@sb1/ffe-icons-react';
 
 <InfoMessage title="Handlevognen din er tom" icon={<HandlevognIkon/>}>
-    <Paragraph>
-        Du har ingenting i handlevognen din.
-    </Paragraph>
+    Du har ingenting i handlevognen din.
 </InfoMessage>;
 
