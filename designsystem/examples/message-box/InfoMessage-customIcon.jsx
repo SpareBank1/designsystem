3daa0a752552424a4d7c3af4b4bcb5aadad66734
Kristofer Walters <kristofer.walters@sparebank1.no>
2021-08-23 15:25:39 +0200

    fix(designsystem): revert "Update message box styling (#1152)"
    This reverts parts of commit ef58fc8.

--- designsystem/examples/message-box/InfoMessage-customIcon.jsx
+++ designsystem/examples/message-box/InfoMessage-customIcon.jsx
@@ -1,7 +1,10 @@
 import { InfoMessage } from '@sb1/ffe-message-box-react';
+import { Paragraph } from '@sb1/ffe-core-react';
 import { HandlevognIkon } from '@sb1/ffe-icons-react';
 
 <InfoMessage title="Handlevognen din er tom" icon={<HandlevognIkon/>}>
-    Du har ingenting i handlevognen din.
+    <Paragraph>
+        Du har ingenting i handlevognen din.
+    </Paragraph>
 </InfoMessage>;
 
