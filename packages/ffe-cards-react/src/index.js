0a86013faf1f343bbdb2e98ea58e805ab12c51b2
HeleneKassandra <hjuterud@gmail.com>
2021-09-10 14:16:31 +0200

    feat(ffe-cards-react): fjern deprecated link og product card
    BREAKING CHANGE: LinkCard og ProductCard har vært deprecated i flere år,
    nå fjernes de helt fra koden

--- packages/ffe-cards-react/src/index.js
+++ packages/ffe-cards-react/src/index.js
@@ -1,5 +1,3 @@
-export { default as LinkCard } from './LinkCard';
-export { default as ProductCard } from './ProductCard';
 export { default as CardBase } from './CardBase';
 export { default as TextCard } from './TextCard/TextCard';
 export { default as IconCard } from './IconCard/IconCard';
