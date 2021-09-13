cce16f9b9fca4371eac7845fa3531b002c8f1579
Kristofer Walters <kristofer.walters@sparebank1.no>
2021-09-13 14:11:34 +0200

    feat(ffe-core): add design tokens as css

---
+++ packages/ffe-core/scripts/build.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const path = require('path');
+
+const writeToFile = require('./lib/writeToFile');
+const renderLessVarsToCSSProps = require('./lib/renderLessVarsToCSSProps');
+const extractCustomProps = require('./lib/extractCustomProps');
+const { genTSSource, genTSModIndex } = require('./lib/genTypeScript');
+
+const configFilePath = process.argv[2];
+
+if (!configFilePath) {
+    console.error('Usage: build.js path/to/build.config.js');
+    process.exit(1);
+}
+
+const config = require(path.resolve(configFilePath));
+const lessFiles = config.sources.map(p => path.resolve(p));
+
+const basename = fname => path.basename(fname, '.less');
+const cssFilePathFor = fname =>
+    path.join(config.output.css, `${basename(fname)}.css`);
+const tsFilePathFor = fname =>
+    path.join(config.output.typescript, `${basename(fname)}.ts`);
+
+const pipeline = lessFile =>
+    renderLessVarsToCSSProps(lessFile)
+        .then(writeToFile(cssFilePathFor(lessFile)))
+        .then(extractCustomProps)
+        .then(genTSSource)
+        .then(writeToFile(tsFilePathFor(lessFile)));
+
+Promise.all(lessFiles.map(pipeline))
+    .then(() => genTSModIndex(lessFiles.map(basename)))
+    .then(writeToFile(path.join(config.output.typescript, 'index.ts')))
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
---
+++ packages/ffe-core/tokens.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    sources: [
+        'less/breakpoints.less',
+        'less/colors.less',
+        'less/dimensions.less',
+        'less/motion.less',
+        'less/spacing.less',
+    ],
+    output: {
+        css: 'css',
+        typescript: 'gen-src',
+    },
+};
