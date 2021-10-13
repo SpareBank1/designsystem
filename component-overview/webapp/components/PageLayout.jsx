9d1d766dc1497cb3abce32078a6da988e8ac641a
HeleneKassandra <hjuterud@gmail.com>
2021-10-13 15:14:17 +0200

    feat(component-overview): add darkmode toggling

--- component-overview/webapp/components/PageLayout.jsx
+++ component-overview/webapp/components/PageLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import DarkmodeButton from './DarkmodeButton';
+// eslint-disable-next-line react/prop-types
 export default function PageLayout({ title, children }) {
     return (
         <>
             <header className="sb1ex-header">
                 <h1 className="sb1ex-header__headline">{title}</h1>
+                <DarkmodeButton />
             </header>
             <main className="sb1ex-main">{children}</main>
         </>
