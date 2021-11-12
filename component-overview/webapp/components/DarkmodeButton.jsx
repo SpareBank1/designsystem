3c4c1b70d4a0752464f88ff872da5dc12664b29f
Kristofer Walters <kris@wltrs.org>
2021-11-12 12:41:02 +0100

    chore(component-overview): fix import path of darkmode button

--- component-overview/webapp/components/DarkmodeButton.jsx
+++ component-overview/webapp/components/DarkmodeButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import useDarkColorSchemePreference from '../../util/useDarkColorSchemePreference.js';
+import useDarkColorSchemePreference from '../util/useDarkColorSchemePreference.js';
 import '../styles/sb1ds-darkmode-button.css';
 export default function DarkmodeButton() {
     const { prefersDarkMode, toggleDarkMode } = useDarkColorSchemePreference();
