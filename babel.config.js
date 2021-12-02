f8c924a93806d2ca5e2388d8ae38baf80b5eba39
Kristofer Walters <kristofer.walters@sparebank1.no>
2021-12-02 21:25:54 +0100

    chore: upgrade babel and remove unused babel plugins

--- babel.config.js
+++ babel.config.js
@@ -9,10 +9,4 @@ module.exports = api => ({
         ],
         '@babel/react',
     ],
-    plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-syntax-import-meta',
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-        '@babel/plugin-proposal-json-strings',
-    ],
 });
