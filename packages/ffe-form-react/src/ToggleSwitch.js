e41465e9f7dfb2a920839e926db30cb3487b0748
Anders <anders.johnsen@gmail.com>
2022-01-06 10:36:15 +0100

    feat(ffe-form-react): ny toggleswitch-komponent

---
+++ packages/ffe-form-react/src/ToggleSwitch.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { bool, node, string } from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import classNames from 'classnames';
+
+class ToggleSwitch extends React.Component {
+    id = `toggle-${uuidv4()}`;
+
+    render() {
+        const { children, className, alignRight, id, ...rest } = this.props;
+
+        return (
+            <div
+                className={classNames(
+                    'ffe-toggle-switch',
+                    { 'ffe-toggle-switch--align-right': alignRight },
+                    className,
+                )}
+            >
+                <input
+                    className="ffe-toggle-switch__input"
+                    type="checkbox"
+                    id={id || this.id}
+                    {...rest}
+                />
+                <label
+                    className="ffe-toggle-switch__label"
+                    htmlFor={id || this.id}
+                >
+                    {children}
+                </label>
+            </div>
+        );
+    }
+}
+
+ToggleSwitch.propTypes = {
+    /** The label text */
+    children: node.isRequired,
+    /** Any extra classes */
+    className: string,
+    /** Overrides the generated id with a custom one */
+    id: string,
+    /** Overrides the value attribute of the input with a custom one */
+    value: string,
+    /** Aligns the switch to the right */
+    alignRight: bool,
+};
+
+ToggleSwitch.defaultProps = {
+    alignRight: false,
+    value: 'on',
+};
+
+export default ToggleSwitch;
--- packages/ffe-form-react/src/index.d.ts
+++ packages/ffe-form-react/src/index.d.ts
@@ -150,6 +150,15 @@ export interface TextAreaProps
     className?: string;
 }
 
+export interface ToggleSwitchProps
+    extends React.InputHTMLAttributes<HTMLInputElement> {
+    children: React.ReactNode;
+    className: string;
+    id: string;
+    value: string;
+    alignRight: boolean;
+}
+
 declare class Checkbox extends React.Component<CheckboxProps, any> {}
 declare class ErrorFieldMessage extends React.Component<
     BaseFieldMessageProps,
@@ -168,6 +177,7 @@ declare class PhoneNumber extends React.Component<PhoneNumberProps, any> {}
 declare class Label extends React.Component<LabelProps, any> {}
 declare class InputGroup extends React.Component<InputGroupProps, any> {}
 declare class Tooltip extends React.Component<TooltipProps, any> {}
+declare class ToggleSwitch extends React.Component<ToggleSwitchProps, any> {}
 declare class RadioBlock extends React.Component<RadioBlockProps, any> {}
 declare class RadioButton extends React.Component<RadioButtonProps, any> {}
 declare class RadioButtonInputGroup extends React.Component<
