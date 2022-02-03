2a26d1ae91084e269d22b6f4da1a3704d0e2c398
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2022-02-03 09:30:55 +0100

    fix(ffe-context-message-react): icon override not possible anymore
    The breaking changes in 6.0.0 suggests that the icon could no longer be sent as a property.
    You could stil to that, but this commit fixes that and the ts types.

--- packages/ffe-context-message-react/src/ContextInfoMessage.js
+++ packages/ffe-context-message-react/src/ContextInfoMessage.js
@@ -3,7 +3,7 @@ import ContextMessage from './ContextMessage';
 import InfoIkon from '@sb1/ffe-icons-react/lib/info-ikon';
 
 const ContextInfoMessage = props => (
-    <ContextMessage messageType="info" icon={<InfoIkon />} {...props} />
+    <ContextMessage {...props} messageType="info" icon={<InfoIkon />} />
 );
 
 export default ContextInfoMessage;
