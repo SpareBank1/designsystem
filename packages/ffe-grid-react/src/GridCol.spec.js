925faf87451cbcb2d3be74fb833ac6e83128a10a
Kristofer Walters <kristofer.walters@sparebank1.no>
2022-02-09 11:08:39 +0100

    chore(ffe-grid-react): use ffe-buildtool for testing

--- packages/ffe-grid-react/src/GridCol.spec.js
+++ packages/ffe-grid-react/src/GridCol.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { GridCol } from '.';
 import backgroundColors from './background-colors';
 
--- packages/ffe-grid-react/src/GridRow.spec.js
+++ packages/ffe-grid-react/src/GridRow.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 
 import { GridRow, GridCol } from '.';
 import backgroundColors from './background-colors';
