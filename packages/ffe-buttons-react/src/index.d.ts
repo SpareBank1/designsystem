60b752a58e7148e1e123053821c8ed6591a0e2a6
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2022-02-15 13:33:21 +0100

    feat(ffe-buttons-react): remove react-router to attribute
    BREAKING CHANGE: Typescript users have any more react-router types.
    This must now be created with generics
    
    <SecondaryButton <Link> to="pathname" />

--- packages/ffe-buttons-react/src/index.d.ts
+++ packages/ffe-buttons-react/src/index.d.ts
@@ -1,22 +1,14 @@
 import * as React from 'react';
 
-type To =
-    | string
-    | {
-          pathname: string;
-          search?: string;
-          hash?: string;
-          state?: { [key: string]: any };
-      };
+export type MinimalBaseButtonProps<ExtraProps> = React.HTMLProps<HTMLElement> &
+    ExtraProps & {
+        className?: string;
+        element?: HTMLElement | string | React.ElementType;
+        innerRef?: React.Ref<HTMLElement>;
+    };
 
-export interface MinimalBaseButtonProps extends React.HTMLProps<HTMLElement> {
-    className?: string;
-    element?: HTMLElement | string | React.ElementType;
-    innerRef?: React.Ref<HTMLElement>;
-    to?: To; //used in order to make buttons work with react-router functionality in typescript-files.
-}
-
-export interface BaseButtonProps extends MinimalBaseButtonProps {
+export interface BaseButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children?: React.ReactNode;
     ariaLoadingMessage?: string;
     condensed?: boolean;
@@ -26,11 +18,13 @@ export interface BaseButtonProps extends MinimalBaseButtonProps {
     rightIcon?: React.ReactNode;
 }
 
-export interface ActionButtonProps extends BaseButtonProps {
+export interface ActionButtonProps<ExtraProps>
+    extends BaseButtonProps<ExtraProps> {
     ghost?: boolean;
 }
 
-export interface BackButtonProps extends MinimalBaseButtonProps {
+export interface BackButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children?: React.ReactNode;
 }
 
@@ -40,7 +34,8 @@ export interface ButtonGroupProps {
     inline?: boolean;
 }
 
-export interface ExpandButtonProps extends MinimalBaseButtonProps {
+export interface ExpandButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children: React.ReactNode;
     closeLabel?: string;
     leftIcon?: React.ReactNode;
@@ -49,56 +44,77 @@ export interface ExpandButtonProps extends MinimalBaseButtonProps {
     onClick: (e: React.MouseEvent | undefined) => void;
 }
 
-export interface InlineExpandButtonProps extends MinimalBaseButtonProps {
+export interface InlineExpandButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children?: React.ReactNode;
     innerRef?: React.Ref<HTMLElement>;
     isExpanded: boolean;
     onClick: (e: React.MouseEvent | undefined) => void;
 }
 
-export interface PrimaryButtonProps extends BaseButtonProps {}
+export interface PrimaryButtonProps<ExtraProps>
+    extends BaseButtonProps<ExtraProps> {}
 
-export interface SecondaryButtonProps extends BaseButtonProps {}
+export interface SecondaryButtonProps<ExtraProps>
+    extends BaseButtonProps<ExtraProps> {}
 
-export interface ShortcutButtonProps extends MinimalBaseButtonProps {
+export interface ShortcutButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children?: React.ReactNode;
     condensed?: boolean;
     disabled?: boolean;
     leftIcon?: React.ReactNode;
 }
 
-export interface TaskButtonProps extends MinimalBaseButtonProps {
+export interface TaskButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children?: React.ReactNode;
     condensed?: boolean;
     disabled?: boolean;
     icon: React.ReactNode;
 }
 
-export interface TertiaryButtonProps extends MinimalBaseButtonProps {
+export interface TertiaryButtonProps<ExtraProps>
+    extends MinimalBaseButtonProps<ExtraProps> {
     children?: React.ReactNode;
     leftIcon?: React.ReactNode;
     rightIcon?: React.ReactNode;
 }
 
-declare class ActionButton extends React.Component<ActionButtonProps, any> {}
-declare class BackButton extends React.Component<BackButtonProps, any> {}
+declare class ActionButton<ExtraProps> extends React.Component<
+    ActionButtonProps<ExtraProps>,
+    any
+> {}
+declare class BackButton<ExtraProps> extends React.Component<
+    BackButtonProps<ExtraProps>,
+    any
+> {}
 declare class ButtonGroup extends React.Component<ButtonGroupProps, any> {}
-declare class ExpandButton extends React.Component<ExpandButtonProps, any> {}
-declare class InlineExpandButton extends React.Component<
-    InlineExpandButtonProps,
+declare class ExpandButton<ExtraProps> extends React.Component<
+    ExpandButtonProps<ExtraProps>,
     any
 > {}
-declare class PrimaryButton extends React.Component<PrimaryButtonProps, any> {}
-declare class SecondaryButton extends React.Component<
-    SecondaryButtonProps,
+declare class InlineExpandButton<ExtraProps> extends React.Component<
+    InlineExpandButtonProps<ExtraProps>,
     any
 > {}
-declare class ShortcutButton extends React.Component<
-    ShortcutButtonProps,
+declare class PrimaryButton<ExtraProps> extends React.Component<
+    PrimaryButtonProps<ExtraProps>,
     any
 > {}
-declare class TaskButton extends React.Component<TaskButtonProps, any> {}
-declare class TertiaryButton extends React.Component<
-    TertiaryButtonProps,
+declare class SecondaryButton<ExtraProps> extends React.Component<
+    SecondaryButtonProps<ExtraProps>,
+    any
+> {}
+declare class ShortcutButton<ExtraProps> extends React.Component<
+    ShortcutButtonProps<ExtraProps>,
+    any
+> {}
+declare class TaskButton<ExtraProps> extends React.Component<
+    TaskButtonProps<ExtraProps>,
+    any
+> {}
+declare class TertiaryButton<ExtraProps> extends React.Component<
+    TertiaryButtonProps<ExtraProps>,
     any
 > {}
