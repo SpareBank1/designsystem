c0325741d0d3f7afe685d94112307c7b70ad86f2
Kristofer Walters <kristofer.walters@sparebank1.no>
2022-02-18 08:38:04 +0100

    fix(buildtool): run jest tests with jsdom environment

--- buildtool/config/jest.config.js
+++ buildtool/config/jest.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     rootDir: path.join(process.cwd(), 'src'),
     setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.js')],
+    testEnvironment: 'jsdom',
     transform: {
         '\\.js$': [
             'babel-jest',
