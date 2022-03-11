832d5a4c6438cf7018acd27c44f6f5f2f2b388a1
andrea-sb1 <77671758+andrea-sb1@users.noreply.github.com>
2022-03-11 12:22:12 +0100

    fix(component-overview): Rette opp tekst i meldingeksempeler

--- component-overview/examples/context-message/ContextErrorMessage-coloredbg.jsx
+++ component-overview/examples/context-message/ContextErrorMessage-coloredbg.jsx
@@ -2,6 +2,6 @@ import { ContextErrorMessage } from '@sb1/ffe-context-message-react';
 
 <div style={{ backgroundColor: 'var(--ffe-farge-frost-30)', padding: '8px' }}>
     <ContextErrorMessage onColoredBg={true}>
-        Dette gikk ikke som forventet i det hele tatt!
+        Dette gikk ikke helt som forventet!
     </ContextErrorMessage>
 </div>;
--- component-overview/examples/message-box/InfoMessage-coloredbg.jsx
+++ component-overview/examples/message-box/InfoMessage-coloredbg.jsx
@@ -4,8 +4,7 @@ import { Paragraph } from '@sb1/ffe-core-react';
 <div style={{ backgroundColor: 'var(--ffe-farge-frost-30)', padding: '8px' }}>
     <InfoMessage title="Du har ingen kort" onColoredBg={true}>
         <Paragraph>
-            Denne tjenesten er kun tilgjengelig for deg med et debitkort eller
-            kredittkort fra SpareBank 1.
+            For å bruke denne tjenesten må du ha et debetkort eller kredittkort fra SpareBank 1.
         </Paragraph>
     </InfoMessage>
 </div>;
