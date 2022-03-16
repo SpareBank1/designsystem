9b3adaec910a464354daad5a8dd4a78c443a5f94
Kristofer Walters <kristofer.walters@sparebank1.no>
2022-03-16 14:46:20 +0100

    fix(component-overview): group examples for messages

--- component-overview/examples/messages/message-box/InfoMessage-coloredbg.jsx
+++ component-overview/examples/messages/message-box/InfoMessage-coloredbg.jsx
@@ -4,7 +4,8 @@ import { Paragraph } from '@sb1/ffe-core-react';
 <div style={{ backgroundColor: 'var(--ffe-farge-frost-30)', padding: '8px' }}>
     <InfoMessage title="Du har ingen kort" onColoredBg={true}>
         <Paragraph>
-            For å bruke denne tjenesten må du ha et debetkort eller kredittkort fra SpareBank 1.
+            For å bruke denne tjenesten må du ha et debetkort eller kredittkort
+            fra SpareBank 1.
         </Paragraph>
     </InfoMessage>
 </div>;
