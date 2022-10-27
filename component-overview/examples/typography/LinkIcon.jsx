af162c56c48e0f9deb998bf7ae60fad080ba2cdb
HeleneKassandra <hjuterud@gmail.com>
2022-10-27 18:15:10 +0200

    fix(component-overview): rename linktext-icon example and show new LinkIcon component

---
+++ component-overview/examples/typography/LinkIcon.jsx
@@ -0,0 +1,9 @@
+import { LinkIcon } from '@sb1/ffe-core-react';
+import { SnakkebobleIkon } from '@sb1/ffe-icons-react';
+
+<LinkIcon href="https://www.sparebank1.no">
+    <SnakkebobleIkon
+        title="Snakk med oss"
+        style={{ height: '80px' }}
+    />
+</LinkIcon>
