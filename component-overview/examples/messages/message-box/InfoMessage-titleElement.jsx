0efe5c54fb5d8fc46c81ee209ff73bcbe11aaded
HeleneKassandra <hjuterud@gmail.com>
2022-12-08 11:26:33 +0100

    fix(component-overview): legg til eksempel for bruk av titleElement i messagebox

---
+++ component-overview/examples/messages/message-box/InfoMessage-titleElement.jsx
@@ -0,0 +1,8 @@
+import { InfoMessage } from '@sb1/ffe-message-box-react';
+import { Paragraph } from '@sb1/ffe-core-react';
+
+<InfoMessage title="Tittelen her er h4" titleElement="h4">
+    <Paragraph>
+        Ved å bruke titleElement propertyen kan man endre HTML-elementet på tittelen.
+    </Paragraph>
+</InfoMessage>
