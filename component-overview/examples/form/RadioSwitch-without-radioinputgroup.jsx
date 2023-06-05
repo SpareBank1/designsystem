17eb51ef4b494f5ca96e2db42e81131d81933d16
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2023-06-05 14:08:59 +0200

    fix(component-overview): legg til eksempel med radioswitch uten group

---
+++ component-overview/examples/form/RadioSwitch-without-radioinputgroup.jsx
@@ -0,0 +1,11 @@
+import { RadioSwitch } from '@sb1/ffe-form-react';
+
+() => {
+    return ( 
+    <RadioSwitch
+        leftLabel="Ja"
+        leftValue="true"
+        rightLabel="Nei"
+        rightValue="false"
+    />);
+}
