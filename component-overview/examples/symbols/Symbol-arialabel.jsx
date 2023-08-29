b608975d12311296db2d6dc0cce7411a8dab551d
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2023-08-29 15:31:13 +0200

    fix(component-overview): oppdater eksemplene

--- component-overview/examples/symbols/Symbol-arialabel.jsx
+++ component-overview/examples/symbols/Symbol-arialabel.jsx
@@ -3,7 +3,7 @@ import Symbol from "@sb1/ffe-symbols-react";
 () => {
     return (
         <>
-            <Symbol ariaLabel="hus ikon" size="md">home</Symbol>
+            <Symbol ariaLabel="hus" size="md">home</Symbol>
             <Symbol ariaLabel={null} size="md">home</Symbol>  
         </>
     )
