c8190b9b95645b4ce1dc8ba06841e0233d42393e
Simen Skogen <simen.skogen@sparebank1.no>
2023-08-29 14:45:20 +0200

    chore(ffe-account-selector-react)!: remove AccountSelectorHighCapacity
    AccountSelectorHighCapacity depends on SearchableDropdownHighCapacity
    from ffe-searchable-dropdown-react, which depends on
    react-virtualization; a package which is no longer maintained and
    does not work seamlessly with modern build tools such as vite.
    
    If high capacity is still needed, use AccountSelectorMulti or
    BaseSelector with prop highCapacity=true.
    
    BREAKING CHANGE: AccountSelectorHighCapacity is no longer exported.
    Use AccountSelectorMulti or BaseSelector with prop highCapacity=true

--- packages/ffe-account-selector-react/src/index.js
+++ packages/ffe-account-selector-react/src/index.js
@@ -1,6 +1,3 @@
-export {
-    AccountSelector,
-    AccountSelectorHighCapacity,
-} from './components/account-selector';
+export { AccountSelector } from './components/account-selector';
 export { default as AccountSelectorMulti } from './components/account-selector-multi';
 export { default as BaseSelector } from './components/base-selector';
