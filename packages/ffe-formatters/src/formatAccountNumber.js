8f073bdc624612e130ddaf000bff05de89e2073d
Henning Huseby <henning.huseby@sparebank1.no>
2023-08-31 15:21:41 +0200

    feat(ffe-formatters): add foreign account number formating

--- packages/ffe-formatters/src/formatAccountNumber.js
+++ packages/ffe-formatters/src/formatAccountNumber.js
@@ -1,9 +1,20 @@
 import { NON_BREAKING_SPACE } from './internal/unicode';
 
-export default function formatAccountNumber(accountNumber) {
-    if (!accountNumber) {
-        return accountNumber;
+function foreignAccountNumber(accountNumber) {
+    let formattedAccountNumber = '';
+
+    for (let i = 0; i < accountNumber.length; i++) {
+        const isFourthIteration = (i + 1) % 4 === 0;
+        formattedAccountNumber += accountNumber[i].valueOf();
+
+        if (isFourthIteration && i + 1 < accountNumber.length) {
+            formattedAccountNumber += NON_BREAKING_SPACE;
+        }
     }
+    return formattedAccountNumber;
+}
+
+function domesticAccountNumber(accountNumber) {
     let formattedAccountNumber = '';
 
     if (accountNumber.length > 0) {
@@ -21,4 +32,14 @@ export default function formatAccountNumber(accountNumber) {
     }
 
     return formattedAccountNumber;
+}
+
+export default function formatAccountNumber(accountNumber) {
+    if (!accountNumber) {
+        return accountNumber;
+    }
+    if (accountNumber.length <= 11) {
+        return domesticAccountNumber(accountNumber);
+    }
+    return foreignAccountNumber(accountNumber);
 }
