030c55e84061ffeb205a66e3dc976838fba96bd9
Helene Kassandra <39946146+HeleneKassandra@users.noreply.github.com>
2023-11-06 11:53:29 +0100

    fix(ffe-spinner-react): legger til loadingText-prop BREAKING CHANGE: Classname-prop settes nå på div rundt spinner ikon og ikke på selve ikonet.

--- packages/ffe-spinner-react/src/Spinner.js
+++ packages/ffe-spinner-react/src/Spinner.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { bool, string } from 'prop-types';
+import { bool, string, node } from 'prop-types';
 import classNames from 'classnames';
 
-const Spinner = ({ className, immediate, large, ...rest }) => (
-    <span
-        aria-live="assertive"
-        className={classNames(
-            'ffe-loading-spinner',
-            { 'ffe-loading-spinner--immediate': immediate },
-            { 'ffe-loading-spinner--large': large },
-            className,
-        )}
-        {...rest}
-    />
+const Spinner = ({ className, immediate, large, loadingText, ...rest }) => (
+    <div aria-live="assertive" className={className} {...rest}>
+        <span
+            className={classNames(
+                'ffe-loading-spinner',
+                { 'ffe-loading-spinner--immediate': immediate },
+                { 'ffe-loading-spinner--large': large },
+            )}
+            role="img"
+            aria-label="Vennligst vent"
+            aria-hidden={!!loadingText}
+        />
+        {loadingText}
+    </div>
 );
 
 Spinner.propTypes = {
     className: string,
     immediate: bool,
     large: bool,
+    loadingText: node,
 };
 
 Spinner.defaultProps = {
--- packages/ffe-spinner-react/src/Spinner.spec.js
+++ packages/ffe-spinner-react/src/Spinner.spec.js
@@ -7,20 +7,34 @@ describe('<Spinner />', () => {
     it('renders without exploding', () => {
         const wrapper = getWrapper();
         expect(wrapper.exists()).toBe(true);
-        expect(wrapper.is('span')).toBe(true);
+        expect(wrapper.is('div')).toBe(true);
+        expect(wrapper.find('span').exists()).toBe(true);
     });
     it('renders classes correctly', () => {
         const wrapper = getWrapper({ className: 'test-class' });
-        expect(wrapper.hasClass('ffe-loading-spinner')).toBe(true);
+        expect(wrapper.find('span').hasClass('ffe-loading-spinner')).toBe(true);
         expect(wrapper.hasClass('test-class')).toBe(true);
     });
     it('renders a large spinner', () => {
         const wrapper = getWrapper({ large: true });
-
-        expect(wrapper.hasClass('ffe-loading-spinner--large')).toBe(true);
+        expect(
+            wrapper.find('span').hasClass('ffe-loading-spinner--large'),
+        ).toBe(true);
     });
     it('passes props correctly', () => {
         const wrapper = getWrapper({ id: 'test-id' });
         expect(wrapper.prop('id')).toBe('test-id');
+    });
+    it('aria-hidden is set to false by default', () => {
+        const wrapper = getWrapper();
+        expect(wrapper.find('span').prop('aria-hidden')).toBe(false);
+    });
+    it('aria-hidden set to true when loadingText set', () => {
+        const wrapper = getWrapper({ loadingText: 'test' });
+        expect(wrapper.find('span').prop('aria-hidden')).toBe(true);
+    });
+    it('set loadingText correctly', () => {
+        const wrapper = getWrapper({ loadingText: <p>Text</p> });
+        expect(wrapper.find('p').exists()).toBe(true);
     });
 });
