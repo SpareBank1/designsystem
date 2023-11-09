5be3f03541635a9b5f6450ac276e95be3484a6bc
Helene Kassandra <39946146+HeleneKassandra@users.noreply.github.com>
2023-11-09 12:32:42 +0100

    fix(component-overview): legg til eksempel

---
+++ component-overview/examples/buttons/ActionButton-Ikon2.jsx
@@ -0,0 +1,13 @@
+import { PrimaryButton, ButtonGroup } from '@sb1/ffe-buttons-react';
+
+<>
+    <ButtonGroup thin={true}>
+        <PrimaryButton
+            onClick={f => f}
+            ariaLoadingMessage="Vennligst vent..."
+            leftIcon="check"
+        >
+            Bekreft
+        </PrimaryButton>
+    </ButtonGroup>
+</>;
