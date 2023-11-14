7ef339bc4a98da44bf5acd75c16a5c51c4f4b404
Anders <anders.johnsen@gmail.com>
2023-11-14 15:37:23 +0100

    docs(component-overview): bytter eksempel-ikon til noe med mer normale proporsjoner

--- component-overview/examples/buttons/SecondaryButton-leftIcon.jsx
+++ component-overview/examples/buttons/SecondaryButton-leftIcon.jsx
@@ -1,7 +1,7 @@
 import { SecondaryButton, ButtonGroup } from '@sb1/ffe-buttons-react';
 
 <ButtonGroup thin={true}>
-    <SecondaryButton leftIcon="attach_file" onClick={f => f}>
+    <SecondaryButton leftIcon="check" onClick={f => f}>
         Secondary m/ ikon
     </SecondaryButton>
 </ButtonGroup>;
