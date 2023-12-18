795b666007b86b28e06d0554db2a5fde745f3d5e
Helene Kassandra <39946146+HeleneKassandra@users.noreply.github.com>
2023-12-18 15:26:52 +0100

    feat(component-overview): legg til icon eksempler

---
+++ component-overview/examples/icons/Icon.jsx
@@ -0,0 +1,6 @@
+import { Icon } from '@sb1/ffe-icons-react';
+import batteryIcon from '@sb1/ffe-icons/icons/300/filled/sm/battery_90.svg';
+
+() => {
+    return <Icon filePath={batteryIcon} size="sm" ariaLabel="Batteri" />;
+}
