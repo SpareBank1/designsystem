3bfa3542aa633d87ce06490efdd81929f606c481
Helene Kassandra <39946146+HeleneKassandra@users.noreply.github.com>
2023-12-18 15:28:21 +0100

    feat(ffe-icons-react): legg til ikon wrapper komponent BREAKING CHANGE: helt ny komponent

---
+++ packages/ffe-icons-react/src/Icon.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Icon from './Icon';
+
+const getWrapper = props => shallow(<Icon {...props} />);
+
+describe('Icon', () => {
+    it('renders without crashing and with right class', () => {
+        const wrapper = getWrapper({ filePath: '<svg></svg>' });
+        expect(wrapper.exists()).toBe(true);
+        expect(wrapper.hasClass('ffe-icons')).toBe(true);
+    });
+    it('renders with role="img', () => {
+        const wrapper = getWrapper({ filePath: '<svg></svg>' });
+        expect(wrapper.props()).toHaveProperty('role', 'img');
+    });
+    it('renders correct classname based on size-prop', () => {
+        const wrapper = getWrapper({ filePath: '<svg></svg>', size: 'sm' });
+        expect(wrapper.hasClass('ffe-icons--sm')).toBe(true);
+    });
+    it('applies additional CSS classes when provided', () => {
+        const wrapper = getWrapper({
+            filePath: '<svg></svg>',
+            size: 'md',
+            className: 'my-class',
+        });
+        expect(wrapper.hasClass('ffe-icons ffe-icons--md my-class')).toBe(true);
+    });
+});
