f31b96845e3049ffb2623e4387d29cd0b2a83234
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-01-11 13:51:19 +0100

    feat(ffe-feedback-react): tomler direkte fra svg som react-komponenter

--- packages/ffe-feedback-react/src/feedback-thumbs.js
+++ packages/ffe-feedback-react/src/feedback-thumbs.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import Symbol from '@sb1/ffe-symbols-react';
 import i18n from './i18n/i18n';
 import { func, oneOf } from 'prop-types';
+import { ThumbUp, ThumbUpFill, ThumbDown, ThumbDownFill } from './icons';
 
 export const Thumbs = {
     UP: 'THUMB_UP',
     DOWN: 'THUMB_DOWN',
 };
-const THUMB_SIZE = '2xl';
 
 const FeedbackThumbs = ({ language, onClick }) => {
-    const handleMouseEnter = event => {
-        // eslint-disable-next-line no-param-reassign
-        event.target.className = `ffe-symbol ffe-symbol--filled-400 ffe-symbol--${THUMB_SIZE}`;
-    };
-
-    const handleMouseLeave = event => {
-        // eslint-disable-next-line no-param-reassign
-        event.target.className = `ffe-symbol ffe-symbol--400 ffe-symbol--${THUMB_SIZE}`;
-    };
-
     return (
         <div>
             <button
@@ -28,12 +17,8 @@ const FeedbackThumbs = ({ language, onClick }) => {
                 className="ffe-feedback__thumb"
                 onClick={() => onClick(Thumbs.UP)}
             >
-                <Symbol
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    size={THUMB_SIZE}
-                    icon="thumb_up"
-                />
+                <ThumbUp className="ffe-feedback__thumb-icon" />
+                <ThumbUpFill className="ffe-feedback__thumb-icon--fill" />
             </button>
             <button
                 aria-label={i18n[language].ARIA_LABEL_THUMB_DOWN}
@@ -41,12 +26,8 @@ const FeedbackThumbs = ({ language, onClick }) => {
                 className="ffe-feedback__thumb"
                 onClick={() => onClick(Thumbs.DOWN)}
             >
-                <Symbol
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    size={THUMB_SIZE}
-                    icon="thumb_down"
-                />
+                <ThumbDown className="ffe-feedback__thumb-icon" />
+                <ThumbDownFill className="ffe-feedback__thumb-icon--fill" />
             </button>
         </div>
     );
