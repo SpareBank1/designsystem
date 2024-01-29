3649799c472110bb41b7e605733eada9f9d12f08
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2024-01-29 15:20:53 +0100

    fix(ffe-accordion-react): erstatt ikon med inline svg Fjerner dependency til ffe-symbols-react

--- packages/ffe-accordion-react/src/AccordionItem.js
+++ packages/ffe-accordion-react/src/AccordionItem.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { bool, func, node, string } from 'prop-types';
 import { v4 as uuid } from 'uuid';
-import Symbol from '@sb1/ffe-symbols-react';
 import Collapse from '@sb1/ffe-collapse-react';
 import classNames from 'classnames';
 
@@ -71,11 +70,16 @@ const AccordionItem = ({
                     <span className="ffe-accordion-item__heading-button-content">
                         {heading}
                         <span className="ffe-accordion-item__heading-icon-wrapper">
-                            <Symbol
+                            <svg
                                 className="ffe-accordion-item__heading-icon"
-                                icon="expand_more"
-                                size="sm"
-                            />
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24"
+                                viewBox="0 -960 960 960"
+                                width="24"
+                            >
+                                <path d="M480-373.539q-7.231 0-13.461-2.308-6.231-2.308-11.846-7.923L274.924-563.539q-8.308-8.307-8.5-20.884-.193-12.577 8.5-21.269 8.692-8.692 21.076-8.692t21.076 8.692L480-442.768l162.924-162.924q8.307-8.307 20.884-8.5 12.576-.192 21.268 8.5 8.693 8.692 8.693 21.077 0 12.384-8.693 21.076L505.307-383.77q-5.615 5.615-11.846 7.923-6.23 2.308-13.461 2.308Z" />
+                            </svg>
                         </span>
                     </span>
                 </button>
