dfdbc9823c5f0ed43e516174ab8baeb0fd469bd8
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2024-01-29 15:43:29 +0100

    fix(ffe-account-selector-react): erstatter ikoner med inline svg Endrer fra color til fill på ikonene og fjerner dependency til ffe-symbols-react

--- packages/ffe-account-selector-react/src/subcomponents/input-field/InputField.js
+++ packages/ffe-account-selector-react/src/subcomponents/input-field/InputField.js
@@ -2,7 +2,6 @@
 /* eslint jsx-a11y/role-has-required-aria-props:0 */
 import React from 'react';
 import { func, string, bool, number } from 'prop-types';
-import Symbol from '@sb1/ffe-symbols-react';
 import classNames from 'classnames';
 import txt from '../../i18n/i18n';
 import { Locale } from '../../util/types';
@@ -83,12 +82,16 @@ const InputField = props => {
                     type="button"
                     aria-label={txt[locale].RESET_SEARCH}
                 >
-                    <Symbol
-                        icon="close"
-                        size="sm"
-                        weight={300}
+                    <svg
                         className="ffe-base-selector__reset-button-icon"
-                    />
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="20"
+                        viewBox="0 -960 960 960"
+                        width="20"
+                    >
+                        <path d="M480-442.847 309.076-271.924q-8.307 8.308-17.884 8-9.576-.307-18.268-9-8.693-8.692-8.693-18.576t8.693-18.576L442.847-480 271.924-650.924q-8.308-8.307-8-18.384.307-10.076 9-18.768 8.692-8.693 18.576-8.693t18.576 8.693L480-517.153l170.924-170.923q8.307-8.308 18.384-8.5 10.076-.193 18.768 8.5 8.693 8.692 8.693 18.576t-8.693 18.576L517.153-480l170.923 170.924q8.308 8.307 8.5 17.884.193 9.576-8.5 18.268-8.692 8.693-18.576 8.693t-18.576-8.693L480-442.847Z" />
+                    </svg>
                 </button>
             )}
             <button
@@ -102,17 +105,21 @@ const InputField = props => {
                         : txt[locale].ACCOUNTSLIST_OPEN
                 }
             >
-                <Symbol
-                    icon="expand_more"
-                    size="md"
-                    weight={300}
+                <svg
                     className={classNames(
                         'ffe-base-selector__expand-button-icon',
                         {
                             'ffe-base-selector__expand-button-icon--invalid': ariaInvalid,
                         },
                     )}
-                />
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24"
+                >
+                    <path d="M480-373.539q-7.231 0-13.461-2.308-6.231-2.308-11.846-7.923L274.924-563.539q-8.308-8.307-8.5-20.884-.193-12.577 8.5-21.269 8.692-8.692 21.076-8.692t21.076 8.692L480-442.768l162.924-162.924q8.307-8.307 20.884-8.5 12.576-.192 21.268 8.5 8.693 8.692 8.693 21.077 0 12.384-8.693 21.076L505.307-383.77q-5.615 5.615-11.846 7.923-6.23 2.308-13.461 2.308Z" />
+                </svg>
             </button>
         </div>
     );
