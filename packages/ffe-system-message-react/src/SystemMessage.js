5f916e8fcf41bc3bd17c77ac673c3535da001a35
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-02-08 17:11:27 +0100

    chore(ffe-system-message-react): use ffe-icons-react instad og ffe-symbols-react

--- packages/ffe-system-message-react/src/SystemMessage.js
+++ packages/ffe-system-message-react/src/SystemMessage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { func, string, number, node, oneOf, bool } from 'prop-types';
 import classNames from 'classnames';
-import Symbol from '@sb1/ffe-symbols-react';
+import { Icon } from '@sb1/ffe-icons-react';
 
 class SystemMessage extends Component {
     constructor() {
@@ -65,8 +65,6 @@ class SystemMessage extends Component {
                         {icon &&
                             React.cloneElement(icon, {
                                 ...icon.props,
-                                size: 'sm',
-                                weight: 300,
                             })}
                     </span>
                     <p className="ffe-system-message__content">{children}</p>
@@ -76,7 +74,10 @@ class SystemMessage extends Component {
                         onClick={this.close}
                         type="button"
                     >
-                        <Symbol icon="close" size="sm" weight={300} />
+                        <Icon
+                            fileUrl="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjAiPjxwYXRoIGQ9Ik00ODAtNDQyLjg0NyAzMDkuMDc2LTI3MS45MjRxLTguMzA3IDguMzA4LTE3Ljg4NCA4LTkuNTc2LS4zMDctMTguMjY4LTktOC42OTMtOC42OTItOC42OTMtMTguNTc2dDguNjkzLTE4LjU3Nkw0NDIuODQ3LTQ4MCAyNzEuOTI0LTY1MC45MjRxLTguMzA4LTguMzA3LTgtMTguMzg0LjMwNy0xMC4wNzYgOS0xOC43NjggOC42OTItOC42OTMgMTguNTc2LTguNjkzdDE4LjU3NiA4LjY5M0w0ODAtNTE3LjE1M2wxNzAuOTI0LTE3MC45MjNxOC4zMDctOC4zMDggMTguMzg0LTguNSAxMC4wNzYtLjE5MyAxOC43NjggOC41IDguNjkzIDguNjkyIDguNjkzIDE4LjU3NnQtOC42OTMgMTguNTc2TDUxNy4xNTMtNDgwbDE3MC45MjMgMTcwLjkyNHE4LjMwOCA4LjMwNyA4LjUgMTcuODg0LjE5MyA5LjU3Ni04LjUgMTguMjY4LTguNjkyIDguNjkzLTE4LjU3NiA4LjY5M3QtMTguNTc2LTguNjkzTDQ4MC00NDIuODQ3WiIvPjwvc3ZnPg=="
+                            size="sm"
+                        />
                     </button>
                 </div>
             </div>
--- packages/ffe-system-message-react/src/SystemSuccessMessage.js
+++ packages/ffe-system-message-react/src/SystemSuccessMessage.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Symbol from '@sb1/ffe-symbols-react';
+import { Icon } from '@sb1/ffe-icons-react';
 
 import SystemMessage from './SystemMessage';
 
 export default function SystemSuccessMessage(props) {
+    const checkIconSmall =
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjAiPjxwYXRoIGQ9Im0zOTUtMzcyLjM4NCAyNzAuNTM5LTI2OS41MzhxNy45MjMtNy45MjMgMTcuNjkyLTcuNjE2IDkuNzY5LjMwOCAxOC4wNzYgOC42MTYgOC4zMDggOC4zMDcgOC4zMDggMTcuODg0IDAgOS41NzYtOC4zMDggMTcuODg0bC0yODMgMjgyLjk5OXEtOS44NDYgOS44NDYtMjIuODA3IDkuODQ2LTEyLjk2MSAwLTIyLjgwNy05Ljg0NmwtMTE0LTExMy45OTlxLTcuOTIzLTcuOTIzLTguMzA4LTE3LjY5Mi0uMzg0LTkuNzY5IDcuOTIzLTE4LjA3NiA4LjMwOC04LjMwOCAxNy44ODQtOC4zMDggOS41NzcgMCAxNy44ODQgOC4zMDhMMzk1LTM3Mi4zODRaIi8+PC9zdmc+';
     return (
         <SystemMessage
             modifier="success"
             aria-label="Suksessmelding"
-            icon={<Symbol ariaLabel="Hake" icon="check" />}
+            icon={<Icon ariaLabel="Hake" size="sm" fileUrl={checkIconSmall} />}
             {...props}
         />
     );
