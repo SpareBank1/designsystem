9248b0ba3ddea0c31697be553e2bf4f049ca4e91
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-02-08 17:04:10 +0100

    chore(ffe-accordion-react): use ffe-icons-react instad og ffe-symbols-react

--- packages/ffe-accordion-react/src/AccordionItem.js
+++ packages/ffe-accordion-react/src/AccordionItem.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { bool, func, node, string } from 'prop-types';
 import { v4 as uuid } from 'uuid';
-import Symbol from '@sb1/ffe-symbols-react';
+import { Icon } from '@sb1/ffe-icons-react';
 import Collapse from '@sb1/ffe-collapse-react';
 import classNames from 'classnames';
 
@@ -20,6 +20,9 @@ const AccordionItem = ({
     const [isFocused, setFocus] = useState(false);
     const buttonId = useRef(uuid());
     const contentId = useRef(uuid());
+
+    const expandMoreIcon =
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjQiPjxwYXRoIGQ9Ik00ODAtMzczLjUzOXEtNy4yMzEgMC0xMy40NjEtMi4zMDgtNi4yMzEtMi4zMDgtMTEuODQ2LTcuOTIzTDI3NC45MjQtNTYzLjUzOXEtOC4zMDgtOC4zMDctOC41LTIwLjg4NC0uMTkzLTEyLjU3NyA4LjUtMjEuMjY5IDguNjkyLTguNjkyIDIxLjA3Ni04LjY5MnQyMS4wNzYgOC42OTJMNDgwLTQ0Mi43NjhsMTYyLjkyNC0xNjIuOTI0cTguMzA3LTguMzA3IDIwLjg4NC04LjUgMTIuNTc2LS4xOTIgMjEuMjY4IDguNSA4LjY5MyA4LjY5MiA4LjY5MyAyMS4wNzcgMCAxMi4zODQtOC42OTMgMjEuMDc2TDUwNS4zMDctMzgzLjc3cS01LjYxNSA1LjYxNS0xMS44NDYgNy45MjMtNi4yMyAyLjMwOC0xMy40NjEgMi4zMDhaIi8+PC9zdmc+';
 
     useEffect(() => {
         if (isOpen != null) {
@@ -71,9 +74,10 @@ const AccordionItem = ({
                     <span className="ffe-accordion-item__heading-button-content">
                         {heading}
                         <span className="ffe-accordion-item__heading-icon-wrapper">
-                            <Symbol
+                            <Icon
                                 className="ffe-accordion-item__heading-icon"
                                 icon="expand_more"
+                                fileUrl={expandMoreIcon}
                                 size="sm"
                             />
                         </span>
