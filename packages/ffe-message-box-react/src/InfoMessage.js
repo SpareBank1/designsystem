7667d913ca8666d17b93ecf3b43d6bde7082fc9b
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-03-04 13:18:57 +0100

    fix(ffe-message-box-react): use translations from locale

--- packages/ffe-message-box-react/src/InfoMessage.js
+++ packages/ffe-message-box-react/src/InfoMessage.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { node, string } from 'prop-types';
+import { node, oneOf, string } from 'prop-types';
 import { Icon } from '@sb1/ffe-icons-react';
 import BaseMessage from './BaseMessage';
+import texts from './texts';
 
-const InfoMessage = props => {
+const InfoMessage = ({ locale, ...rest }) => {
     const infoIcon =
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iNDgiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iNDgiPjxwYXRoIGQ9Ik00NzkuODgxLTY5NC42MTVxLTI2LjE4OCAwLTQ0LjM4MS0xOC4zMTEtMTguMTkyLTE4LjMxMi0xOC4xOTItNDQuNXQxOC4zMTItNDQuMzhxMTguMzExLTE4LjE5MyA0NC40OTktMTguMTkzIDI2LjE4OCAwIDQ0LjM4MSAxOC4zMTIgMTguMTkyIDE4LjMxMSAxOC4xOTIgNDQuNDk5IDAgMjYuMTg5LTE4LjMxMiA0NC4zODEtMTguMzExIDE4LjE5Mi00NC40OTkgMTguMTkyWm0uMDI0IDU1NC42MTRxLTE3LjgyOCAwLTMwLjIxMi0xMi40NTItMTIuMzg1LTEyLjQ1Mi0xMi4zODUtMzAuMjR2LTM1OS4yMjlxMC0xNy43ODkgMTIuNDgtMzAuMjQgMTIuNDc5LTEyLjQ1MiAzMC4zMDctMTIuNDUydDMwLjIxMiAxMi40NTJxMTIuMzg1IDEyLjQ1MSAxMi4zODUgMzAuMjR2MzU5LjIyOXEwIDE3Ljc4OC0xMi40OCAzMC4yNC0xMi40NzkgMTIuNDUyLTMwLjMwNyAxMi40NTJaIi8+PC9zdmc+';
     return (
         <BaseMessage
             type="info"
-            aria-label="Infomelding"
-            icon={<Icon ariaLabel="Infotegn" fileUrl={infoIcon} size="xl" />}
-            {...props}
+            ariaLabel={texts[locale].info.ariaLabel}
+            icon={
+                <Icon
+                    ariaLabel={texts[locale].info.icon}
+                    fileUrl={infoIcon}
+                    size="xl"
+                />
+            }
+            {...rest}
         />
     );
 };
 
+InfoMessage.defaultProps = {
+    locale: 'nb',
+};
 InfoMessage.propTypes = {
     /** The content of the message box */
     children: node,
@@ -25,6 +35,8 @@ InfoMessage.propTypes = {
     icon: node,
     /** An optional title for the message */
     title: node,
+    /** 'nb', 'nn', or 'en' */
+    locale: oneOf(['en', 'nb', 'nn']),
 };
 
 export default InfoMessage;
