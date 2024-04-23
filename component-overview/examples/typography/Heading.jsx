5232de2150126b6911f39f26831e1dddd8850084
SindreSafvenbom <161836106+SindreSafvenbom@users.noreply.github.com>
2024-04-23 14:55:40 +0200

    docs(component-overview): Update Heading Structure Example

--- component-overview/examples/typography/Heading.jsx
+++ component-overview/examples/typography/Heading.jsx
@@ -10,6 +10,9 @@ import {
 <div>
     <Heading1>Overskrift nivå 1</Heading1>
     <Heading2>Overskrift nivå 2</Heading2>
+    <Heading2>Overskrift nivå 2</Heading2>
+    <Heading3>Overskrift nivå 3</Heading3>
+    <Heading4>Overskrift nivå 4</Heading4>
     <Heading3>Overskrift nivå 3</Heading3>
     <Heading4>Overskrift nivå 4</Heading4>
     <Heading5>Overskrift nivå 5</Heading5>
