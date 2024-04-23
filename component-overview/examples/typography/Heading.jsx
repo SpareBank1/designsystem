c77d805f7178d5768aa31730506785c27ea23ff4
SindreSafvenbom <161836106+SindreSafvenbom@users.noreply.github.com>
2024-04-23 09:42:21 +0200

    Update Heading.jsx
    Oppdaterer eksempelet for å gjenspeile den strukturelt korrekte bruken av <h> elementer. Trenger litt hjelp med styling for visuelt tydeliggjøre organiseringen (innrykk o.l.)

--- component-overview/examples/typography/Heading.jsx
+++ component-overview/examples/typography/Heading.jsx
@@ -9,9 +9,12 @@ import {
 
 <div>
     <Heading1>Overskrift nivå 1</Heading1>
-    <Heading2>Overskrift nivå 2</Heading2>
-    <Heading3>Overskrift nivå 3</Heading3>
-    <Heading4>Overskrift nivå 4</Heading4>
-    <Heading5>Overskrift nivå 5</Heading5>
-    <Heading6>Overskrift nivå 6</Heading6>
+        <Heading2>Overskrift nivå 2</Heading2>
+        <Heading2>Overskrift nivå 2</Heading2>
+            <Heading3>Overskrift nivå 3</Heading3>
+                <Heading4>Overskrift nivå 4</Heading4>
+            <Heading3>Overskrift nivå 3</Heading3>
+                <Heading4>Overskrift nivå 4</Heading4>
+                    <Heading5>Overskrift nivå 5</Heading5>
+                        <Heading6>Overskrift nivå 6</Heading6>
 </div>
