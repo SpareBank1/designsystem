562fac349b2726a86611e365b5c5ab46c6ea04dd
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-04-28 10:52:01 +0200

    fix(ffe-message-box-react): remove default props

--- packages/ffe-message-box-react/src/ErrorMessage.js
+++ packages/ffe-message-box-react/src/ErrorMessage.js
@@ -5,7 +5,7 @@ import BaseMessage from './BaseMessage';
 import texts from './texts';
 
 const ErrorMessage = props => {
-    const { alert, locale, ...rest } = props;
+    const { alert = true, locale = 'nb', ...rest } = props;
 
     const priorityHighIcon =
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iNDgiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iNDgiPjxwYXRoIGQ9Ik00NzkuODgxLTE2My4wOHEtMjEuNzg0IDAtMzcuMTc4LTE1LjUxMy0xNS4zOTQtMTUuNTEzLTE1LjM5NC0zNy4yOTd0MTUuNTEzLTM3LjE3OHExNS41MTMtMTUuMzk0IDM3LjI5Ny0xNS4zOTR0MzcuMTc4IDE1LjUxM3ExNS4zOTQgMTUuNTEzIDE1LjM5NCAzNy4yOTd0LTE1LjUxMyAzNy4xNzhxLTE1LjUxMyAxNS4zOTQtMzcuMjk3IDE1LjM5NFptLS4wNjEtMTk2LjE1MXEtMTkuNTg5IDAtMzMuMzU3LTEzLjg5Ni0xMy43NjktMTMuODk2LTEzLjc2OS0zMy40MXYtMzYzLjA3OHEwLTE5LjUxNCAxMy45NDktMzMuNDEgMTMuOTQ5LTEzLjg5NiAzMy41MzctMTMuODk2IDE5LjU4OSAwIDMzLjM1NyAxMy44OTYgMTMuNzY5IDEzLjg5NiAxMy43NjkgMzMuNDF2MzYzLjA3OHEwIDE5LjUxNC0xMy45NDkgMzMuNDEtMTMuOTQ5IDEzLjg5Ni0zMy41MzcgMTMuODk2WiIvPjwvc3ZnPg==';
@@ -19,11 +19,6 @@ const ErrorMessage = props => {
             {...rest}
         />
     );
-};
-
-ErrorMessage.defaultProps = {
-    alert: true,
-    locale: 'nb',
 };
 
 ErrorMessage.propTypes = {
--- packages/ffe-message-box-react/src/SuccessMessage.js
+++ packages/ffe-message-box-react/src/SuccessMessage.js
@@ -4,7 +4,7 @@ import { Icon } from '@sb1/ffe-icons-react';
 import BaseMessage from './BaseMessage';
 import texts from './texts';
 
-const SuccessMessage = ({ locale, ...rest }) => {
+const SuccessMessage = ({ locale = 'nb', ...rest }) => {
     const checkIcon =
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iNDgiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iNDgiPjxwYXRoIGQ9Im0zNzkuMTU0LTMyMy4xNTMgMzYzLTM2My4zODVxNi43NTktNi4zMDcgMTYuMTQ4LTYuNSA5LjM5LS4xOTIgMTYuNDY3IDYuNTUzIDYuNjkyIDcuMTI5IDYuNjkyIDE2LjQ5OSAwIDkuMzcxLTYuNjkyIDE2LjA2M0wzOTkuMzg1LTI3OC41MzlxLTguNjE2IDguNjE1LTIwLjIzMSA4LjYxNS0xMS42MTUgMC0yMC4yMzEtOC42MTVMMTg0LjYxNi00NTIuODQ2cS02LjY5Mi02Ljc1OS02LjU3Ny0xNi4xNDkuMTE2LTkuMzg5IDcuMjQ1LTE2LjQ2NiA3LjEyOS02LjY5MiAxNi4zMDctNi42OTIgOS4xNzggMCAxNi4yNTUgNi42OTJsMTYxLjMwOCAxNjIuMzA4WiIvPjwvc3ZnPg==';
 
@@ -16,10 +16,6 @@ const SuccessMessage = ({ locale, ...rest }) => {
             {...rest}
         />
     );
-};
-
-SuccessMessage.defaultProps = {
-    locale: 'nb',
 };
 
 SuccessMessage.propTypes = {
