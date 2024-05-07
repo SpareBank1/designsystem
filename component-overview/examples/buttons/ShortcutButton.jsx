ae66c53a676264bfba01308d33a2ead43d1283b4
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-05-07 09:09:14 +0200

    docs(component-overview): update ffe-buttons-react to match ts rewrite

--- component-overview/examples/buttons/ShortcutButton.jsx
+++ component-overview/examples/buttons/ShortcutButton.jsx
@@ -1,7 +1,7 @@
 import { ButtonGroup, ShortcutButton } from '@sb1/ffe-buttons-react';
 
 <ButtonGroup thin={true}>
-    <ShortcutButton element="a" href="#shortcutbutton">
+    <ShortcutButton as="a" href="#shortcutbutton">
         Snarvei
     </ShortcutButton>
 </ButtonGroup>
