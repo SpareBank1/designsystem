dd9855e2baab109bb35930c1f99b1d0f168ecf33
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-05-14 22:39:57 +0200

    fix(ffe-accordion-react): add missing props export

--- packages/ffe-accordion-react/src/index.ts
+++ packages/ffe-accordion-react/src/index.ts
@@ -1,2 +1,2 @@
-export { Accordion } from './Accordion';
-export { AccordionItem } from './AccordionItem';
+export { Accordion, AccordionProps } from './Accordion';
+export { AccordionItem, AccordionItemProps } from './AccordionItem';
