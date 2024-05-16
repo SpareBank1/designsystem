0d29a53c6f997d0268044a8d21a1ac252d3efaca
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-05-16 15:53:01 +0200

    feat(ffe-dropdown-react): rewrite to ts
    BREAKING CHANGE: innerRef -> refm default export -> named export

---
+++ packages/ffe-dropdown-react/src/Dropdown.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export interface DropdownProps
+    extends React.ComponentPropsWithoutRef<'select'> {
+    inline?: boolean;
+}
+
+export const Dropdown = React.forwardRef<HTMLSelectElement, DropdownProps>(
+    ({ className, inline, ...rest }, ref) => {
+        return (
+            <select
+                className={classNames(
+                    'ffe-dropdown',
+                    { 'ffe-dropdown--inline': inline },
+                    className,
+                )}
+                ref={ref}
+                {...rest}
+            />
+        );
+    },
+);
