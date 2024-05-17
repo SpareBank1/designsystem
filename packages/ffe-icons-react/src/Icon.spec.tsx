8341177a72e04882b326f589547748a15af9af04
peter <peter.hellstrand83@gmail.com>
2024-05-17 18:25:09 +0200

    feat(ffe-icons-react): rewrite to ts
    BREAKING CHANGE: default export -> named export

--- packages/ffe-icons-react/src/Icon.spec.tsx
+++ packages/ffe-icons-react/src/Icon.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Icon from './Icon';
+import { Icon } from './Icon';
 import { render, screen } from '@testing-library/react';
 
 describe('Icon', () => {
---
+++ packages/ffe-icons-react/src/Icon.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export interface IconProps extends React.ComponentPropsWithoutRef<'span'> {
+    /** The path to the svg-file og base64 string */
+    fileUrl: string;
+    /** Size of the icon, default is the closest defined font-size */
+    size?: 'sm' | 'md' | 'lg' | 'xl';
+    /** Symbols stroke weight. This can affect overall size of symbol, 400 is default */
+    weight?: 300 | 500;
+    /** Aria label text. If null/undefined, aria-hidden is automatically set to true */
+    ariaLabel?: React.ComponentProps<'span'>['aria-label'];
+}
+
+export const Icon: React.FC<IconProps> = ({
+    fileUrl,
+    className,
+    ariaLabel,
+    size = 'md',
+    ...rest
+}) => {
+    return (
+        <span
+            role="img"
+            aria-label={ariaLabel}
+            aria-hidden={!ariaLabel}
+            className={classNames('ffe-icons', `ffe-icons--${size}`, className)}
+            style={{
+                maskImage: `url(${fileUrl})`,
+                WebkitMaskImage: `url(${fileUrl})`,
+            }}
+            {...rest}
+        />
+    );
+};
