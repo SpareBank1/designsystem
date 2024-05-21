b6a619ee6351a420c110949d11e78fee10edc8e1
Tuva Ødegård <tuva.odegard@sparebank1.no>
2024-05-21 13:59:33 +0200

    docs(component-overview): Fikset ikon i headingen

--- component-overview/examples/header/header.jsx
+++ component-overview/examples/header/header.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from 'react';
+import { Icon } from '@sb1/ffe-icons-react';
+import classNames from 'classnames';
 
 () => {
     const [isUserNavOpen, setIsUserNavOpen] = useState(false);
+
+    const expandMoreIcon =
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjQiPjxwYXRoIGQ9Ik00ODAtMzczLjUzOXEtNy4yMzEgMC0xMy40NjEtMi4zMDgtNi4yMzEtMi4zMDgtMTEuODQ2LTcuOTIzTDI3NC45MjQtNTYzLjUzOXEtOC4zMDgtOC4zMDctOC41LTIwLjg4NC0uMTkzLTEyLjU3NyA4LjUtMjEuMjY5IDguNjkyLTguNjkyIDIxLjA3Ni04LjY5MnQyMS4wNzYgOC42OTJMNDgwLTQ0Mi43NjhsMTYyLjkyNC0xNjIuOTI0cTguMzA3LTguMzA3IDIwLjg4NC04LjUgMTIuNTc2LS4xOTIgMjEuMjY4IDguNSA4LjY5MyA4LjY5MiA4LjY5MyAyMS4wNzcgMCAxMi4zODQtOC42OTMgMjEuMDc2TDUwNS4zMDctMzgzLjc3cS01LjYxNSA1LjYxNS0xMS44NDYgNy45MjMtNi4yMyAyLjMwOC0xMy40NjEgMi4zMDhaIi8+PC9zdmc+';
+
     return (
         <div>
             <header className="ffe-header">
@@ -71,20 +77,23 @@ import { useState } from 'react';
                             <span className="ffe-header__user-name">
                                 Jomar Beate Skrothaug
                                 <span className="ffe-header__user-chevron">
-                                    <span
-                                        className="ffe-symbol ffe-symbol--400 ffe-symbol--md ffe-header__svg-icon ffe-header__user-chevron-icon"
-                                        role="graphics-symbol img"
-                                        aria-label='utvid'
-                                    >
-                                        expand_more
-                                    </span>
+                                    <Icon
+                                        fileUrl={expandMoreIcon}
+                                        className={classNames(
+                                            'ffe-header__user-chevron-icon',
+                                            {
+                                                'ffe-header__user-chevron--expanded':
+                                                    isUserNavOpen,
+                                            },
+                                        )}
+                                    />
                                 </span>
                             </span>
                             <div className="ffe-header__svg-icon ffe-header__user-icon">
                                 <span
                                     className="ffe-symbol ffe-symbol--400 ffe-symbol--xl ffe-header__svg-icon ffe-header__user-icon"
                                     role="graphics-symbol img"
-                                    aria-label='bruker'
+                                    aria-label="bruker"
                                 >
                                     person
                                 </span>
@@ -92,14 +101,13 @@ import { useState } from 'react';
                                     5
                                 </span>
                             </div>
-
                         </button>
                     </div>
 
                     <div className="ffe-header__site-nav-toggle">
                         <button className="ffe-header__icon-button ffe-header__icon-button--site-nav">
                             <span className="ffe-header__site-nav-hamburger">
-                                <span className="ffe-header__site-nav-hamburger__bar" />
+                                <span className="ffe-header__site-nav-hamburger-bar" />
                             </span>
                         </button>
                     </div>
@@ -109,8 +117,13 @@ import { useState } from 'react';
                     <div className="ffe-header__wrapper">
                         <nav className="ffe-header__user-nav" role="navigation">
                             <ul
-                                className={`ffe-header__list ffe-header__user-nav-list ${isUserNavOpen &&
-                                    'ffe-header__user-nav-list--visible'}`}
+                                className={classNames(
+                                    'ffe-header__list ffe-header__user-nav-list',
+                                    {
+                                        'ffe-header__user-nav-list--visible':
+                                            isUserNavOpen,
+                                    },
+                                )}
                             >
                                 <li className="ffe-header__list-item">
                                     <a className="ffe-header__link" href="#">
@@ -209,4 +222,4 @@ import { useState } from 'react';
             </div>
         </div>
     );
-}
+};
