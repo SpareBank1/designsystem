f85fa077ae27fec7e018217f477e78ad8db0d6dd
Tuva Ødegård <tuva.odegard@sparebank1.no>
2024-05-27 16:29:20 +0200

    docs(component-overview): Legger til StrongMicroText eksempel

--- component-overview/examples/typography/MicroText.jsx
+++ component-overview/examples/typography/MicroText.jsx
@@ -1,3 +1,8 @@
 import { MicroText } from '@sb1/ffe-core-react';
-
-<MicroText>Dette er den minste typen vi har</MicroText>
+<>
+    <MicroText>Dette er den minste typen vi har</MicroText>
+    <br />
+    <MicroText strong={true}>
+        Dette er den minste typen vi har, men i en tykkere variant
+    </MicroText>
+</>;
--- component-overview/examples/typography/StrongMicroText.jsx
+++
@@ -1,3 +0,0 @@
-import { MicroText } from '@sb1/ffe-core-react';
-
-<MicroText>Dette er den minste typen vi har</MicroText>
