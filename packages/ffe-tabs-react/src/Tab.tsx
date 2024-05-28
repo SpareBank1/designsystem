98ddca869f4b435a249c1232f3d1409abd87e467
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-05-28 09:11:49 +0200

    feat(ffe-tabs-react): vise refs

--- packages/ffe-tabs-react/src/Tab.tsx
+++ packages/ffe-tabs-react/src/Tab.tsx
@@ -5,18 +5,21 @@ export interface TabProps extends React.ComponentPropsWithoutRef<'button'> {
     selected?: boolean;
 }
 
-export const Tab: React.FC<TabProps> = ({ className, selected, ...rest }) => {
-    return (
-        <button
-            type="button"
-            role="tab"
-            aria-selected={selected}
-            className={classNames(
-                'ffe-tab',
-                { 'ffe-tab--selected': selected },
-                className,
-            )}
-            {...rest}
-        />
-    );
-};
+export const Tab = React.forwardRef<HTMLButtonElement, TabProps>(
+    ({ className, selected, ...rest }, ref) => {
+        return (
+            <button
+                ref={ref}
+                type="button"
+                role="tab"
+                aria-selected={selected}
+                className={classNames(
+                    'ffe-tab',
+                    { 'ffe-tab--selected': selected },
+                    className,
+                )}
+                {...rest}
+            />
+        );
+    },
+);
--- packages/ffe-tabs-react/src/TabGroup.tsx
+++ packages/ffe-tabs-react/src/TabGroup.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { RefObject, useEffect, useState } from 'react';
 import classNames from 'classnames';
+import { mergeRefs } from './mergeRefs';
+import { TabProps } from './Tab';
 
-export interface TabGroupProps extends React.ComponentPropsWithoutRef<'div'> {
+export interface TabGroupProps
+    extends Omit<React.ComponentPropsWithoutRef<'div'>, 'children'> {
     /** Applies the noBreak modifier to avoid it collapsing over multiple lines */
     noBreak?: boolean;
+    children: React.ReactElement<TabProps>[];
 }
 
 export const TabGroup: React.FC<TabGroupProps> = ({
     className,
     noBreak,
+    children,
     ...rest
 }) => {
+    const [tabs, setTabs] = useState<
+        RefObject<HTMLButtonElement>[] | null | undefined
+    >(React.Children.map(children, () => React.createRef()) ?? []);
+
+    useEffect(() => {
+        setTabs(React.Children.map(children, () => React.createRef()));
+    }, [children]);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowRight') {
+            console.log(tabs?.[1].current);
+        }
+        if (e.key === 'ArrowLeft') {
+            console.log(tabs?.[0].current);
+        }
+    };
+
     return (
+        // eslint-disable-next-line jsx-a11y/interactive-supports-focus
         <div
             className={classNames(
                 'ffe-tab-group',
@@ -19,7 +42,18 @@ export const TabGroup: React.FC<TabGroupProps> = ({
                 className,
             )}
             role="tablist"
+            onKeyDown={handleKeyDown}
             {...rest}
-        />
+        >
+            {React.Children.map(
+                children,
+                (child, index) =>
+                    React.isValidElement(child) &&
+                    React.cloneElement(child, {
+                        ...child.props,
+                        ref: mergeRefs([child.props.ref, tabs?.[index]]),
+                    }),
+            )}
+        </div>
     );
 };
