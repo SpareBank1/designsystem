8ca9d1c620d7d42b584f8873f5117c7f942ea53c
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2024-05-29 10:43:43 +0200

    feat(component-overview): legg til eksempel for cardbase no padding

---
+++ component-overview/examples/cards/CardBase-no-padding.jsx
@@ -0,0 +1,10 @@
+import { CardBase } from '@sb1/ffe-cards-react';
+
+<CardBase
+    as="div"
+    bgColor="syrin-30"
+    bgDarkmodeColor="koksgraa"
+    noPadding={true}
+>
+    Dette er ett kort uten padding
+</CardBase>;
