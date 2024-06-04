938bc1d9807f1552118992d9d4f2247c636bef9f
Tuva Ødegård <tuva.odegard@sparebank1.no>
2024-06-04 12:40:10 +0200

    fix(ffe-account-selector-react): åpne currencycode for andre koder enn bare nok og eur

--- packages/ffe-account-selector-react/src/types.ts
+++ packages/ffe-account-selector-react/src/types.ts
@@ -1,8 +1,36 @@
 export type Locale = 'nb' | 'nn' | 'en';
 
+type Letter =
+    | 'a'
+    | 'b'
+    | 'c'
+    | 'd'
+    | 'e'
+    | 'f'
+    | 'g'
+    | 'h'
+    | 'i'
+    | 'j'
+    | 'k'
+    | 'l'
+    | 'm'
+    | 'n'
+    | 'o'
+    | 'p'
+    | 'q'
+    | 'r'
+    | 's'
+    | 't'
+    | 'u'
+    | 'v'
+    | 'w'
+    | 'x'
+    | 'y'
+    | 'z';
+
 export interface Account {
     accountNumber: string;
     name: string;
-    currencyCode?: 'NOK' | 'EUR';
+    currencyCode?: `${Uppercase<Letter>}${Uppercase<Letter>}${Uppercase<Letter>}`;
     balance?: number;
 }
