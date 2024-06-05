a12a9615f085366422aef5343b7f52be5bc95212
Anders <anders.johnsen@gmail.com>
2024-06-05 14:03:41 +0200

    feat(ffe-grid-react): legger til støtte for responsiv spacing

--- packages/ffe-grid-react/src/Grid.tsx
+++ packages/ffe-grid-react/src/Grid.tsx
@@ -1,25 +1,59 @@
 import React, { ElementType } from 'react';
 import classNames from 'classnames';
-import { ComponentWithoutRefAsPropParams } from './types';
+import { ComponentWithoutRefAsPropParams, Gap } from './types';
+
+type SizeModifier = GridGapSize | Gap;
+
+interface GridGapSize {
+    gap?: Gap;
+}
 
 export type GridProps<As extends ElementType = any> =
     ComponentWithoutRefAsPropParams<As> & {
         /** Specify the internal gutter of the grid */
-        gap?: 'none' | '2xs' | 'xs' | 'sm' | 'md' | 'lg';
+        gap?: Gap;
+        /** Size modifiers for small screen sizes */
+        sm?: SizeModifier;
+        /** Size modifiers for medium screen sizes */
+        md?: SizeModifier;
+        /** Size modifiers for large screen sizes */
+        lg?: SizeModifier;
     };
+
+const sizeClasses = (size: 'none' | 'sm' | 'md' | 'lg', def?: SizeModifier) => {
+    if (def === undefined) {
+        return null;
+    } else if (typeof def === 'string') {
+        return `ffe-grid--gap-${size}-${def}`;
+    }
+
+    return classNames({
+        [`ffe-grid--${size}-gap-${def.gap}`]: def.gap,
+    });
+};
 
 export const Grid: React.FC<GridProps> = ({
     children,
     className,
     gap,
+    sm,
+    md,
+    lg,
     as: Comp = 'div',
     ...rest
 }) => {
     return (
         <Comp
-            className={classNames(className, 'ffe-grid', {
-                [`ffe-grid--gap-${gap}`]: gap,
-            })}
+            className={classNames(
+                className,
+                'ffe-grid',
+                {
+                    [`ffe-grid--gap-${gap}`]: gap,
+                },
+                sizeClasses('lg', lg),
+                sizeClasses('md', md),
+                sizeClasses('sm', sm),
+            )}
             {...rest}
         >
             {children}
