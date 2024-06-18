979e0f80b42c8b64e9ae40be143eec4cf4fb295a
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-06-18 11:47:53 +0200

    chore(buildtool): remove Enzyme

--- buildtool/config/jest.setup.js
+++ buildtool/config/jest.setup.js
@@ -1,11 +1,1 @@
-// do not import these render functions yourself in your test case, or react
-// will be get confused and cannot find enzyme.
-import { configure, shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import '@testing-library/jest-dom';
-
-configure({ adapter: new Adapter() });
-
-global.shallow = shallow;
-global.render = render;
-global.mount = mount;
