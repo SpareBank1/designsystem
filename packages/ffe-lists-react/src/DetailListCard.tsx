fa90d69fbdf6b72ec0d5286adbab40dccaa8bbeb
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2024-07-02 14:20:24 +0200

    feat(ffe-lists-react): legg til bgColor og bgDarkmodeColor props

--- packages/ffe-lists-react/src/DetailListCard.tsx
+++ packages/ffe-lists-react/src/DetailListCard.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import classNames from 'classnames';
 
 export interface DetailListCardProps
-    extends React.ComponentPropsWithoutRef<'dl'> {}
+    extends React.ComponentPropsWithoutRef<'dl'> {
+    bgColor?: 'sand-30' | 'sand-70' | 'frost-30' | 'syrin-30' | 'syrin-70';
+    bgDarkmodeColor?: 'natt' | 'svart' | 'koksgraa' | 'grey-charcoal';
+}
 
 export const DetailListCard: React.FC<DetailListCardProps> = ({
     className,
+    bgColor,
+    bgDarkmodeColor,
     children,
     ...rest
 }) => {
     return (
-        <dl className={classNames('ffe-detail-list-card', className)} {...rest}>
+        <dl
+            className={classNames('ffe-detail-list-card', className, {
+                [`ffe-detail-list-card--bg-${bgColor}`]: bgColor,
+                [`ffe-detail-list-card--dm-bg-${bgDarkmodeColor}`]:
+                    bgDarkmodeColor,
+            })}
+            {...rest}
+        >
             {children}
         </dl>
     );
