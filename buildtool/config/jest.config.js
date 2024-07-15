2a9d5515a47cc11c7178aa441c8149d53d21e896
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-07-15 13:52:55 +0200

    feat(buildtool): no more babel support
    BREAKING CHANGE: babel commands removed

--- buildtool/config/jest.config.js
+++ buildtool/config/jest.config.js
@@ -3,15 +3,7 @@ const path = require('path');
 module.exports = {
     preset: 'ts-jest',
     rootDir: path.join(process.cwd(), 'src'),
-    setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.js')],
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
     modulePathIgnorePatterns: ['test'],
     testEnvironment: 'jsdom',
-    transform: {
-        '\\.js$': [
-            'babel-jest',
-            {
-                configFile: path.join(__dirname, 'babel.config.js'),
-            },
-        ],
-    },
 };
--- buildtool/src/index.js
+++ buildtool/src/index.js
@@ -1,38 +1,9 @@
 const { Command } = require('commander');
-const babelCommand = require('./commands/babelCommand');
-const copyfileCommand = require('./commands/copyfileCommand');
 const jestCommand = require('./commands/jestCommand');
 const stylelintCommand = require('./commands/stylelintCommand');
 const tscCommand = require('./commands/tscCommand');
 
 const program = new Command();
-
-program
-    .command('babel')
-    .description('compile code with babel')
-    .argument('[source]', 'directory with source code', 'src')
-    .option('--cjs <cjsDir>', 'where to output commonjs modules', 'lib')
-    .option('--no-cjs', 'do not create commonjs')
-    .option('--es <esDir>', 'where to output es modules', 'es')
-    .option('--no-es', 'do not es modules')
-    .option(
-        '--copy-typedef <typeDefFile>',
-        'copy typescript definitions',
-        'src/index.d.ts',
-    )
-    .option('--no-copy-typedef', 'do not copy typedef definitions')
-    .option(
-        '--copy-typedef-outdir <outdir>',
-        'where to copy typescript definitions to',
-        'types',
-    )
-    .action((source, options) => {
-        babelCommand({ source, ...options });
-
-        if (options.copyTypedef) {
-            copyfileCommand(options.copyTypedef, options.copyTypedefOutdir);
-        }
-    });
 
 program
     .command('jest')
