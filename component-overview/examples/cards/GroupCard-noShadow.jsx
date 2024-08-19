2bae659906152283ad1e18adb857a976acebc51c
Tuva Ødegård <tuva.odegard@sparebank1.no>
2024-08-19 12:12:32 +0200

    docs(component-overview): groupcard, fjernet nopadding og oppdatert utseende

--- component-overview/examples/cards/GroupCard-noShadow.jsx
+++ component-overview/examples/cards/GroupCard-noShadow.jsx
@@ -2,7 +2,7 @@ import { GroupCard, GroupCardElement } from '@sb1/ffe-cards-react';
 
 () => {
     return (
-        <GroupCard shadow={false}>
+        <GroupCard noShadow={true}>
             <GroupCardElement>Innhold nr 1</GroupCardElement>
             <GroupCardElement>Innhold nr 2</GroupCardElement>
             <GroupCardElement>Innhold nr 3</GroupCardElement>
--- component-overview/examples/cards/GroupCard.jsx
+++ component-overview/examples/cards/GroupCard.jsx
@@ -1,11 +1,27 @@
-import { GroupCard, GroupCardElement } from '@sb1/ffe-cards-react';
+import {
+    GroupCard,
+    GroupCardElement,
+    GroupCardFooter,
+    GroupCardTitle,
+} from '@sb1/ffe-cards-react';
+import { Heading2 } from '@sb1/ffe-core-react';
 
 () => {
     return (
-        <GroupCard shadow={true}>
+        <GroupCard>
+            <GroupCardTitle>
+                <Heading2 lookLike={5}>Tittel på gruppe</Heading2>
+            </GroupCardTitle>
             <GroupCardElement>Dette er et element i GroupCard</GroupCardElement>
             <GroupCardElement>Dette er et element i GroupCard</GroupCardElement>
             <GroupCardElement>Dette er et element i GroupCard</GroupCardElement>
+            <GroupCardFooter>
+                {({ CardAction }) => (
+                    <CardAction href="https://design.sparebank1.no">
+                        Vis mer
+                    </CardAction>
+                )}
+            </GroupCardFooter>
         </GroupCard>
     );
 };
