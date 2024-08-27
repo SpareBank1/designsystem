281caa1bfeef4f83c1c0fe1a6b48ffdc20613e92
Tuva Ødegård <tuva.odegard@sparebank1.no>
2024-08-27 15:01:47 +0200

    fix(component-overview): endre fra noShadow til shadow

--- component-overview/examples/cards/GroupCard-noShadow.jsx
+++ component-overview/examples/cards/GroupCard-noShadow.jsx
@@ -2,7 +2,7 @@ import { GroupCard, GroupCardElement } from '@sb1/ffe-cards-react';
 
 () => {
     return (
-        <GroupCard noShadow={true}>
+        <GroupCard shadow={false}>
             <GroupCardElement>Innhold nr 1</GroupCardElement>
             <GroupCardElement>Innhold nr 2</GroupCardElement>
             <GroupCardElement>Innhold nr 3</GroupCardElement>
