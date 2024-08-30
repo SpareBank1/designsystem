f7827d017ce7f4a4efb189a445ac7fe60baa6ee9
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2024-08-30 13:09:07 +0200

    feat(component-overview): legg til noMargin eksempel på paragraph

---
+++ component-overview/examples/typography/Paragraph-noMargin.jsx
@@ -0,0 +1,12 @@
+import { Paragraph } from '@sb1/ffe-core-react';
+
+<>
+    <Paragraph noMargin={true}>
+        Velg en av bankene våre, og bruk BankID for å bli kunde. Nettbank og
+        mobilbank får du med en gang, og bankkortet kommer i posten om en ukes
+        tid.
+    </Paragraph>
+    <Paragraph noMargin={true}>
+        Denne paragrafen viser at den over ikke lenger har noen margin
+    </Paragraph>
+</>
