5b6e08b5707aeb8ef69d67a71272394a2705413e
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-09-06 21:15:51 +0200

    chore(ffe-accordion-react): storybook example

---
+++ packages/ffe-accordion-react/src/Accordion.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Accordion } from './Accordion';
+import { AccordionItem } from './AccordionItem';
+import type { StoryObj, Meta } from '@storybook/react';
+
+const meta: Meta<typeof Accordion> = {
+    title: 'components/accordion/Accordion',
+    component: Accordion,
+    tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof Accordion>;
+
+export const Standard: Story = {
+    args: {
+        headingLevel: 2,
+    },
+    render: args => (
+        <Accordion {...args}>
+            <AccordionItem heading="Tittel">Skjult innhold</AccordionItem>
+            <AccordionItem heading={<span>Enda en tittel</span>}>
+                Mer skjult innhold
+            </AccordionItem>
+            <AccordionItem heading="En siste tittel">
+                Enda mer innhold
+            </AccordionItem>
+        </Accordion>
+    ),
+};
