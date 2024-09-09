54befff259f7e552917515f550eb98c2fc206584
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-09-09 14:35:51 +0200

    chore: darkmode toggle for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,7 +16,8 @@ const config: StorybookConfig = {
         getAbsolutePath('@storybook/addon-onboarding'),
         getAbsolutePath('@storybook/addon-links'),
         getAbsolutePath('@storybook/addon-essentials'),
-        getAbsolutePath('@storybook/addon-interactions'),
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('storybook-dark-mode'),
     ],
     framework: {
         name: getAbsolutePath('@storybook/react-vite'),
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,14 @@
 import './main.less';
-
+import { themes } from '@storybook/theming';
 import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
     parameters: {
+        darkMode: {
+            dark: { ...themes.dark },
+            light: { ...themes.normal },
+            darkClass: 'regard-color-scheme-preference',
+        },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
