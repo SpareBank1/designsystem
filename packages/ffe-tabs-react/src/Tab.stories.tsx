f1d2247969730dd016bc4d747922a4202a681f24
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-09-26 14:21:17 +0200

    docs(ffe-tabs-react): added storybook example

---
+++ packages/ffe-tabs-react/src/Tab.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Tab } from './Tab';
+import type { StoryObj, Meta } from '@storybook/react';
+
+const meta: Meta<typeof Tab> = {
+    title: 'components/tabs/Tab',
+    component: Tab,
+};
+export default meta;
+
+type Story = StoryObj<typeof Tab>;
+
+export const Standard: Story = {
+    args: { children: 'Dette er en fane' },
+    render: args => <Tab {...args} />,
+};
+
+export const Selected: Story = {
+    args: { ...Standard.args, selected: true },
+    render: args => <Tab {...args} />,
+};
