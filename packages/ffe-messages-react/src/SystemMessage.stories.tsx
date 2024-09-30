9d87e4440976a3437d576b5cec02a06821755686
Anders Johnsen <anders.johnsen@sparebank1.no>
2024-09-30 12:34:31 +0200

    docs(ffe-messages-react): legger til meldinger i storybook

---
+++ packages/ffe-messages-react/src/SystemMessage.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SystemMessage } from './SystemMessage';
+import type { StoryObj, Meta } from '@storybook/react';
+import { MessageIcon } from './MessageIcon';
+
+const meta: Meta<typeof SystemMessage<any>> = {
+    title: 'components/messages/SystemMessage',
+    component: SystemMessage,
+    subcomponents: { MessageIcon },
+};
+export default meta;
+
+type Story = StoryObj<typeof SystemMessage<any>>;
+
+export const Standard: Story = {
+    args: {
+        type: 'success',
+    },
+    render: args => (
+        <SystemMessage {...args}>
+            Systemmeldinger skal bare brukes til viktige, midlertidige
+            meldinger.
+        </SystemMessage>
+    ),
+};
