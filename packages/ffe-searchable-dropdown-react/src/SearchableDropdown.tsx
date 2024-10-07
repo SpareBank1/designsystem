f24c56614b768ea03c6f0564a4138c416364583b
Sebastian Nomme <sebastian.nomme@sparebank1.no>
2024-10-07 16:04:11 +0200

    Progress on swapping icons

--- packages/ffe-searchable-dropdown-react/src/SearchableDropdown.tsx
+++ packages/ffe-searchable-dropdown-react/src/SearchableDropdown.tsx
@@ -160,6 +160,8 @@ function SearchableDropdownWithForwardRef<Item extends Record<string, any>>(
     const shouldFocusToggleButton = useRef(false);
     const shouldFocusInput = useRef(false);
 
+    const isInputEmpty = state.inputValue.trim() === '';
+
     const handleInputClick = () => {
         dispatch({ type: 'InputClick' });
     };
@@ -376,7 +378,7 @@ function SearchableDropdownWithForwardRef<Item extends Record<string, any>>(
                     }
                     className={classNames('ffe-searchable-dropdown__button', {
                         'ffe-searchable-dropdown__button--flip':
-                            state.isExpanded,
+                            state.isExpanded && !isInputEmpty,
                     })}
                     onClick={() => {
                         dispatch({
@@ -386,6 +388,11 @@ function SearchableDropdownWithForwardRef<Item extends Record<string, any>>(
                 >
                     {isLoading ? (
                         <Spinner />
+                    ) : isInputEmpty ? (
+                        <Icon
+                            fileUrl="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJmZWF0aGVyIGZlYXRoZXItc2VhcmNoIj48Y2lyY2xlIGN4PSIxMSIgY3k9IjExIiByPSI4Ij48L2NpcmNsZT48bGluZSB4MT0iMjEiIHkxPSIyMSIgeDI9IjE2LjY1IiB5Mj0iMTYuNjUiPjwvbGluZT48L3N2Zz4="
+                            size="md"
+                        />
                     ) : (
                         <Icon
                             fileUrl="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjQiPjxwYXRoIGQ9Ik00ODAtMzczLjUzOXEtNy4yMzEgMC0xMy40NjEtMi4zMDgtNi4yMzEtMi4zMDgtMTEuODQ2LTcuOTIzTDI3NC45MjQtNTYzLjUzOXEtOC4zMDgtOC4zMDctOC41LTIwLjg4NC0uMTkzLTEyLjU3NyA4LjUtMjEuMjY5IDguNjkyLTguNjkyIDIxLjA3Ni04LjY5MnQyMS4wNzYgOC42OTJMNDgwLTQ0Mi43NjhsMTYyLjkyNC0xNjIuOTI0cTguMzA3LTguMzA3IDIwLjg4NC04LjUgMTIuNTc2LS4xOTIgMjEuMjY4IDguNSA4LjY5MyA4LjY5MiA4LjY5MyAyMS4wNzcgMCAxMi4zODQtOC42OTMgMjEuMDc2TDUwNS4zMDctMzgzLjc3cS01LjYxNSA1LjYxNS0xMS44NDYgNy45MjMtNi4yMyAyLjMwOC0xMy40NjEgMi4zMDhaIi8+PC9zdmc+"
