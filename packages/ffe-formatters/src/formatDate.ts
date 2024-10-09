83162869c6bed8860b2830bb7b1a4ee17759c43e
peter <peter.hellstrand83@gmail.com>
2024-10-09 22:05:27 +0200

    refactor(ffe-formatters): input to formatDate not validet
    BREAKING CHANGE: Typescript users are not affected by
    this change but for js users the input to formateDate are
    not validated any more.

--- packages/ffe-formatters/src/formatDate.ts
+++ packages/ffe-formatters/src/formatDate.ts
@@ -1,10 +1,6 @@
 const ensureTwoDigits = (d: number) => `00${d}`.slice(-2);
 
 export const formatDate = (timestamp: Date | number) => {
-    if (!(Number.isInteger(timestamp) || timestamp instanceof Date)) {
-        return null;
-    }
-
     const date = timestamp instanceof Date ? timestamp : new Date(timestamp);
     const dd = ensureTwoDigits(date.getDate());
     const mm = ensureTwoDigits(date.getMonth() + 1);
