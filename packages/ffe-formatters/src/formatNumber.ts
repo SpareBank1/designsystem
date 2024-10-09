3183e80705d28b22bda894697f71495d3a0f7bb5
Sara Idris <sara.elisabeth.idris@sparebank1.no>
2024-10-09 10:34:19 +0200

    Lagt til formatering for nummer basert på språket som er satt i banken

--- packages/ffe-formatters/src/formatNumber.ts
+++ packages/ffe-formatters/src/formatNumber.ts
@@ -1,26 +1,51 @@
-import { numberFormat } from 'underscore.string';
 import { NON_BREAKING_SPACE } from './internal/unicode';
 import { parseNumber } from './internal/parseNumber';
+import { numberFormat } from 'underscore.string';
+
+function customNumberFormat(
+    number: number,
+    decimals: number,
+    decimalMark: string,
+    thousandSeparator: string,
+) {
+    const parts = number.toFixed(decimals).split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousandSeparator);
+    return parts.join(decimalMark);
+}
 
 export const formatNumber = (
     number: number | string | null | undefined,
     opts = {},
+    language?: string,
 ) => {
-    const { decimals, thousandSeparator, decimalMark } = {
-        decimals: 0,
-        thousandSeparator: NON_BREAKING_SPACE,
-        decimalMark: ',',
-        ...opts,
-    };
+    let decimals: number;
+    let thousandSeparator: string;
+    let decimalMark: string;
 
-    const toFormat = parseNumber(number);
+    if (language === 'en') {
+        ({ decimals, thousandSeparator, decimalMark } = {
+            decimals: 0,
+            thousandSeparator: ',',
+            decimalMark: '.',
+            ...opts,
+        });
+    } else {
+        ({ decimals, thousandSeparator, decimalMark } = {
+            decimals: 0,
+            thousandSeparator: NON_BREAKING_SPACE,
+            decimalMark: ',',
+            ...opts,
+        });
+    }
+
+    const toFormat = parseNumber(number, language);
     if (typeof toFormat !== 'number') {
         return number;
     }
-    return `${numberFormat(
+    return customNumberFormat(
         toFormat,
         decimals,
         decimalMark,
         thousandSeparator,
-    )}`;
+    );
 };
--- packages/ffe-formatters/src/internal/parseNumber.ts
+++ packages/ffe-formatters/src/internal/parseNumber.ts
@@ -1,11 +1,22 @@
-export const parseNumber = (number: number | string | null | undefined) => {
+export const parseNumber = (
+    number: number | string | null | undefined,
+    language?: string,
+) => {
     if (!number || typeof number === 'number') {
         return number;
     }
 
-    const parsed = parseFloat(
-        number.replace(/[^\d,.-]/g, '').replace(/,/g, '.'),
-    );
+    let numberString: string;
+
+    if (language === 'en') {
+        numberString = number.replace(/[^\d,.-]/g, '').replace(/,/g, '.');
+    } else {
+        numberString = number
+            .replace(/[^\d,.-]/g, '')
+            .replace(/,(?=[^.]*$)/, '.');
+    }
+
+    const parsed = parseFloat(numberString);
     if (Number.isNaN(parsed)) {
         return null;
     }
