7a87f46c119d935f2f85a6d12c7a0e15938b2bcb
peter <peter.hellstrand83@gmail.com>
2024-10-12 13:31:36 +0200

    feat(.storybook): use ffe components for markdown

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import './main.less';
 import './preview.less';
-
 import type { Preview } from '@storybook/react';
+import {
+    Paragraph,
+    Heading1,
+    Heading2,
+    Heading3,
+    Heading4,
+    Heading5,
+    Heading6,
+    LinkText,
+    StrongText,
+    EmphasizedText,
+} from '@sb1/ffe-core-react';
+import { BulletList, BulletListItem } from '@sb1/ffe-lists-react';
+
+type Props = Record<string, unknown>;
+
+const components = {
+    h1: (props: Props) => <Heading1 {...props} className="sb-unstyled" />,
+    h2: (props: Props) => <Heading2 {...props} className="sb-unstyled" />,
+    h3: (props: Props) => <Heading3 {...props} className="sb-unstyled" />,
+    h4: (props: Props) => <Heading4 {...props} className="sb-unstyled" />,
+    h5: (props: Props) => <Heading5 {...props} className="sb-unstyled" />,
+    h6: (props: Props) => <Heading6 {...props} className="sb-unstyled" />,
+    p: (props: Props) => <Paragraph {...props} className="sb-unstyled" />,
+    ul: (props: Props) => <BulletList {...props} className="sb-unstyled" />,
+    li: (props: Props) => <BulletListItem {...props} className="sb-unstyled" />,
+    a: (props: Props) => <LinkText {...props} className="sb-unstyled" />,
+    strong: (props: Props) => <StrongText {...props} className="sb-unstyled" />,
+    em: (props: Props) => <EmphasizedText {...props} className="sb-unstyled" />,
+};
 
 const preview: Preview = {
     parameters: {
@@ -11,6 +40,9 @@ const preview: Preview = {
                 color: /(background|color)$/i,
                 date: /Date$/i,
             },
+        },
+        docs: {
+            components,
         },
     },
     decorators: [
