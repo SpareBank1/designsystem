843d83de33cdb8cf745712f2408217ae95f3d07c
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-10-14 14:27:13 +0200

    fix(component-overview): add locale to examples

--- component-overview/examples/formatting/Numbers.jsx
+++ component-overview/examples/formatting/Numbers.jsx
@@ -1,14 +1,13 @@
 import { formatNumber } from '@sb1/ffe-formatters';
 
 <>
-    <div>{formatNumber(1000000)}</div>
-    <div>{formatNumber(1234.567)}</div>
-    <div>{formatNumber(1234.567, { decimals: 2 })}</div>
+    <div>{formatNumber(1000000, { locale: 'nb' })}</div>
+    <div>{formatNumber(1234.567, { locale: 'nb' })}</div>
+    <div>{formatNumber(1234.567, { decimals: 2, locale: 'nb' })}</div>
     <div>
         {formatNumber(1234.567, {
             decimals: 2,
-            thousandSeparator: ',',
-            decimalMark: '.',
+            locale: 'en',
         })}
     </div>
-</>
+</>;
