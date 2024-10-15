fa89c2e3d4da41588ef01198d73bd0a7af2dc55a
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-10-15 14:51:07 +0200

    refactor(ffe-searchable-dropdown-react): refactor css

--- packages/ffe-searchable-dropdown-react/src/SearchableDropdown.tsx
+++ packages/ffe-searchable-dropdown-react/src/SearchableDropdown.tsx
@@ -318,13 +318,12 @@ function SearchableDropdownWithForwardRef<Item extends Record<string, any>>(
             onMouseDown={addFlagOnEventHandler}
             onFocus={addFlagOnEventHandler}
         >
-            <div>
+            <div className="ffe-searchable-dropdown__input">
                 <input
                     {...inputProps}
                     ref={mergeRefs([inputRef, ref])}
                     id={id}
                     aria-labelledby={labelledById}
-                    className="ffe-input-field"
                     onClick={handleInputClick}
                     onChange={e => {
                         if (inputProps?.onChange) {
@@ -366,68 +365,70 @@ function SearchableDropdownWithForwardRef<Item extends Record<string, any>>(
                     }
                     aria-invalid={rest['aria-invalid'] ?? ariaInvalid}
                 />
-                <button
-                    type="button"
-                    ref={toggleButtonRef}
-                    aria-label={
-                        state.isExpanded
-                            ? getButtonLabelClose(locale)
-                            : getButtonLabelOpen(locale)
-                    }
-                    className={classNames('ffe-searchable-dropdown__button', {
-                        'ffe-searchable-dropdown__button--flip':
-                            state.isExpanded,
-                    })}
-                    onClick={() => {
-                        dispatch({
-                            type: 'ToggleButtonPressed',
-                        });
-                    }}
-                >
-                    {isLoading ? (
-                        <Spinner />
-                    ) : (
-                        <Icon
-                            fileUrl="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjQiPjxwYXRoIGQ9Ik00ODAtMzczLjUzOXEtNy4yMzEgMC0xMy40NjEtMi4zMDgtNi4yMzEtMi4zMDgtMTEuODQ2LTcuOTIzTDI3NC45MjQtNTYzLjUzOXEtOC4zMDgtOC4zMDctOC41LTIwLjg4NC0uMTkzLTEyLjU3NyA4LjUtMjEuMjY5IDguNjkyLTguNjkyIDIxLjA3Ni04LjY5MnQyMS4wNzYgOC42OTJMNDgwLTQ0Mi43NjhsMTYyLjkyNC0xNjIuOTI0cTguMzA3LTguMzA3IDIwLjg4NC04LjUgMTIuNTc2LS4xOTIgMjEuMjY4IDguNSA4LjY5MyA4LjY5MiA4LjY5MyAyMS4wNzcgMCAxMi4zODQtOC42OTMgMjEuMDc2TDUwNS4zMDctMzgzLjc3cS01LjYxNSA1LjYxNS0xMS44NDYgNy45MjMtNi4yMyAyLjMwOC0xMy40NjEgMi4zMDhaIi8+PC9zdmc+"
-                            size="md"
-                            className="ffe-searchable-dropdown__button-icon"
-                        />
-                    )}
-                </button>
             </div>
-            <div
-                tabIndex={-1}
-                className={classNames('ffe-searchable-dropdown__list', {
-                    'ffe-searchable-dropdown__list--open': state.isExpanded,
+
+            <button
+                type="button"
+                ref={toggleButtonRef}
+                aria-label={
+                    state.isExpanded
+                        ? getButtonLabelClose(locale)
+                        : getButtonLabelOpen(locale)
+                }
+                className={classNames('ffe-searchable-dropdown__button', {
+                    'ffe-searchable-dropdown__button--flip': state.isExpanded,
                 })}
+                onClick={() => {
+                    dispatch({
+                        type: 'ToggleButtonPressed',
+                    });
+                }}
             >
-                <div ref={listBoxRef} id={`${id}-listbox`} role="listbox">
-                    {state.isExpanded && (
-                        <Results
-                            listToRender={state.listToRender}
-                            ListItemBodyElement={ListItemBodyElement}
-                            highlightedIndex={state.highlightedIndex}
-                            dropdownAttributes={dropdownAttributes}
-                            locale={locale}
-                            refs={refs}
-                            onChange={item => {
-                                shouldFocusToggleButton.current = true;
-                                dispatch({
-                                    type: 'ItemOnClick',
-                                    payload: { selectedItem: item },
-                                });
-                                onChange?.(item);
-                            }}
-                            noMatch={state.noMatch ? noMatch : undefined}
-                            noMatchMessageId={noMatchMessageId}
-                            selectedItem={state.selectedItem}
-                        />
-                    )}
-                    {postListElement && (
-                        <div className="ffe-searchable-dropdown__list--post-list-element">
-                            {postListElement}
-                        </div>
-                    )}
+                {isLoading ? (
+                    <Spinner />
+                ) : (
+                    <Icon
+                        fileUrl="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgLTk2MCA5NjAgOTYwIiB3aWR0aD0iMjQiPjxwYXRoIGQ9Ik00ODAtMzczLjUzOXEtNy4yMzEgMC0xMy40NjEtMi4zMDgtNi4yMzEtMi4zMDgtMTEuODQ2LTcuOTIzTDI3NC45MjQtNTYzLjUzOXEtOC4zMDgtOC4zMDctOC41LTIwLjg4NC0uMTkzLTEyLjU3NyA4LjUtMjEuMjY5IDguNjkyLTguNjkyIDIxLjA3Ni04LjY5MnQyMS4wNzYgOC42OTJMNDgwLTQ0Mi43NjhsMTYyLjkyNC0xNjIuOTI0cTguMzA3LTguMzA3IDIwLjg4NC04LjUgMTIuNTc2LS4xOTIgMjEuMjY4IDguNSA4LjY5MyA4LjY5MiA4LjY5MyAyMS4wNzcgMCAxMi4zODQtOC42OTMgMjEuMDc2TDUwNS4zMDctMzgzLjc3cS01LjYxNSA1LjYxNS0xMS44NDYgNy45MjMtNi4yMyAyLjMwOC0xMy40NjEgMi4zMDhaIi8+PC9zdmc+"
+                        size="md"
+                        className="ffe-searchable-dropdown__button-icon"
+                    />
+                )}
+            </button>
+            <div className="ffe-searchable-dropdown__list-container">
+                <div
+                    tabIndex={-1}
+                    className={classNames('ffe-searchable-dropdown__list', {
+                        'ffe-searchable-dropdown__list--open': state.isExpanded,
+                    })}
+                >
+                    <div ref={listBoxRef} id={`${id}-listbox`} role="listbox">
+                        {state.isExpanded && (
+                            <Results
+                                listToRender={state.listToRender}
+                                ListItemBodyElement={ListItemBodyElement}
+                                highlightedIndex={state.highlightedIndex}
+                                dropdownAttributes={dropdownAttributes}
+                                locale={locale}
+                                refs={refs}
+                                onChange={item => {
+                                    shouldFocusToggleButton.current = true;
+                                    dispatch({
+                                        type: 'ItemOnClick',
+                                        payload: { selectedItem: item },
+                                    });
+                                    onChange?.(item);
+                                }}
+                                noMatch={state.noMatch ? noMatch : undefined}
+                                noMatchMessageId={noMatchMessageId}
+                                selectedItem={state.selectedItem}
+                            />
+                        )}
+                        {postListElement && (
+                            <div className="ffe-searchable-dropdown__list--post-list-element">
+                                {postListElement}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
