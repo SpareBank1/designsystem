c1dfa250378ad7f1550f30d2db37667aad83d5e7
Peter Hellstrand <peter.hellstrand@sparebank1.no>
2024-10-21 12:10:44 +0200

    chore(ffe-messages-react): sletter pakke tilfeldig

--- packages/ffe-messages-react/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { SystemMessage, SystemMessageProps } from './SystemMessage';
-export { ContextMessage, ContextMessageProps } from './ContextMessage';
-export { MessageBox, MessageBoxProps } from './MessageBox';
-export { MessageHeader, MessageHeaderProps } from './MessageHeader';
-export { MessageList, MessageListItem } from './MessageList';
