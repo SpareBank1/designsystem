3ca9b0c95ee9b30807765541e9ad0519f1eaf00e
Anders <anders.johnsen@sparebank1.no>
2024-11-11 16:14:12 +0100

    docs(ffe-dropdown-react): dropdown i storybook

---
+++ packages/ffe-dropdown-react/src/Dropdown.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Dropdown } from './Dropdown';
+import { InputGroup } from '@sb1/ffe-form-react';
+import type { StoryObj, Meta } from '@storybook/react';
+
+const meta: Meta<typeof Dropdown> = {
+    title: 'Komponenter/Dropdown/Dropdown',
+    component: Dropdown,
+};
+export default meta;
+
+type Story = StoryObj<typeof Dropdown>;
+
+export const Standard: Story = {
+    args: {
+        inline: false,
+    },
+    render: args => {
+        return (
+            <InputGroup label="Måned">
+                <Dropdown {...args}>
+                    <option value="jan">Januar</option>
+                    <option value="feb">Februar</option>
+                    <option value="mar">Mars</option>
+                    <option value="apr">April</option>
+                    <option value="mai">Mai</option>
+                </Dropdown>
+            </InputGroup>
+        );
+    },
+};
