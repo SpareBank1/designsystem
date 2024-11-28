693118ad24b4ccf6d90533e12c61ffed1411f08a
HeleneKassandra <helene.kassandra.juterud@sparebank1.no>
2024-11-28 10:32:26 +0100

    feat(ffe-cards-react): legg til rightImg-prop legger til muligheten til å ha ikon på høyreside, og for at man kan ha 2 ikoner i samme kort

--- packages/ffe-cards-react/src/StippledCard/StippledCard.stories.tsx
+++ packages/ffe-cards-react/src/StippledCard/StippledCard.stories.tsx
@@ -83,6 +83,68 @@ export const WithIcon: Story = {
     ),
 };
 
+export const WithRightIconOnly: Story = {
+    args: {
+        as: 'div',
+        rightImg: {
+            type: 'icon',
+            element: <Icon fileUrl="./icons/open/300/md/add.svg" size="md" />,
+        },
+    },
+    render: args => (
+        <StippledCard {...args}>
+            {({ CardName, Title, Subtext, Text }) => (
+                <>
+                    <CardName>CardName</CardName>
+                    <Title>Tittel</Title>
+                    <Subtext as="span">Subtext er grå</Subtext>
+                    <Text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </Text>
+                </>
+            )}
+        </StippledCard>
+    ),
+};
+
+export const With2Icons: Story = {
+    args: {
+        as: 'div',
+        img: {
+            type: 'icon',
+            element: (
+                <Icon fileUrl="./icons/open/300/xl/monitoring.svg" size="xl" />
+            ),
+        },
+        rightImg: {
+            type: 'icon',
+            element: <Icon fileUrl="./icons/open/300/md/add.svg" size="md" />,
+        },
+    },
+    render: args => (
+        <StippledCard {...args}>
+            {({ CardName, Title, Subtext, Text }) => (
+                <>
+                    <CardName>CardName</CardName>
+                    <Title>Tittel</Title>
+                    <Subtext as="span">Subtext er grå</Subtext>
+                    <Text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </Text>
+                </>
+            )}
+        </StippledCard>
+    ),
+};
+
 export const WithCustom: Story = {
     args: {
         as: 'div',
