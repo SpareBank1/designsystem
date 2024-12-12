a170b0b2c97748a4706c7e46f622515b21780708
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2024-12-12 11:10:08 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@05f30f25815eeffcdfb26b1db90886855f4f6181 🚀

---
+++ assets/AccountSelector-eCBiwmC1.js
@@ -0,0 +1,18 @@
+import{ae as e,af as a,ag as t,ah as d}from"./index-COJpopas.js";import{useMDXComponents as i}from"./index-CVdx34fM.js";import{A as l,S as r,a as c,b as m,N as u,L as p,H as k,P as j}from"./AccountSelector.stories-nIWYp7b8.js";import"./iframe-Ca0gGwRB.js";import"../sb-preview/runtime.js";import"./index-eCxJ45ll.js";import"./index-BVyQ2LRr.js";import"./index-CNjFNOVn.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DezCrw0f.js";import"./texts-BNZzZC2E.js";import"./index-BhB8jUJx.js";import"./index-DrzhKqaf.js";import"./Paragraph-Qjq98ToE.js";import"./Icon-B88nEPeu.js";import"./formatNumber-DejPEPKF.js";import"./MicroText-DhU_-SBY.js";import"./InputGroup-BoYo0rXD.js";import"./Collapse-CKwb5mfJ.js";function s(o){const n={code:"code",h1:"h1",p:"p",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(n.h1,{id:"accountselector",children:"AccountSelector"}),`
+`,e.jsx(n.p,{children:"Kontovelger brukes som navnet tilsier til å velge konto på de ulike platformene våre. Forskjellen på denne og en vanlig dropdown, er at den er tilpasset situasjoner der man ønsker å velge en konto, siden den vil vise valgt kontonummer og saldo, etter du har valgt et alternativ."}),`
+`,e.jsxs(n.p,{children:["I mobilbanken så er foretrukken løsning ",e.jsx(n.code,{children:"openAccountPicker"})," i stedet for denne."]}),`
+`,e.jsx(t,{of:r}),`
+`,e.jsx(d,{of:r}),`
+`,e.jsx(n.p,{children:"Kontovelger som også viser saldo/beløp"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsxs(n.p,{children:["Trenger du å tillate at brukere kan skrive inn sitt eget kontonummer kan du aktivere dette ved å sende med ",e.jsx(n.code,{children:"allowCustomAccount={true}"}),"."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsxs(n.p,{children:["Hvis du ikke ønsker at kontonummeret en bruker skriver inn skal formatteres, kan du skru det av med ",e.jsx(n.code,{children:"formatAccountNumber={false}"}),"."]}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å gjøre endringer på selve listevisningen, kan du gjøre det med ",e.jsx(n.code,{children:"listElementBody"}),"."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å skjule kontodetaljer, kan du bruke ",e.jsx(n.code,{children:"hideAccountDetails"}),"."]}),`
+`,e.jsx(t,{of:k}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å ha ekstra tekst på bunnen av lista, kan du bruke ",e.jsx(n.code,{children:"postListElement"}),"."]}),`
+`,e.jsx(t,{of:j})]})}function K(o={}){const{wrapper:n}={...i(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(s,{...o})}):s(o)}export{K as default};
---
+++ assets/Pagination-iPEoFjdd.js
@@ -0,0 +1,13 @@
+import{ae as e,af as a,ag as o,ah as p}from"./index-COJpopas.js";import{useMDXComponents as i}from"./index-CVdx34fM.js";import{P as m,S as s,W as l,N as d,a as c}from"./Pagination.stories-C0Dbc6Pp.js";import"./iframe-Ca0gGwRB.js";import"../sb-preview/runtime.js";import"./index-eCxJ45ll.js";import"./index-BVyQ2LRr.js";import"./index-CNjFNOVn.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DezCrw0f.js";import"./Paragraph-Qjq98ToE.js";import"./SecondaryButton-aa3aHmpA.js";import"./BaseButton-CEf8UDzm.js";import"./fixedForwardRef-BdaB0qYT.js";import"./Icon-B88nEPeu.js";import"./TableRow-Cxob1Dlz.js";import"./StrongText-DYs74pMv.js";import"./TableCaption-BfPxYegm.js";function t(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(n.h1,{id:"pagination",children:"Pagination"}),`
+`,e.jsx(n.p,{children:"Paginering benyttes til søkeresultater og lignende presentasjon av store mengder informasjon som ikke får plass på en enkelt side."}),`
+`,e.jsx(n.p,{children:"For de fleste scenarioer anbefales det å bruke infinite scroll, men paginering kan benyttes hvis det vurderes som bedre basert på brukerfeedback."}),`
+`,e.jsx(o,{of:s}),`
+`,e.jsx(p,{of:s}),`
+`,e.jsx(n.h2,{id:"paginering-med-tabelleksempel",children:"Paginering med tabelleksempel"}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(n.h2,{id:"variasjoner",children:"Variasjoner"}),`
+`,e.jsxs(n.p,{children:['Det originale eksempelet har tekstene "Neste" og "Forrige". Hvis man ikke ønsker tekst på knappene kan proppen ',e.jsx(n.code,{children:"noNavigationText"})," brukes."]}),`
+`,e.jsx(o,{of:d}),`
+`,e.jsxs(n.p,{children:["Og hvis man ikke ønsker å gi brukeren valg om størrelse på lista, kan man bruke ",e.jsx(n.code,{children:"showRowsPerPageText={false}"})," og la være å ha med ",e.jsx(n.code,{children:"rowsPerPageOptions"}),"."]}),`
+`,e.jsx(o,{of:c})]})}function X(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{X as default};
