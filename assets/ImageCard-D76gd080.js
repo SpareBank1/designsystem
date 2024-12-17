4ed72d81cef307337d6d1bfd76bc185e7712ed8b
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2024-12-17 15:10:42 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@a787feb4a72419af9c1c37b58e8a5801c7405a16 🚀

---
+++ assets/ImageCard-D76gd080.js
@@ -0,0 +1,7 @@
+import{ae as t,af as m,ag as a,ah as s}from"./index-C8VVPGfr.js";import{useMDXComponents as i}from"./index-CVdx34fM.js";import{I as d,S as o}from"./ImageCard.stories-IbhAPthX.js";import"./iframe-1I2PRVRO.js";import"../sb-preview/runtime.js";import"./index-eCxJ45ll.js";import"./index-BVyQ2LRr.js";import"./index-CNjFNOVn.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DezCrw0f.js";import"./WithCardAction-D8xJ0iKc.js";import"./Title-CCAmy5ta.js";function n(e){const r={h1:"h1",p:"p",...i(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:d}),`
+`,t.jsx(r.h1,{id:"imagecard",children:"ImageCard"}),`
+`,t.jsx(r.p,{children:`Bildekort er kort med en gitt maksbredde, minimumshøyde, og et topplasert bilde. Den tilbyr alle underkomponenter
+(CardName, Title, Subtext, Text og CardAction) og plasserer dem i gitt rekkefølge nedover i kortet, med teksten midtstilt,
+nedenfor bildet.`}),`
+`,t.jsx(a,{of:o}),`
+`,t.jsx(s,{of:o})]})}function S(e={}){const{wrapper:r}={...i(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(n,{...e})}):n(e)}export{S as default};
--- assets/StrongText-zXpqkByu.js
+++
@@ -1,5 +0,0 @@
-import{ae as t,af as i,ag as a,ah as m}from"./index-ChGXV_Uy.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{S as p,a as e}from"./StrongText.stories-B82vBz92.js";import"./iframe-CqX-w38p.js";import"../sb-preview/runtime.js";import"./index-eCxJ45ll.js";import"./index-BVyQ2LRr.js";import"./index-CNjFNOVn.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DezCrw0f.js";function r(o){const n={h1:"h1",p:"p",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
-`,t.jsx(n.h1,{id:"strongtext",children:"StrongText"}),`
-`,t.jsx(n.p,{children:"Bold tekst som brukes når vi vil utheve noe i en lengre tekst."}),`
-`,t.jsx(a,{of:e}),`
-`,t.jsx(m,{of:e})]})}function v(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{v as default};
