d49d6b48a7cc6e4f08244adcf73f265d591e87c0
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2024-12-19 13:02:46 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@982f16cd5ea279b758f2826c355b0ddf023c0c2f 🚀

---
+++ assets/Feedback-DX4zTcZg.js
@@ -0,0 +1,11 @@
+import{ae as e,af as s,ag as d,ah as a}from"./index-CmcPDUN2.js";import{useMDXComponents as i}from"./index-CVdx34fM.js";import{F as m,S as r}from"./Feedback.stories-ByEjr_pI.js";import"./iframe-Bu68wkuK.js";import"../sb-preview/runtime.js";import"./index-eCxJ45ll.js";import"./index-BVyQ2LRr.js";import"./index-CNjFNOVn.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DezCrw0f.js";import"./TertiaryButton-c9j_cgsc.js";import"./fixedForwardRef-BdaB0qYT.js";import"./Paragraph-Qjq98ToE.js";import"./InputGroup-B73IVGIl.js";import"./Collapse-CKwb5mfJ.js";import"./Icon-B88nEPeu.js";import"./ButtonGroup-C8Gx9PgT.js";import"./ActionButton-Dg03nuh2.js";import"./BaseButton-CEf8UDzm.js";function t(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:m}),`
+`,e.jsx(n.h1,{id:"feedback",children:"Feedback"}),`
+`,e.jsx(n.p,{children:"Komponenten består av en tekst og tommel opp/ned-knapper som ved klikk viser et tilbakemeldingsskjema."}),`
+`,e.jsx(n.h2,{id:"farger-og-layout",children:"Farger og layout"}),`
+`,e.jsxs(n.p,{children:["Du kan velge bakgrunnsfarge ved å sende med ",e.jsx(n.code,{children:"bgColor"})," og ",e.jsx(n.code,{children:"bgDarkmodeColor"}),". Komponenten kan også brukes i kombinasjon med bølgen"]}),`
+`,e.jsx(n.h2,{id:"contactlink",children:"contactLink"}),`
+`,e.jsxs(n.p,{children:["Du kan endre lenken i teksten ved å sende med ",e.jsx(n.code,{children:"contactLink"}),"-prop og element som består av ",e.jsx(n.code,{children:"url"}),", ",e.jsx(n.code,{children:"linkText"})," og ",e.jsx(n.code,{children:"onClick"}),`.
+Ved å bare sende inn `,e.jsx(n.code,{children:"onClick"}),` kan du overskrive hva som skjer når lenken blir trykket på.
+Hvis du vil sende med din egen tekst kan du legge til `,e.jsx(n.code,{children:"linkText"}),"."]}),`
+`,e.jsx(d,{of:r}),`
+`,e.jsx(a,{of:r})]})}function T(o={}){const{wrapper:n}={...i(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{T as default};
---
+++ assets/Header-kOVeviOo.js
@@ -0,0 +1,6 @@
+import{ae as e,af as i,ag as a,ah as m}from"./index-CmcPDUN2.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{H as p,S as o}from"./Header.stories-CSiPdyxy.js";import"./iframe-Bu68wkuK.js";import"../sb-preview/runtime.js";import"./index-eCxJ45ll.js";import"./index-BVyQ2LRr.js";import"./index-CNjFNOVn.js";import"./index-D-8MO0q_.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DezCrw0f.js";import"./Icon-B88nEPeu.js";function r(n){const t={h1:"h1",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:p}),`
+`,e.jsx(t.h1,{id:"header",children:"Header"}),`
+`,e.jsx(t.p,{children:`Headeren er seksjonen som vises i toppen av nettsidene. Designsystemet inneholder foreløpig bare styling/klasser for denne (ingen HTML/JSX-kode).
+Under finner du et eksempel på hvordan headeren skal se ut i innlogget nettbank:`}),`
+`,e.jsx(a,{of:o}),`
+`,e.jsx(m,{of:o})]})}function v(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{v as default};
