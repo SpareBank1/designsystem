de1e6c93c8e0122b5ed304e29d6a08047acc7141
Georg Ekeberg <georg.ekeberg@sparebank1.no>
2025-01-10 13:18:07 +0100

    Optimization attempt

--- examples/sbanky/src/App.tsx
+++ examples/sbanky/src/App.tsx
@@ -2,6 +2,7 @@ import { Grid, GridRow, GridCol } from '@sb1/ffe-grid-react';
 import { ButtonsDemo } from './components/ButtonsDemo';
 import { Wave } from '@sb1/ffe-core-react';
 import { ContextMessage } from '@sb1/ffe-messages-react';
+import { Accordion, AccordionItem } from '@sb1/ffe-accordion-react';
 
 export const SbankyWave = () => (
     <div className="sbanky-wave-wrapper">
@@ -37,6 +38,19 @@ const App = () => {
                             console.log('Secondary clicked')
                         }
                     />
+                </GridCol>
+                <GridCol md={{ cols: 12 }} lg={{ cols: 12 }}>
+                    <Accordion headingLevel={2}>
+                        <AccordionItem heading="Tittel">
+                            Skjult innhold
+                        </AccordionItem>
+                        <AccordionItem heading={<span>Enda en tittel</span>}>
+                            Mer skjult innhold
+                        </AccordionItem>
+                        <AccordionItem heading="En siste tittel">
+                            Enda mer innhold
+                        </AccordionItem>
+                    </Accordion>
                 </GridCol>
             </GridRow>
         </Grid>
