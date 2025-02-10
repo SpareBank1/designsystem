8c87285735db2a791c2f7da9941e59f37fe85334
Anders Johnsen <anders.johnsen@sparebank1.no>
2025-02-10 12:53:38 +0100

    feat(ffe-lists-react): semantiske farger i lists
    BREAKING CHANGE: nye farger i komponentene, bgDarkmodeColor er fjernet fra DetailListCard

--- packages/ffe-lists-react/src/DetailListCard.tsx
+++ packages/ffe-lists-react/src/DetailListCard.tsx
@@ -3,14 +3,12 @@ import classNames from 'classnames';
 
 export interface DetailListCardProps
     extends React.ComponentPropsWithoutRef<'dl'> {
-    bgColor?: 'sand-30' | 'sand-70' | 'frost-30' | 'syrin-30' | 'syrin-70';
-    bgDarkmodeColor?: 'natt' | 'svart' | 'koksgraa';
+    bgColor?: 'primary' | 'secondary' | 'tertiary';
 }
 
 export const DetailListCard: React.FC<DetailListCardProps> = ({
     className,
     bgColor,
-    bgDarkmodeColor,
     children,
     ...rest
 }) => {
@@ -18,8 +16,6 @@ export const DetailListCard: React.FC<DetailListCardProps> = ({
         <dl
             className={classNames('ffe-detail-list-card', className, {
                 [`ffe-detail-list-card--bg-${bgColor}`]: bgColor,
-                [`ffe-detail-list-card--dm-bg-${bgDarkmodeColor}`]:
-                    bgDarkmodeColor,
             })}
             {...rest}
         >
