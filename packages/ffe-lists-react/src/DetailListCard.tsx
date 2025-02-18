c7f8708b06afb10b3fab87e8ff54794194d6ae6b
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-18 10:44:26 +0100

    fix(ffe-list-react): Gi bedre feilmelding ved bruk av utdatert property

--- packages/ffe-lists-react/src/DetailListCard.tsx
+++ packages/ffe-lists-react/src/DetailListCard.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
+import { BackgroundColor } from '../../ffe-cards-react/src/types';
 
 export interface DetailListCardProps
     extends React.ComponentPropsWithoutRef<'dl'> {
-    bgColor?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Property has new values that work with dark and accent mode as part of the Semantic Color update
+     * Possible values: `primary` `secondary` `tertiary`
+     * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger)
+     */
+    bgColor?: BackgroundColor;
+    /** @deprecated as part of update to Semantic Colors
+     *
+     * Property is replaced by the updated values for `bgColor` that works on dark mode
+     * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger) */
+    bgDarkmodeColor?: never;
 }
 
 export const DetailListCard: React.FC<DetailListCardProps> = ({
