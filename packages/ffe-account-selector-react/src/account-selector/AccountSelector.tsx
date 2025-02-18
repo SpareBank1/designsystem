ee3b23b9865b6c4ff08922617f7746cb0e8d093d
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-18 10:43:09 +0100

    fix(ffe-account-selector-react): Gi bedre feilmelding ved bruk av utdatert property

--- packages/ffe-account-selector-react/src/account-selector/AccountSelector.tsx
+++ packages/ffe-account-selector-react/src/account-selector/AccountSelector.tsx
@@ -64,7 +64,11 @@ export interface AccountSelectorProps<T extends Account = Account> {
     /**
      * Called when emptying the input field and moving focus away from the account selector
      * */
-    onReset: () => void;
+    onReset: () => void /** @deprecated as part of update to Semantic Colors
+     *
+     * Use the `ffe-accent-color` class on the component or on the container of the component instead
+     * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger) */;
+    onColoredBg?: never;
 }
 
 export const AccountSelector = <T extends Account = Account>({
