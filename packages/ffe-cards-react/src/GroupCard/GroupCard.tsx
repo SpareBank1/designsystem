8331d079ac03b21b4d54a678dbb58251218b0293
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-18 10:43:30 +0100

    fix(ffe-cards-react): Gi bedre feilmelding ved bruk av utdatert property

--- packages/ffe-cards-react/src/GroupCard/GroupCard.tsx
+++ packages/ffe-cards-react/src/GroupCard/GroupCard.tsx
@@ -7,8 +7,19 @@ export interface GroupCardProps
     extends Omit<React.ComponentPropsWithoutRef<'div'>, 'children'> {
     /** The children of the GroupCard component */
     children: React.ReactNode;
-    /** The background color of the whole groupcard element */
+    /**
+     * The background color of the whole groupcard element
+     *
+     * Property has new values that work with dark and accent mode as part of the Semantic Color update
+     * Possible values: `primary` `secondary` `tertiary`
+     * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger)
+     */
     bgColor?: BackgroundColor;
+    /** @deprecated as part of update to Semantic Colors
+     *
+     * Property is replaced by the updated values for `bgColor` that works on dark mode
+     * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger) */
+    bgDarkmodeColor?: never;
     /** No margin on card */
     noMargin?: boolean;
 }
