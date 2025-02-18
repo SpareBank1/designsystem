b5ea5f286883e446216ff7e8e0ded711e61c147a
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-18 10:44:01 +0100

    fix(ffe-grid-react): Gi bedre feilmelding ved bruk av utdatert property

--- packages/ffe-grid-react/src/GridCol.tsx
+++ packages/ffe-grid-react/src/GridCol.tsx
@@ -8,7 +8,13 @@ import {
 
 export type GridColProps<As extends ElementType = any> =
     ComponentWithoutRefAsPropParams<As> & {
-        /** Supported background colors */
+        /**
+         * Supported background colors
+         *
+         * Property has new values that work with dark and accent mode as part of the Semantic Color update
+         * Possible values: `primary` `secondary` `tertiary`
+         * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger)
+         */
         bgColor?: BackgroundColor;
         /** Center text content horizontally */
         centerText?: boolean;
