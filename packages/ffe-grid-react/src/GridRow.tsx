37e12631727fa4f5c7d025d11228bd0c10bcc8bc
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-18 10:44:01 +0100

    fix(ffe-grid-react): Gi bedre feilmelding ved bruk av utdatert property

--- packages/ffe-grid-react/src/GridRow.tsx
+++ packages/ffe-grid-react/src/GridRow.tsx
@@ -20,7 +20,13 @@ export type GridRowProps<As extends ElementType = any> =
         padding?: Padding;
         /** Margin in the top and bottom of the row */
         margin?: Margin;
-        /** Supported background colors */
+        /**
+         * Supported background colors
+         *
+         * Property has new values that work with dark and accent mode as part of the Semantic Color update
+         * Possible values: `primary` `secondary` `tertiary`
+         * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger)
+         */
         bgColor?: BackgroundColor;
         /** Size modifiers for small screen sizes */
         sm?: SizeModifier;
