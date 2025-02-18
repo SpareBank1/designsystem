e0e43aec315c9921aff3f8ead6cb5d3c4b70b56b
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-18 10:43:42 +0100

    fix(ffe-chips-react): Gi bedre feilmelding ved bruk av utdatert property

--- packages/ffe-chips-react/src/Chip.tsx
+++ packages/ffe-chips-react/src/Chip.tsx
@@ -20,7 +20,11 @@ export type ChipProps<As extends ElementType = 'button'> =
     ComponentAsPropParams<As> & {
         /** Size of chip*/
         size: 'sm' | 'md' | 'lg';
-        /** Adds alternative styling for better contrast on certain backgrounds */
+        /** @deprecated as part of update to Semantic Colors
+         *
+         * Use the `ffe-accent-color` class on the component or on the container of the component instead
+         * [Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger) */
+        onColoredBg?: never;
     } & LeftOrRightIcon;
 
 function ChipWithForwardRef<As extends ElementType>(
