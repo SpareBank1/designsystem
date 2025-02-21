0fec185ac830aa3f8272a5ac25a95668c05040c5
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-02-21 10:24:22 +0100

    chore: Update modal semantic documentation

--- packages/ffe-modals-react/src/Modal.stories.tsx
+++ packages/ffe-modals-react/src/Modal.stories.tsx
@@ -123,3 +123,57 @@ export const Custom: Story = {
         );
     },
 };
+
+export const DarkMode: Story = {
+    args: {
+        ariaLabelledby: 'heading-id',
+        locale: 'nb',
+        onClose: () => null,
+        portalContainer: document.body,
+    },
+    render: function Render(args) {
+        const modalRef = useRef<ModalHandle>(null);
+        const headingId = args.ariaLabelledby;
+
+        return (
+            <>
+                <ActionButton
+                    type="button"
+                    onClick={() => {
+                        modalRef.current?.open();
+                    }}
+                >
+                    Åpne modal
+                </ActionButton>
+                <Modal
+                    ref={modalRef}
+                    {...args}
+                    className={'dark-mode regard-color-scheme-preference'}
+                >
+                    <ModalBlock>
+                        <Heading2 id={headingId}>En modal</Heading2>
+                        <Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Aperiam atque excepturi exercitationem harum
+                            neque obcaecati, ratione sapiente sit temporibus
+                            unde.
+                        </Paragraph>
+                        <ButtonGroup thin={true} ariaLabel="Knappegruppe">
+                            <SecondaryButton
+                                type="button"
+                                onClick={() => {
+                                    modalRef.current?.close();
+                                }}
+                            >
+                                Lukk modal
+                            </SecondaryButton>
+                            <ActionButton autoFocus={true} type="button">
+                                Lagre
+                            </ActionButton>
+                        </ButtonGroup>
+                    </ModalBlock>
+                </Modal>
+            </>
+        );
+    },
+};
