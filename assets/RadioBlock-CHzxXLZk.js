7f9dbbf0ebcfa70559b81363d73c862b0d380292
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-03-03 11:10:39 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@a61e4bdaca93cf846effda44a8e17343167b68b5 🚀

--- assets/RadioBlock-CHzxXLZk.js
+++
@@ -1,5 +0,0 @@
-import{j as o,M as i,C as a,a as m}from"./index-DHOK-dV1.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{R as p,S as n}from"./RadioBlock.stories-Dd4aXLxb.js";import"./iframe-oCkeZWNF.js";import"./index-eCxJ45ll.js";import"./index-BhUHFDTH.js";import"./index-D3-0uUTW.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./RadioBlock-DO_NDSRs.js";import"./index-DezCrw0f.js";function e(r){const t={h1:"h1",p:"p",...s(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:p}),`
-`,o.jsx(t.h1,{id:"radioblock",children:"RadioBlock"}),`
-`,o.jsx(t.p,{children:"Radioblokker brukes når vi ber brukeren om å ta valg som krever en del."}),`
-`,o.jsx(a,{of:n}),`
-`,o.jsx(m,{of:n})]})}function b(r={}){const{wrapper:t}={...s(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(e,{...r})}):e(r)}export{b as default};
