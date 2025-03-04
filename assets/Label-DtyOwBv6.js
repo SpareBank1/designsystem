d206c518e64969ef500ef9b197779a4ddab4e638
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-03-04 15:11:17 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@8061cc14c9b1b70b12ac858aebb46dca4215319d 🚀

---
+++ assets/Label-DtyOwBv6.js
@@ -0,0 +1,5 @@
+import{j as t,M as a,C as m,a as i}from"./index-DWvKATFl.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{L as p,S as o}from"./Label.stories-rvcXUn5H.js";import"./iframe-01gEQZIt.js";import"./index-eCxJ45ll.js";import"./index-BhUHFDTH.js";import"./index-D3-0uUTW.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Label-DD3UyPji.js";import"./index-DezCrw0f.js";function r(e){const n={h1:"h1",p:"p",...s(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:p}),`
+`,t.jsx(n.h1,{id:"label",children:"Label"}),`
+`,t.jsx(n.p,{children:"Om du ikke bruker InputGroup kan du fremdeles bruke deler av den, som f.eks. Label."}),`
+`,t.jsx(m,{of:o}),`
+`,t.jsx(i,{of:o})]})}function M(e={}){const{wrapper:n}={...s(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(r,{...e})}):r(e)}export{M as default};
---
+++ assets/formatPercentage-CItlGYxj.js
@@ -0,0 +1,6 @@
+import{j as e,M as a,C as m,a as i}from"./index-DWvKATFl.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{f as c,S as n}from"./formatPercentage.stories-DkknIpxY.js";import"./iframe-01gEQZIt.js";import"./index-eCxJ45ll.js";import"./index-BhUHFDTH.js";import"./index-D3-0uUTW.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./unicode-BkFkzGng.js";function r(o){const t={code:"code",h1:"h1",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:c}),`
+`,e.jsx(t.h1,{id:"formatpercentage",children:"formatPercentage"}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.code,{children:"formatPercentage"}),` gir prosenter det riktige mellomrommet mellom tall og % med et non-breaking space.
+Runder av til så få desimaler som mulig, med en default cap på to desimaler. Dette kan overstyres i `,e.jsx(t.code,{children:"options"}),"-objektet."]}),`
+`,e.jsx(m,{of:n}),`
+`,e.jsx(i,{of:n})]})}function M(o={}){const{wrapper:t}={...s(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}export{M as default};
