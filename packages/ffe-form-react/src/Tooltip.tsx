9fcec11eab8d14ef65ea89bbc698cf15955ad1b2
Anders Johnsen <anders.johnsen@sparebank1.no>
2025-03-07 15:16:35 +0100

    fix(ffe-form-react): oppdatering av tooltip-ikon
    * oppdaterer tooltip-ikonet i henhold til modernisering i figma

--- packages/ffe-form-react/src/Tooltip.tsx
+++ packages/ffe-form-react/src/Tooltip.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useId } from 'react';
 import classNames from 'classnames';
 import { Collapse } from '@sb1/ffe-collapse-react';
+import { Icon } from '@sb1/ffe-icons-react';
 
 export interface TooltipProps
     extends Pick<
@@ -49,6 +50,9 @@ export const Tooltip = React.forwardRef<HTMLButtonElement, TooltipProps>(
             }
         };
 
+        const helpIcon =
+            'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjRweCIgdmlld0JveD0iMCAtOTYwIDk2MCA5NjAiIHdpZHRoPSIyNHB4Ij48cGF0aCBkPSJNNDc4LTI0MHEyMSAwIDM1LjUtMTQuNVQ1MjgtMjkwcTAtMjEtMTQuNS0zNS41VDQ3OC0zNDBxLTIxIDAtMzUuNSAxNC41VDQyOC0yOTBxMCAyMSAxNC41IDM1LjVUNDc4LTI0MFptLTM2LTE1NGg3NHEwLTMzIDcuNS01MnQ0Mi41LTUycTI2LTI2IDQxLTQ5LjV0MTUtNTYuNXEwLTU2LTQxLTg2dC05Ny0zMHEtNTcgMC05Mi41IDMwVDM0Mi02MThsNjYgMjZxNS0xOCAyMi41LTM5dDUzLjUtMjFxMzIgMCA0OCAxNy41dDE2IDM4LjVxMCAyMC0xMiAzNy41VDUwNi01MjZxLTQ0IDM5LTU0IDU5dC0xMCA3M1ptMzggMzE0cS04MyAwLTE1Ni0zMS41VDE5Ny0xOTdxLTU0LTU0LTg1LjUtMTI3VDgwLTQ4MHEwLTgzIDMxLjUtMTU2VDE5Ny03NjNxNTQtNTQgMTI3LTg1LjVUNDgwLTg4MHE4MyAwIDE1NiAzMS41VDc2My03NjNxNTQgNTQgODUuNSAxMjdUODgwLTQ4MHEwIDgzLTMxLjUgMTU2VDc2My0xOTdxLTU0IDU0LTEyNyA4NS41VDQ4MC04MFptMC04MHExMzQgMCAyMjctOTN0OTMtMjI3cTAtMTM0LTkzLTIyN3QtMjI3LTkzcS0xMzQgMC0yMjcgOTN0LTkzIDIyN3EwIDEzNCA5MyAyMjd0MjI3IDkzWm0wLTMyMFoiLz48L3N2Zz4=';
+
         return (
             <div
                 {...containerProps}
@@ -67,7 +71,14 @@ export const Tooltip = React.forwardRef<HTMLButtonElement, TooltipProps>(
                     tabIndex={tabIndex}
                     id={tooltipButtonId}
                 >
-                    <span aria-hidden={true}>?</span>
+                    <span aria-hidden={true}>
+                        <Icon
+                            className="ffe-button__icon"
+                            fileUrl={helpIcon}
+                            size="md"
+                            ariaLabel="?"
+                        />
+                    </span>
                 </button>
                 {children && (
                     <Collapse
