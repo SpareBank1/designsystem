784198a4b6f6c688418282e3b72e545cdc8ff9b2
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-03-10 11:10:57 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@99fe0236f4cf8abd325c3588922e74f9baf4145c 🚀

---
+++ assets/ActionButton-DXkLk8Se.js
@@ -0,0 +1,5 @@
+import{j as t,M as i,C as a,a as m}from"./index-4efzVFvR.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{A as p,S as r}from"./ActionButton.stories-8S7FvGsY.js";import"./iframe-CQf03YiD.js";import"./index-eCxJ45ll.js";import"./index-BhUHFDTH.js";import"./index-D3-0uUTW.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./BaseButton-D99V6fdL.js";import"./fixedForwardRef-Cb2PomjO.js";import"./index-DezCrw0f.js";function e(o){const n={h1:"h1",p:"p",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(n.h1,{id:"actionbutton",children:"ActionButton"}),`
+`,t.jsx(n.p,{children:"Brukes for svært høyt prioriterte handlinger (call to action), og der du trenger en knapp som skiller seg ut. Det skal som hovedregel kun være en ActionButton per side/view."}),`
+`,t.jsx(a,{of:r}),`
+`,t.jsx(m,{of:r})]})}function v(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{v as default};
