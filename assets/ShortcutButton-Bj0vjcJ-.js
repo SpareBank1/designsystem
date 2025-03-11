6f981c06b183fe4a3e933cadc3951f71711fa452
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-03-11 11:10:47 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@024265317875e4f562324aed919710e18729509e 🚀

--- assets/ShortcutButton-Bj0vjcJ-.js
+++
@@ -1,5 +0,0 @@
-import{j as t,M as i,C as a,a as m}from"./index-BzIi2iqi.js";import{useMDXComponents as s}from"./index-CVdx34fM.js";import{S as p,a as r}from"./ShortcutButton.stories-BQBzPaPj.js";import"./iframe-B9y8CEWx.js";import"./index-eCxJ45ll.js";import"./index-BhUHFDTH.js";import"./index-D3-0uUTW.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./BaseButton-D99V6fdL.js";import"./fixedForwardRef-Cb2PomjO.js";import"./index-DezCrw0f.js";import"./Icon-B88nEPeu.js";function e(n){const o={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
-`,t.jsx(o.h1,{id:"shortcutbutton",children:"ShortcutButton"}),`
-`,t.jsx(o.p,{children:"Et alternativ til lenke der du ønsker å tilby en snarvei til et annet sted i løsningen."}),`
-`,t.jsx(a,{of:r}),`
-`,t.jsx(m,{of:r})]})}function X(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{X as default};
--- assets/formatFodselsnummer-DpZ0ML9j.js
+++
@@ -1,4 +0,0 @@
-import{j as o,M as e,C as a,a as i}from"./index-BzIi2iqi.js";import{useMDXComponents as m}from"./index-CVdx34fM.js";import{f,S as r}from"./formatFodselsnummer.stories-CHNw5OhL.js";import"./iframe-B9y8CEWx.js";import"./index-eCxJ45ll.js";import"./index-BhUHFDTH.js";import"./index-D3-0uUTW.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./unicode-BkFkzGng.js";function s(t){const n={h1:"h1",...m(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:f}),`
-`,o.jsx(n.h1,{id:"formatfodselsnummer",children:"formatFodselsnummer"}),`
-`,o.jsx(a,{of:r}),`
-`,o.jsx(i,{of:r})]})}function D(t={}){const{wrapper:n}={...m(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(s,{...t})}):s(t)}export{D as default};
