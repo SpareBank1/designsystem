ac03d2c8123e3bc5c0863b46abe90477ff2ff45e
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-03-24 13:17:53 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@e460af3c480ef9bfdf20df4ac44985e88d2a4c0a 🚀

---
+++ assets/RadioSwitch-DgOHEivb.js
@@ -0,0 +1,7 @@
+import{j as n,M as s,C as a,a as p}from"./index-D_VJqC7S.js";import{useMDXComponents as e}from"./index-DmqVK_gK.js";import{R as m,S as t}from"./RadioSwitch.stories-FXLaPhao.js";import"./iframe-CoEYM5dv.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./RadioSwitch-BBYjSt2C.js";import"./index-QCImZZ3W.js";import"./BaseRadioButton-Brnhr778.js";import"./Tooltip-B8J4kvmt.js";import"./Icon-BqsuuMcG.js";import"./Collapse-BrKhsD1f.js";import"./RadioButtonInputGroup-XI03ZI0T.js";import"./ErrorFieldMessage-eNbkZ9Td.js";import"./BaseFieldMessage-Bp22b08z.js";function i(o){const r={h1:"h1",h2:"h2",p:"p",...e(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:m}),`
+`,n.jsx(r.h1,{id:"radioswitch",children:"RadioSwitch"}),`
+`,n.jsx(r.p,{children:'Brukes når kunden skal ta stilling til enkle valg - typisk i formen "ja/nei", eller "av/på" og hvor du i tillegg har behov for en lagreknapp. Har du behov for mer enn 3 radio-switcher i en liste, skal du bruke radioknapper.'}),`
+`,n.jsx(r.p,{children:"Radio-switcher uten defaultvalg."}),`
+`,n.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,n.jsx(a,{of:t}),`
+`,n.jsx(p,{of:t})]})}function X(o={}){const{wrapper:r}={...e(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(i,{...o})}):i(o)}export{X as default};
--- assets/TertiaryButton-CMZXmX0w.js
+++
@@ -1,6 +0,0 @@
-import{j as t,M as s,C as a,a as m}from"./index-CP5kGe7Y.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{T as p,S as o}from"./TertiaryButton.stories-CpHeuRfN.js";import"./iframe-CM1KxdK_.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./TertiaryButton-CdE8yrwQ.js";import"./InlineBaseButton-3hc5Qzgi.js";import"./fixedForwardRef-DqyCgkTx.js";import"./index-QCImZZ3W.js";function e(n){const r={h1:"h1",h2:"h2",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
-`,t.jsx(r.h1,{id:"tertiarybutton",children:"TertiaryButton"}),`
-`,t.jsx(r.p,{children:"For lavt prioriterte handlinger, eller i situasjoner der du trenger å «linke» til innhold på samme side. For eksempel «anchors» som leder deg til et avsnitt i en artikkel."}),`
-`,t.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,t.jsx(a,{of:o}),`
-`,t.jsx(m,{of:o})]})}function y(n={}){const{wrapper:r}={...i(),...n.components};return r?t.jsx(r,{...n,children:t.jsx(e,{...n})}):e(n)}export{y as default};
