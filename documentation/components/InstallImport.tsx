1ec82ecad9662213b0cfc3b4d4d5145b208fa656
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-04-03 08:55:02 +0200

    chore: Add dep to package install instructions

--- documentation/components/InstallImport.tsx
+++ documentation/components/InstallImport.tsx
@@ -5,29 +5,27 @@ import { Source } from '@storybook/blocks';
 type Props = {
     dependencies: string[];
     stylingDependencies?: string[];
-    nocore?: boolean;
 };
 
 export const InstallImport: React.FC<Props> = ({
     dependencies,
     stylingDependencies,
-    nocore,
 }) => {
-    const installCode = `npm install${!nocore ? ' @sb1/ffe-core' : ''} ${dependencies.join(' ')}`;
+    const installCode = `npm install ${dependencies.join(' ')}`;
     const innstallStyling =
         stylingDependencies &&
         `@import '@sb1/ffe-core/less/ffe-core';\n${stylingDependencies.map(dep => `@import '${dep}';`).join('\n')}`;
     return (
         <>
             <Heading2>Installasjon</Heading2>
-            <Paragraph>Innstaller pakken med avhangigheter</Paragraph>
-            <Source code={installCode} />
+            <Paragraph>Installer pakken med avhengigheter</Paragraph>
+            <Source type="bash" code={installCode} />
             {stylingDependencies && (
                 <>
                     <Paragraph>
                         I prosjektets hovedstylingsfil importer stylingen
                     </Paragraph>
-                    <Source code={innstallStyling} />
+                    <Source type="css" code={innstallStyling} />
                 </>
             )}
         </>
