9ae8fda26bf1357a7b229b9b4675e5a97de07ff0
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-04 11:10:50 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@7def3cc76a5ea2bc1fc9b2a10dfb4df558cfcd33 🚀

--- assets/InlineExpandButton-OVl6ERD-.js
+++
@@ -1,6 +0,0 @@
-import{j as n,M as i,C as a,a as m}from"./index-DOBxsM_z.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{I as p,S as e}from"./InlineExpandButton.stories-BNARNnt3.js";import"./iframe-C_P7L4d3.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./InlineBaseButton-B-uXHNhA.js";import"./fixedForwardRef-DqyCgkTx.js";import"./index-QCImZZ3W.js";import"./Icon-BqsuuMcG.js";import"./Paragraph-DEYlXMQQ.js";import"./Collapse-BrKhsD1f.js";function r(t){const o={h1:"h1",h2:"h2",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:p}),`
-`,n.jsx(o.h1,{id:"inlineexpandbutton",children:"InlineExpandButton"}),`
-`,n.jsx(o.p,{children:"Samme som ExpandButton, bortsett fra at denne brukes når du vil ekspandere/kollapse en seksjon fra en linje med tekst."}),`
-`,n.jsx(o.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,n.jsx(a,{of:e}),`
-`,n.jsx(m,{of:e})]})}function g(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{g as default};
