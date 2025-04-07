31bbf2b841b450fdc137246196860f0517cb52f8
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-04-07 15:45:34 +0200

    chore: Add package install instructions

--- documentation/components/InstallImport.tsx
+++ documentation/components/InstallImport.tsx
@@ -1,27 +1,67 @@
 import { Heading2, Paragraph } from '@sb1/ffe-core-react';
-import React from 'react';
+import React, { useState } from 'react';
 import { Source } from '@storybook/blocks';
+import { TabGroup, Tab } from '@sb1/ffe-tabs-react';
 
 type Props = {
-    dependencies: string[];
+    packageName: string;
+    dependencies?: string[];
     stylingDependencies?: string[];
-    nocore?: boolean;
 };
 
 export const InstallImport: React.FC<Props> = ({
-    dependencies,
+    packageName,
     stylingDependencies,
-    nocore,
+    dependencies,
 }) => {
-    const installCode = `npm install${!nocore ? ' @sb1/ffe-core' : ''} ${dependencies.join(' ')}`;
+    const [activeTabId, setActiveTabId] = useState('all');
+    const installCodeDirect = `npm install ${packageName}`;
+    const installCodeAll = `npm install ${packageName} ${dependencies ? dependencies?.join(' ') : ''}`;
     const innstallStyling =
         stylingDependencies &&
         `@import '@sb1/ffe-core/less/ffe-core';\n${stylingDependencies.map(dep => `@import '${dep}';`).join('\n')}`;
     return (
         <>
             <Heading2>Installasjon</Heading2>
-            <Paragraph>Innstaller pakken med avhangigheter</Paragraph>
-            <Source code={installCode} />
+            {dependencies ? (
+                <>
+                    <TabGroup>
+                        <Tab
+                            selected={activeTabId === 'all'}
+                            onClick={() => setActiveTabId('all')}
+                        >
+                            Pakke og avhengigheter
+                        </Tab>
+
+                        <Tab
+                            selected={activeTabId === 'direct'}
+                            onClick={() => setActiveTabId('direct')}
+                        >
+                            Kun pakke
+                        </Tab>
+                    </TabGroup>
+                    <div
+                        id="all-dep"
+                        style={{
+                            display: activeTabId === 'all' ? 'block' : 'none',
+                        }}
+                    >
+                        <Source code={installCodeAll} />
+                    </div>
+                    <div
+                        id="direct-dep"
+                        style={{
+                            display:
+                                activeTabId === 'direct' ? 'block' : 'none',
+                        }}
+                    >
+                        <Source code={installCodeDirect} />
+                    </div>
+                </>
+            ) : (
+                <Source code={installCodeDirect} />
+            )}
+
             {stylingDependencies && (
                 <>
                     <Paragraph>
