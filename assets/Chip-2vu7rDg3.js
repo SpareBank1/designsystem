535cd888c67ef0fc57a4df7ea429d763ad9e8d0f
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-07 13:18:42 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@e6914f00db8d4300304c9d056f93b1a8d7744964 🚀

--- assets/Chip-2vu7rDg3.js
+++
@@ -1,8 +0,0 @@
-import{j as e,M as o,C as l,a as m}from"./index-B0zKewuN.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{C as p,S as t}from"./Chip.stories-fNJOFI6f.js";import"./iframe-BNgERsRZ.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Chip-cFLlwyzA.js";import"./index-QCImZZ3W.js";function i(n){const r={h1:"h1",h2:"h2",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:p}),`
-`,e.jsx(r.h1,{id:"chip",children:"Chip"}),`
-`,e.jsx(r.p,{children:"Bruk Chips for elementer der brukeren trenger å filtrere eller organisere innhold."}),`
-`,e.jsx(r.p,{children:`Skriv klare og tydelige labels slik at brukeren forstår formålet med ulike chips.
-Om brukeren kan velge chips må de også kunne avvelge chips. Dette er spesielt viktig for multiselect eller filtreringsløsninger.`}),`
-`,e.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,e.jsx(l,{of:t}),`
-`,e.jsx(m,{of:t})]})}function v(n={}){const{wrapper:r}={...s(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(i,{...n})}):i(n)}export{v as default};
