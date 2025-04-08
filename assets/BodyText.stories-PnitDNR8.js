598b8513bcc63454dfdfb217eb0c782ea97ec841
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-08 13:19:48 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@e51ee48c403c07e78c27ba3f508c01e2f9fcb62e 🚀

---
+++ assets/BodyText.stories-PnitDNR8.js
@@ -0,0 +1,8 @@
+import{R as n}from"./index-DQDNmYQF.js";import{c as l}from"./index-QCImZZ3W.js";function s(a){const{as:i="div",className:c,...m}=a;return n.createElement(i,{className:l("ffe-body-text",c),...m})}s.__docgenInfo={description:"",methods:[],displayName:"BodyText",props:{as:{required:!1,tsType:{name:"As"},description:""}}};const u={title:"Komponenter/Core/BodyText",component:s},e={args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in sem posuere, fringilla augue et, egestas magna. Sed dapibus, libero quis rutrum pharetra, lectus tortor bibendum quam, quis placerat urna lorem malesuada elit. Maecenas justo lorem, accumsan in leo non, pellentesque facilisis magna.",as:"p",className:"custom-class"},render:a=>n.createElement(s,{...a})};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in sem posuere, fringilla augue et, egestas magna. Sed dapibus, libero quis rutrum pharetra, lectus tortor bibendum quam, quis placerat urna lorem malesuada elit. Maecenas justo lorem, accumsan in leo non, pellentesque facilisis magna.',
+    as: 'p',
+    className: 'custom-class'
+  },
+  render: args => <BodyText {...args} />
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Standard"],f=Object.freeze(Object.defineProperty({__proto__:null,Standard:e,__namedExportsOrder:d,default:u},Symbol.toStringTag,{value:"Module"}));export{f as B,e as S};
