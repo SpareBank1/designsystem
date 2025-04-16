af9587388c083bb281f488502dce12fbde603be3
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-16 11:12:10 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@240a8bceb0ba6f8559bb2c1fd2d3b86b122b0a2a 🚀

---
+++ assets/Dropdown-C14jfrl5.js
@@ -0,0 +1,7 @@
+import{j as n,M as i,C as m,a}from"./index-BiakaBd9.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{D as p,S as e}from"./Dropdown.stories-D8JQ5lpG.js";import{I as d}from"./InstallImport-tN0bg9X0.js";import"./iframe-DyWldjxa.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./InputGroup-DYJmYWjy.js";import"./Icon-BqsuuMcG.js";import"./Collapse-BrKhsD1f.js";import"./Heading-afd3onnD.js";import"./Paragraph-DEYlXMQQ.js";function t(o){const r={h1:"h1",h2:"h2",p:"p",...s(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:p}),`
+`,n.jsx(r.h1,{id:"dropdown",children:"Dropdown"}),`
+`,n.jsx(r.p,{children:"Dropdown er en vanlig nedtrekksliste som kan brukes man når man har 5-10 valg å velge mellom. Har du mindre enn 5 valg, bruk radio buttons istedenfor."}),`
+`,n.jsx(d,{packageName:"@sb1/ffe-dropdown-react",dependencies:["@sb1/ffe-form","@sb1/ffe-buttons","@sb1/ffe-core"],stylingDependencies:["@sb1/ffe-form/less/form","@sb1/ffe-buttons/less/buttons"]}),`
+`,n.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,n.jsx(m,{of:e}),`
+`,n.jsx(a,{of:e})]})}function S(o={}){const{wrapper:r}={...s(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(t,{...o})}):t(o)}export{S as default};
--- assets/Header-W2DE5nDA.js
+++
@@ -1,7 +0,0 @@
-import{j as n,M as i,C as a,a as d}from"./index-Bap1fQqT.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{H as m,S as o}from"./Header.stories-AminZSos.js";import"./iframe-fIkkBHSr.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./Icon-BqsuuMcG.js";function r(e){const t={h1:"h1",h2:"h2",p:"p",...s(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:m}),`
-`,n.jsx(t.h1,{id:"header",children:"Header"}),`
-`,n.jsx(t.p,{children:`Headeren er seksjonen som vises i toppen av nettsidene. Designsystemet inneholder foreløpig bare styling/klasser for denne (ingen HTML/JSX-kode).
-Under finner du et eksempel på hvordan headeren skal se ut i innlogget nettbank:`}),`
-`,n.jsx(t.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,n.jsx(a,{of:o}),`
-`,n.jsx(d,{of:o})]})}function v(e={}){const{wrapper:t}={...s(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(r,{...e})}):r(e)}export{v as default};
