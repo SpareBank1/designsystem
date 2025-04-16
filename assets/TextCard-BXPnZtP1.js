efc64963c2ebbf228c8e18de5951f07b6b1a72a0
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-16 13:17:39 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@48b8a3d6bd20c1299e119ecd02066c1fa6ddbc0c 🚀

---
+++ assets/TextCard-BXPnZtP1.js
@@ -0,0 +1,6 @@
+import{j as t,M as i,C as a,a as m}from"./index-C5tKWooc.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{T as d,S as r}from"./TextCard.stories-Bc8CyhLO.js";import"./iframe-Dz1ODQy0.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./WithCardAction-BEfhMYaK.js";import"./Title-CmDdbz_O.js";function o(e){const n={h1:"h1",h2:"h2",p:"p",...s(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:d,parameters:{viewMode:"docs"}}),`
+`,t.jsx(n.h1,{id:"textcard",children:"TextCard"}),`
+`,t.jsx(n.p,{children:"Tekstkort er den enkleste korttypen. Den tilbyr alle underkomponenter (CardName, Title, Subtext, Text og CardAction) og plasserer dem i gitt rekkefølge nedover i kortet, med teksten midtstilt."}),`
+`,t.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,t.jsx(a,{of:r}),`
+`,t.jsx(m,{of:r})]})}function T(e={}){const{wrapper:n}={...s(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{T as default};
