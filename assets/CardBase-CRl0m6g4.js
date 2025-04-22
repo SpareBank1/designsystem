8e52ed97e0db7a4049b895c3658e35f9369dab16
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-22 13:17:37 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@f5b719bd9675fcd381cbdf6568383a4ab54a1ac3 🚀

---
+++ assets/CardBase-CRl0m6g4.js
@@ -0,0 +1,7 @@
+import{j as e,M as a,C as i,a as d}from"./index-CPC10die.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{C as m,S as o}from"./CardBase.stories-Bjc46ak2.js";import"./iframe-COD9pQ71.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./CardBase-yNeKY91g.js";import"./index-QCImZZ3W.js";import"./WithCardAction-BEfhMYaK.js";function t(n){const r={code:"code",h1:"h1",h2:"h2",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(r.h1,{id:"cardbase",children:"CardBase"}),`
+`,e.jsx(r.p,{children:"Dette er basisen for alle kort. Komponeten kan brukes direkte for å få en ramme du kan fylle dersom du trenger et spesialtilpassert kort."}),`
+`,e.jsxs(r.p,{children:["Du kan skru av margin med ",e.jsx(r.code,{children:"noMargin"}),"-prop, og sette bakgrunnsfargen i både light- og darkmode med ",e.jsx(r.code,{children:"bgColor"})," og ",e.jsx(r.code,{children:"bgDarkmodeColor"}),"."]}),`
+`,e.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(d,{of:o})]})}function M(n={}){const{wrapper:r}={...s(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(t,{...n})}):t(n)}export{M as default};
---
+++ assets/SystemMessage-Be6-NbL7.js
@@ -0,0 +1,11 @@
+import{j as e,M as m,C as a,a as l}from"./index-CPC10die.js";import{useMDXComponents as o}from"./index-DmqVK_gK.js";import{S as h,a as i}from"./SystemMessage.stories-DGmKfCwj.js";import{C as t,a as n}from"./CheckListItem-o7Y4SA9X.js";import"./iframe-COD9pQ71.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./Content-DbXcSOP0.js";import"./MessageCollapse-Dozv0W-N.js";import"./Icon-BqsuuMcG.js";import"./LinkText-fBwg0lJi.js";function d(r){const s={h1:"h1",h2:"h2",p:"p",...o(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(m,{of:h}),`
+`,e.jsx(s.h1,{id:"systemmessage",children:"SystemMessage"}),`
+`,e.jsx(s.p,{children:"Systemmeldinger skal bare brukes til viktige, midlertidige meldinger. De plasseres direkte under headeren og det skal helst bare brukes 1 per side."}),`
+`,e.jsx(s.p,{children:"Bruk systemmelding:"}),`
+`,e.jsxs(t,{children:[e.jsx(n,{children:e.jsx(s.p,{children:"Der meldingen skal vises i en gitt tidsperiode"})}),e.jsx(n,{children:e.jsx(s.p,{children:`Når det skal brukes til informasjon, som ikke krever en handling fra
+bruker`})})]}),`
+`,e.jsx(s.p,{children:"Ikke bruk systemmelding:"}),`
+`,e.jsxs(t,{children:[e.jsx(n,{isCross:!0,children:e.jsx(s.p,{children:"Der det ikke gir mening at meldingen er rett under header"})}),e.jsx(n,{isCross:!0,children:e.jsx(s.p,{children:"Der det skal vises mange meldinger samtidig"})})]}),`
+`,e.jsx(s.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(a,{of:i}),`
+`,e.jsx(l,{of:i})]})}function L(r={}){const{wrapper:s}={...o(),...r.components};return s?e.jsx(s,{...r,children:e.jsx(d,{...r})}):d(r)}export{L as default};
