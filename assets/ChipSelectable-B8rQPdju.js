55336a4c557b37c8b1f3feb3a15e155f5682a2e6
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-04-23 13:18:35 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@3c3f5cf3e54fb60d6f36e9306398481d8fc7858b 🚀

---
+++ assets/ChipSelectable-B8rQPdju.js
@@ -0,0 +1,4 @@
+import{j as t,M as i,C as a,a as m}from"./index-7DFrh8a2.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{C as p,S as e}from"./ChipSelectable.stories-DsbrN-t0.js";import"./iframe-Csd6V0o8.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Chip-cFLlwyzA.js";import"./index-QCImZZ3W.js";import"./Icon-BqsuuMcG.js";function r(o){const n={h1:"h1",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(n.h1,{id:"chipselectable",children:"ChipSelectable"}),`
+`,t.jsx(a,{of:e}),`
+`,t.jsx(m,{of:e})]})}function D(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{D as default};
