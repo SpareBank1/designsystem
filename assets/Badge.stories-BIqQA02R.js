e5da372aca3a1968ea6a02d8c8db60078f338202
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-02 12:45:57 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@05065149c14754a53ccf849b7ff04c63cc514a7c 🚀

---
+++ assets/Badge.stories-BIqQA02R.js
@@ -0,0 +1,26 @@
+import{R as a}from"./index-DQDNmYQF.js";import{c as x}from"./index-QCImZZ3W.js";function s({className:e,children:t,...y}){return a.createElement("span",{className:x("ffe-badge",{"ffe-badge--no-children":!t},e),...y},t)}s.__docgenInfo={description:"",methods:[],displayName:"Badge",props:{className:{required:!1,tsType:{name:"string"},description:""},children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const v={title:"Komponenter/Badge",component:s},r={args:{},render:e=>a.createElement(s,{...e},"9")},n={args:{...r.args},render:e=>a.createElement("div",{style:{display:"flex",gap:"8px",color:"var(--ffe-color-foreground-default)"}},"Meldinger",a.createElement(s,{...e},"23"))},o={args:{},render:e=>a.createElement("div",{style:{height:"24px",width:"24px",backgroundColor:"var(--ffe-color-surface-secondary-default)",borderRadius:"50%"}},a.createElement(s,{...e}))};var d,c,l;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {},
+  render: args => <Badge {...args}>9</Badge>
+}`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var g,i,p;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    ...Standard.args
+  },
+  render: args => <div style={{
+    display: 'flex',
+    gap: '8px',
+    color: 'var(--ffe-color-foreground-default)'
+  }}>
+            Meldinger
+            <Badge {...args}>23</Badge>
+        </div>
+}`,...(p=(i=n.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var m,f,u;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div style={{
+    height: '24px',
+    width: '24px',
+    backgroundColor: 'var(--ffe-color-surface-secondary-default)',
+    borderRadius: '50%'
+  }}>
+            <Badge {...args} />
+        </div>
+}`,...(u=(f=o.parameters)==null?void 0:f.docs)==null?void 0:u.source}}};const B=["Standard","InContext","Indicator"],h=Object.freeze(Object.defineProperty({__proto__:null,InContext:n,Indicator:o,Standard:r,__namedExportsOrder:B,default:v},Symbol.toStringTag,{value:"Module"}));export{h as B,n as I,r as S,o as a};
---
+++ assets/TextArea-BLk4mUft.js
@@ -0,0 +1,9 @@
+import{j as e,M as a,C as i,a as t}from"./index-KoITqNuk.js";import{useMDXComponents as m}from"./index-DmqVK_gK.js";import{T as l,S as o,F as s}from"./TextArea.stories-8IgT-GXf.js";import"./iframe-2dc5Lf86.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./InputGroup-BjV8g-Q_.js";import"./Label-B7lv4noR.js";import"./Tooltip-B8J4kvmt.js";import"./Icon-BqsuuMcG.js";import"./Collapse-BrKhsD1f.js";import"./ErrorFieldMessage-eNbkZ9Td.js";import"./BaseFieldMessage-Bp22b08z.js";function d(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",...m(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(n.h1,{id:"textarea",children:"TextArea"}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(t,{of:o}),`
+`,e.jsx(n.h2,{id:"med-feilmelding",children:"Med feilmelding"}),`
+`,e.jsxs(n.p,{children:["Sender man inn en string eller et ",e.jsx(n.code,{children:"<ErrorFieldMessage>"}),"-element til ",e.jsx(n.code,{children:"fieldMessage"})," på ",e.jsx(n.code,{children:"<InputGroup>"})," vil TextArea-feltet rendres med ",e.jsx(n.code,{children:"aria-invalid"})," og en feilmelding."]}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(t,{of:s})]})}function E(r={}){const{wrapper:n}={...m(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(d,{...r})}):d(r)}export{E as default};
