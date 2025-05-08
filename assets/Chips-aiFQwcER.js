ecdd5550c6143b610b4cc5bfffacb0b89514261e
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-08 13:20:07 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@5d3f742c6fa6d2b883dd445456f77d90ddf244a6 🚀

---
+++ assets/Chips-aiFQwcER.js
@@ -0,0 +1,15 @@
+import{j as e,M as c,C as s,a as i}from"./index-BJ38UV0C.js";import{useMDXComponents as l}from"./index-DmqVK_gK.js";import{Standard as t}from"./Chip.stories-DOYd0hAW.js";import{Standard as o}from"./ChipRemovable.stories-CpdZEZTw.js";import{Standard as p}from"./ChipSelectable.stories-Podt4zxO.js";import{I as m}from"./InstallImport-B16c5dr9.js";import"./iframe-C2em-eWK.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Chip-cFLlwyzA.js";import"./index-QCImZZ3W.js";import"./Icon-BqsuuMcG.js";import"./Heading-afd3onnD.js";import"./Paragraph-DEYlXMQQ.js";function a(n){const r={h1:"h1",h2:"h2",p:"p",...l(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{title:"Komponenter/Chips"}),`
+`,e.jsx(r.h1,{id:"chips",children:"Chips"}),`
+`,e.jsx(r.p,{children:"Bruk Chips for elementer der brukeren trenger å filtrere eller organisere innhold."}),`
+`,e.jsx(r.p,{children:`Skriv klare og tydelige labels slik at brukeren forstår formålet med ulike chips.
+Om brukeren kan velge chips må de også kunne avvelge chips. Dette er spesielt viktig for multiselect eller filtreringsløsninger.`}),`
+`,e.jsx(m,{packageName:"@sb1/ffe-chips-react",dependencies:["@sb1/ffe-chips","@sb1/ffe-core","@sb1/ffe-icons-react","@sb1/ffe-icons"],stylingDependencies:["@sb1/ffe-icons/less/ffe-icons","@sb1/ffe-chips/less/chip"]}),`
+`,e.jsx(r.h2,{id:"chip",children:"Chip"}),`
+`,e.jsx(s,{of:t}),`
+`,e.jsx(i,{of:t}),`
+`,e.jsx(r.h2,{id:"chipremovable",children:"ChipRemovable"}),`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(r.h2,{id:"chipselectable",children:"ChipSelectable"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(i,{of:p})]})}function R(n={}){const{wrapper:r}={...l(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(a,{...n})}):a(n)}export{R as default};
---
+++ assets/IllustrationCard-DfOCF0TF.js
@@ -0,0 +1,9 @@
+import{j as t,M as d,C as r,a}from"./index-BJ38UV0C.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{I as m,S as o,C as l}from"./IllustrationCard.stories-DUyj-TTt.js";import"./iframe-C2em-eWK.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./WithCardAction-BEfhMYaK.js";import"./Title-CmDdbz_O.js";function s(n){const e={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(d,{of:m}),`
+`,t.jsx(e.h1,{id:"illustrationcard",children:"IllustrationCard"}),`
+`,t.jsx(e.p,{children:`Illustrasjonskort er kort med en venstrestilt illustrasjon. Den tilbyr alle underkomponenter (CardName, Title, Subtext, Text og CardAction)
+og plasserer dem i gitt rekkefølge på høyresiden nedover i kortet (med teksten venstrestilt)`}),`
+`,t.jsx(e.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,t.jsx(r,{of:o}),`
+`,t.jsx(a,{of:o}),`
+`,t.jsxs(e.p,{children:["Kortet kan gjøres mer kompakt med ",t.jsx(e.code,{children:"condensed"}),"-modifyeren."]}),`
+`,t.jsx(r,{of:l})]})}function y(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{y as default};
