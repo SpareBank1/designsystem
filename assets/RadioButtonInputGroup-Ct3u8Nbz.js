f087f068c2b268d200b084e60211975d3fad1461
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-09 08:12:13 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@8ca3e43b966de7a6fd8a021caa4d6495af9cb658 🚀

---
+++ assets/RadioButtonInputGroup-Ct3u8Nbz.js
@@ -0,0 +1,15 @@
+import{j as e,M as a,C as o,a as d}from"./index-Ca2ev8BW.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{R as p,S as r,F as m,W as l,a as c}from"./RadioButtonInputGroup.stories-Cia7Vf8W.js";import"./iframe-CokVx6IB.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./RadioButtonInputGroup-XI03ZI0T.js";import"./index-QCImZZ3W.js";import"./Tooltip-B8J4kvmt.js";import"./Icon-BqsuuMcG.js";import"./Collapse-BrKhsD1f.js";import"./ErrorFieldMessage-eNbkZ9Td.js";import"./BaseFieldMessage-Bp22b08z.js";import"./RadioButton-xf9fc-_r.js";import"./BaseRadioButton-Brnhr778.js";import"./RadioSwitch-BBYjSt2C.js";import"./RadioBlock-BGO6GNky.js";function s(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:p}),`
+`,e.jsx(n.h1,{id:"radiobuttoninputgroup",children:"RadioButtonInputGroup"}),`
+`,e.jsx(n.p,{children:`Brukes når kunden kan velge ett av få valg. Hvis valgene er korte og konsise, kan man ha dem ved siden av hverandre.
+Har valgene litt mer tekst, så bør valgene komme under hverandre.
+Default oppførsel er at det holdes av plass under inputfeltene for å vise en feilmelding uten at innholdet lengre ned endres.
+Dersom man ikke ønsker dette kan det skrus av med å sette extraMargin prop til false.`}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(o,{of:r}),`
+`,e.jsx(d,{of:r}),`
+`,e.jsxs(n.p,{children:["Med ",e.jsx(n.code,{children:"fieldMessage"})]}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsxs(n.p,{children:["Med ",e.jsx(n.code,{children:"<RadioSwitch />"})]}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsxs(n.p,{children:["Med ",e.jsx(n.code,{children:"<RadioBlock />"})]}),`
+`,e.jsx(o,{of:c})]})}function H(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{H as default};
