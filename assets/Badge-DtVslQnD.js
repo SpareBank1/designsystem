83057ef7c9b66f9a707ee625f493c5a1b4ae5a1a
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-12 08:12:43 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@74391256425f6889e80bf55877c40acb1239135c 🚀

---
+++ assets/Badge-DtVslQnD.js
@@ -0,0 +1,15 @@
+import{j as e,M as a,C as s,a as d}from"./index-DPMuwLpC.js";import{useMDXComponents as o}from"./index-DmqVK_gK.js";import{B as l,S as r,I as m,a as p}from"./Badge.stories-C2DJGBM0.js";import{I as f}from"./InstallImport-BVeVGiS6.js";import"./iframe-DFILkVU_.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./Heading-afd3onnD.js";import"./Paragraph-DEYlXMQQ.js";function i(t){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(n.h1,{id:"badge",children:"Badge"}),`
+`,e.jsx(n.p,{children:`En Badge er en liten visuell indikator som viser antall uleste varsler, meldinger eller oppgaver.
+Den plasseres vanligvis over et ikon, ved brukerens navn eller i en meny for å tiltrekke brukerens oppmerksomhet.`}),`
+`,e.jsx(f,{packageName:"@sb1/ffe-badge-react",dependencies:["@sb1/ffe-badge","@sb1/ffe-core"],stylingDependencies:["@sb1/ffe-badge/less/badge.less"]}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(s,{of:r}),`
+`,e.jsx(d,{of:r}),`
+`,e.jsx(n.h3,{id:"med-kontekst",children:"Med kontekst"}),`
+`,e.jsx(n.p,{children:"En badge står gjerne til høyre for det det gjelder."}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(n.h3,{id:"indikator-uten-tall",children:"Indikator uten tall"}),`
+`,e.jsx(n.p,{children:`Badgen kan også brukes uten tall for å indikere at det finnes ny aktivitet. Den skal sitte på et ikon eller lignende,
+og plasseres i øverste venstre hjørne av ikonet.`}),`
+`,e.jsx(s,{of:p})]})}function y(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{y as default};
