4e687ae0947a7d643e055b71a2767bd9e02c3317
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-12 11:11:45 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@1975cfd483bf8904239360ae308c7e48a79b3aa3 🚀

---
+++ assets/ImageCard-BQp2vM9w.js
@@ -0,0 +1,8 @@
+import{j as t,M as s,C as m,a}from"./index-CaO-amtf.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{I as d,S as r}from"./ImageCard.stories-DLMXLvmv.js";import"./iframe-D_Juf1mT.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./WithCardAction-BEfhMYaK.js";import"./Title-CmDdbz_O.js";function o(e){const n={h1:"h1",h2:"h2",p:"p",...i(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:d}),`
+`,t.jsx(n.h1,{id:"imagecard",children:"ImageCard"}),`
+`,t.jsx(n.p,{children:`Bildekort er kort med en gitt maksbredde, minimumshøyde, og et topplasert bilde. Den tilbyr alle underkomponenter
+(CardName, Title, Subtext, Text og CardAction) og plasserer dem i gitt rekkefølge nedover i kortet, med teksten midtstilt,
+nedenfor bildet.`}),`
+`,t.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,t.jsx(m,{of:r}),`
+`,t.jsx(a,{of:r})]})}function b(e={}){const{wrapper:n}={...i(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{b as default};
