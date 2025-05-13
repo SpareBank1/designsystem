b9585913ef8a0529b17dd9ec92a04bdaa6d9253c
Dag Frode Solberg <dag.solberg@sparebank1.no>
2025-05-13 12:17:10 +0200

    fix(ffe-searchable-dropdown-react): Update custom scroll dependency to support React 19

--- packages/ffe-searchable-dropdown-react/src/Results.tsx
+++ packages/ffe-searchable-dropdown-react/src/Results.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Scrollbars } from 'react-custom-scrollbars-2';
+import { Scrollbars } from 'react-custom-scrollbars-4';
 import { Option } from './Option';
 import { NoMatch } from './NoMatch';
 import { Locale } from './types';
