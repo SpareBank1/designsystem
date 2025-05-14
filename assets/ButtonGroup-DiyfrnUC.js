0b4d6efcfcbdcb8d63c35aa0e98ba2152a684562
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-14 11:10:36 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@2270bb7a354b272c70a6b1f1d4f97aa0bed46e19 🚀

--- assets/ButtonGroup-DiyfrnUC.js
+++
@@ -1,6 +0,0 @@
-import{j as n,M as s,C as p,a as m}from"./index-SpQhGQhd.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{B as a,S as r}from"./ButtonGroup.stories-DUXX3ibZ.js";import"./iframe-BfpsFZum.js";import"./index-DQDNmYQF.js";import"./index-C8KIgodY.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./PrimaryButton-woHESu9v.js";import"./BaseButton-BWQWHdxR.js";import"./fixedForwardRef-DqyCgkTx.js";import"./SecondaryButton-Djc0LwPQ.js";import"./TertiaryButton-C7aKO4jv.js";import"./InlineBaseButton-B-uXHNhA.js";function e(o){const t={h1:"h1",h2:"h2",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:a}),`
-`,n.jsx(t.h1,{id:"buttongroup",children:"ButtonGroup"}),`
-`,n.jsx(t.p,{children:"ButtonGroup hjelper deg med å samle alle knappene og få de på en rad. På mindre skjermer vil knappene vises under hverandre."}),`
-`,n.jsx(t.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,n.jsx(p,{of:r}),`
-`,n.jsx(m,{of:r})]})}function S(o={}){const{wrapper:t}={...i(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{S as default};
