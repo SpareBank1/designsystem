8cbb53dccaf298a4ec3b75498a67438a256b4866
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-05-28 11:10:20 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@645ae18c4e292ddffed2a0660d6709084a61d77f 🚀

---
+++ assets/CardAction-BRQAOgwT.js
@@ -0,0 +1,26 @@
+import{j as n,M as s,S as d,C as e,a as c}from"./index-D4AAnRLt.js";import{useMDXComponents as a}from"./index-DmqVK_gK.js";import{C as p,S as t,A as k,W as m}from"./CardAction.stories-BEc5u1U-.js";import"./iframe-BBwsEcN9.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./CardBase-CTHMSme-.js";import"./index-QCImZZ3W.js";import"./WithCardAction-BEfhMYaK.js";import"./IconCard-DjVvW_uF.js";import"./Title-CmDdbz_O.js";import"./Heading-D_Y6r1eI.js";import"./Paragraph-DEYlXMQQ.js";import"./Icon-BqsuuMcG.js";function i(o){const r={code:"code",h1:"h1",h2:"h2",p:"p",...a(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{of:p}),`
+`,n.jsx(r.h1,{id:"cardaction-klikkbare-kort",children:"CardAction (Klikkbare kort)"}),`
+`,n.jsx(r.h2,{id:"kode",children:"Kode"}),`
+`,n.jsx(d,{code:`
+<CardBase>
+    {({ CardAction }: CardActionRenderProps) => (
+        <>
+            <Heading4>
+                <CardAction href="https://design.sparebank1.no">
+                    Lenke
+                </CardAction>
+            </Heading4>
+            <Paragraph>Hele kortet er klikkbart</Paragraph>
+        </>
+    )}
+</CardBase>
+`}),`
+`,n.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,n.jsxs(r.p,{children:["Kort er ikke klikkbare som standard. Bruk ",n.jsx(r.code,{children:"<CardAction/>"})," for å lage et klikkbart kort. En ",n.jsx(r.code,{children:"<CardAction/>"})," kan brukes som en lenke eller en knapp ved hjelp av as."]}),`
+`,n.jsx(r.p,{children:"Som lenke:"}),`
+`,n.jsx(e,{of:t}),`
+`,n.jsx(c,{of:t}),`
+`,n.jsx(r.p,{children:"Som knapp:"}),`
+`,n.jsx(e,{of:k}),`
+`,n.jsxs(r.p,{children:["I ",n.jsx(r.code,{children:"Title"}),":"]}),`
+`,n.jsx(e,{of:m})]})}function P(o={}){const{wrapper:r}={...a(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(i,{...o})}):i(o)}export{P as default};
