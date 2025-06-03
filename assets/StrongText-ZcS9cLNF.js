2f8a95e61e05bd858fcb54f23fe35a4403decfb0
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-06-03 11:10:13 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@1f9997bc6579f1509b58ebe178154c4e2a954c11 🚀

--- assets/StrongText-ZcS9cLNF.js
+++
@@ -1,5 +0,0 @@
-import{j as t,M as i,C as m,a}from"./index-CwD-i52G.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{S as p,a as e}from"./StrongText.stories-93pHEdhX.js";import"./iframe-COgSHNXk.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";function r(n){const o={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
-`,t.jsx(o.h1,{id:"strongtext",children:"StrongText"}),`
-`,t.jsx(o.p,{children:"Bold tekst som brukes når vi vil utheve noe i en lengre tekst."}),`
-`,t.jsx(m,{of:e}),`
-`,t.jsx(a,{of:e})]})}function g(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{g as default};
