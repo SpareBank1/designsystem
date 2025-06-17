a90be58e7556b3b9581a728a2b52987371202a1c
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-06-17 15:11:06 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@14af9e03cb6735e06f43b4eb4fd886b61debd049 🚀

--- assets/Dropdown-B4GUcNSS.js
+++
@@ -1,7 +0,0 @@
-import{j as n,M as i,C as m,a}from"./index-BuwnlxNu.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{D as p,S as e}from"./Dropdown.stories-B_LSwo9R.js";import{I as d}from"./InstallImport-CTNYjYui.js";import"./iframe-CpQApkin.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./InputGroup-DOKvm3eQ.js";import"./Icon-BqsuuMcG.js";import"./Collapse-BrKhsD1f.js";import"./Heading-D_Y6r1eI.js";import"./Paragraph-DEYlXMQQ.js";const f=["@sb1/ffe-form","@sb1/ffe-buttons","@sb1/ffe-core"];function t(o){const r={h1:"h1",h2:"h2",p:"p",...s(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:p}),`
-`,n.jsx(r.h1,{id:"dropdown",children:"Dropdown"}),`
-`,n.jsx(r.p,{children:"Dropdown er en vanlig nedtrekksliste som kan brukes man når man har 5-10 valg å velge mellom. Har du mindre enn 5 valg, bruk radio buttons istedenfor."}),`
-`,n.jsx(d,{packageName:"@sb1/ffe-dropdown-react",dependencies:f}),`
-`,n.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,n.jsx(m,{of:e}),`
-`,n.jsx(a,{of:e})]})}function X(o={}){const{wrapper:r}={...s(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(t,{...o})}):t(o)}export{X as default};
---
+++ assets/ImageCard-ChvbStqG.js
@@ -0,0 +1,30 @@
+import{j as e,M as a,S as s,C as m,a as d}from"./index-5aEBrjF7.js";import{useMDXComponents as r}from"./index-DmqVK_gK.js";import{I as l,S as i}from"./ImageCard.stories-DLMXLvmv.js";import"./iframe-BJNE2wdD.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./WithCardAction-BEfhMYaK.js";import"./Title-CmDdbz_O.js";function o(n){const t={h1:"h1",h2:"h2",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(t.h1,{id:"imagecard",children:"ImageCard"}),`
+`,e.jsx(t.p,{children:`Bildekort er kort med en gitt maksbredde, minimumshøyde, og et topplasert bilde. Den tilbyr alle underkomponenter
+(CardName, Title, Subtext, Text og CardAction) og plasserer dem i gitt rekkefølge nedover i kortet, med teksten midtstilt,
+nedenfor bildet.`}),`
+`,e.jsx(t.h2,{id:"kode",children:"Kode"}),`
+`,e.jsx(s,{code:`
+<ImageCard 
+    imageSrc="https://design.sparebank1.no/assets/images/illustrations/illustration-1.svg"
+    imageAltText="To jenter som går å snakker sammen"
+>
+    {({ CardName, Title, Subtext, Text }) => (
+        <>
+            <CardName>Kortnavn</CardName>
+            <Title>Tittel</Title>
+            <Subtext>En liten undertekst</Subtext>
+            <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+            </Text>
+        </>
+    )}
+</ImageCard>
+`}),`
+`,e.jsx(t.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(m,{of:i}),`
+`,e.jsx(d,{of:i})]})}function S(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{S as default};
