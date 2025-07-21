4cc3d8ab2e3c8189205617cd2ed9bcb5bb7f37e5
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-07-21 11:10:50 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@f3f8f68ee889d687bb53b43e257cafe07ca3a806 🚀

---
+++ assets/MicroText-cO2o227N.js
@@ -0,0 +1,5 @@
+import{j as t,M as i,C as m,a}from"./index-BSwe_shE.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{M as c,S as r}from"./MicroText.stories-BRNTvGg_.js";import"./iframe-CZSnEi23.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";function e(n){const o={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:c}),`
+`,t.jsx(o.h1,{id:"microtext",children:"MicroText"}),`
+`,t.jsx(o.p,{children:"Den minste skriftstørrelsen vi har."}),`
+`,t.jsx(m,{of:r}),`
+`,t.jsx(a,{of:r})]})}function D(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{D as default};
