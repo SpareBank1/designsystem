1c5cace22cd3ae2f288d43710d9e375742b5b6e8
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-07-22 13:22:13 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@88e8baada2e92f71d6e8913f57574bb3ecf7f80e 🚀

---
+++ assets/BackButton-DYpKm1C8.js
@@ -0,0 +1,6 @@
+import{j as t,M as s,C as a,a as m}from"./index-CJ85VgmR.js";import{useMDXComponents as e}from"./index-DmqVK_gK.js";import{B as p,S as r}from"./BackButton.stories-BF8TdO4B.js";import"./iframe-Dxqd53oa.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./InlineBaseButton-B-uXHNhA.js";import"./fixedForwardRef-DqyCgkTx.js";import"./index-QCImZZ3W.js";import"./Icon-BqsuuMcG.js";function i(n){const o={h1:"h1",h2:"h2",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(o.h1,{id:"backbutton",children:"BackButton"}),`
+`,t.jsx(o.p,{children:"Brukes der du vil gi brukerne muligheten til å navigere tilbake dit de var."}),`
+`,t.jsx(o.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,t.jsx(a,{of:r}),`
+`,t.jsx(m,{of:r})]})}function M(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(i,{...n})}):i(n)}export{M as default};
