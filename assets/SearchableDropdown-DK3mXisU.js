9da658aa86ad159c5ffc3693ba33d8a7f4262227
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-07-25 11:10:19 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@e9387e334b422a8d3f8459873ed4fe553a05ec50 🚀

---
+++ assets/SearchableDropdown-DK3mXisU.js
@@ -0,0 +1,20 @@
+import{j as e,M as d,C as r,a as l}from"./index-CWAoxve-.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{S as a,a as s,D as m,C as p,E as c,b as u,P as h,W as k}from"./SearchableDropdown.stories-Dft7QUA0.js";import"./iframe-9GAMMy_l.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./ListBox-iefsrwiY.js";import"./index-DlnE8UeV.js";import"./Paragraph-DEYlXMQQ.js";import"./index-D9tCDUwu.js";import"./Icon-BqsuuMcG.js";import"./MicroText-T72i9KwR.js";import"./InputGroup-DOKvm3eQ.js";import"./Collapse-BrKhsD1f.js";function o(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:a}),`
+`,e.jsx(n.h1,{id:"searchabledropdown",children:"SearchableDropdown"}),`
+`,e.jsx(n.p,{children:`Har du 10 eller flere alternativer i nedtrekkslisten din, anbefaler vi at du bruker nedtrekkslisten med søk.
+Det er for å gjøre det enda enklere for brukerne å finne det de er på jakt etter.`}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(r,{of:s}),`
+`,e.jsx(l,{of:s}),`
+`,e.jsxs(n.p,{children:["Hvis du vil vise mer enn en type informasjon, kan du legge det til i ",e.jsx(n.code,{children:"dropdownAttributes"}),"."]}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(n.p,{children:"Vil du lage din egen søkemetode kan du gjøre det også. For eksempel om du vil ignorere mellomrom."}),`
+`,e.jsx(r,{of:p}),`
+`,e.jsx(n.p,{children:"Hvis du ikke får noen resultater på søket ditt, men forsatt ønsker å gi brukerne noen forslag, kan du legge til ekstra søkeresultater."}),`
+`,e.jsx(r,{of:c}),`
+`,e.jsxs(n.p,{children:["Vil du bestemme hvilket element som er valgt, kan du bruke ",e.jsx(n.code,{children:"selectedItem"}),"."]}),`
+`,e.jsx(r,{of:u}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å ha ekstra tekst på bunnen av lista, kan du bruke ",e.jsx(n.code,{children:"postListElement"}),"."]}),`
+`,e.jsx(r,{of:h}),`
+`,e.jsx(n.h2,{id:"med-beskrivelse",children:"Med beskrivelse"}),`
+`,e.jsxs(n.p,{children:["Du kan bruke ",e.jsx(n.code,{children:"description='tekst her'"})," på ",e.jsx(n.code,{children:"InputGroup"})," for å legge til en beskrivelse mellom label og SearchableDropdown."]}),`
+`,e.jsx(r,{of:k})]})}function A(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{A as default};
