526bb6200eadb30bfe7864cb53c195205f3fa453
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-08-01 11:10:10 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@658b3397b163974231c42d10cebe439e0ffe9cb3 🚀

---
+++ assets/Checkbox-jdcVOtqx.js
@@ -0,0 +1,14 @@
+import{j as e,M as a,C as s,a as d}from"./index-D5Vy30LN.js";import{useMDXComponents as t}from"./index-DmqVK_gK.js";import{C as l,S as o,I as c,H as m,A as h,R as p}from"./Checkbox.stories-8LLF4EsA.js";import"./iframe-C_ZJ9AsJ.js";import"./index-DQDNmYQF.js";import"./index-CGBZXJ9a.js";import"./index-DrriUsT5.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-QCImZZ3W.js";import"./InputGroup-0tyP-SwI.js";import"./Label-B7lv4noR.js";import"./Tooltip-BmnhgMk7.js";import"./Icon-BqsuuMcG.js";import"./Collapse-BrKhsD1f.js";import"./ErrorFieldMessage-eNbkZ9Td.js";import"./BaseFieldMessage-Bp22b08z.js";function i(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",...t(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(n.h1,{id:"checkbox",children:"Checkbox"}),`
+`,e.jsx(n.p,{children:"Checkbox brukes der du har 4 eller færre valgalternativer, og du vil gi brukeren muligheten til å velge mer enn ett alternativ."}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(s,{of:o}),`
+`,e.jsx(d,{of:o}),`
+`,e.jsxs(n.p,{children:["Ved å sende inn ",e.jsx(n.code,{children:"inline={false}"}),", kan de også vises under hverandre:"]}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsxs(n.p,{children:["Hvis du skal ha en skjult label, brukes ",e.jsx(n.code,{children:"hiddenLabel={true}"}),", og label sendes fortsatt inn som child."]}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsxs(n.p,{children:["Du kan merke at et felt er ugyldig ved å sette ",e.jsx(n.code,{children:'aria-invalid="true"'}),"."]}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(n.p,{children:"Du kan sende inn children som en funksjon, for å rendre din egen label. Funksjonen mottar props du kan spre på labelen."}),`
+`,e.jsx(s,{of:p})]})}function X(r={}){const{wrapper:n}={...t(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{X as default};
---
+++ assets/Progressbar.stories-CQV3OIUk.js
@@ -0,0 +1,21 @@
+import{R as n}from"./index-DQDNmYQF.js";import{c as P}from"./index-QCImZZ3W.js";const r=({success:e=!1,className:b,...f})=>n.createElement("progress",{className:P("ffe-progressbar",{"ffe-progressbar--theme-success":e},b),...f});r.__docgenInfo={description:"",methods:[],displayName:"Progressbar",props:{success:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}}}};const S={title:"Komponenter/Progressbar/Progressbar",component:r},s={args:{value:40,max:100},render:e=>n.createElement(r,{...e})},a={args:{value:40,max:100,success:!0},render:e=>n.createElement(r,{...e})},o={args:{value:2500,max:5e3,"aria-label":"Nedbetaling av lån i prosent"},render:e=>n.createElement(r,{...e})};var t,c,m;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    value: 40,
+    max: 100
+  },
+  render: args => <Progressbar {...args}></Progressbar>
+}`,...(m=(c=s.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var l,d,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    value: 40,
+    max: 100,
+    success: true
+  },
+  render: args => <Progressbar {...args}></Progressbar>
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var p,u,i;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    value: 2500,
+    max: 5000,
+    'aria-label': 'Nedbetaling av lån i prosent'
+  },
+  render: args => <Progressbar {...args}></Progressbar>
+}`,...(i=(u=o.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};const E=["Standard","Success","WithAria"];export{s as Standard,a as Success,o as WithAria,E as __namedExportsOrder,S as default};
