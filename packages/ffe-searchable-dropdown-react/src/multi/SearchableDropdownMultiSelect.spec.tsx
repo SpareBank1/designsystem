3194ecaa6007f42563df5c74d4ebdfdc1bf97785
Eirik Vikanes <eirik.vikanes@sparebank1.no>
2025-08-19 13:04:38 +0200

    fix(ffe-searchable-dropdown-react): Do not run open or close callbacks on mount

--- packages/ffe-searchable-dropdown-react/src/multi/SearchableDropdownMultiSelect.spec.tsx
+++ packages/ffe-searchable-dropdown-react/src/multi/SearchableDropdownMultiSelect.spec.tsx
@@ -583,4 +583,62 @@ describe('SearchableDropdownMultiSelect', () => {
         await user.click(screen.getByTestId('change-multiselect-input'));
         expect(screen.queryByText(companies[0].organizationName)).toBeNull();
     });
+
+    it('does not run onOpen or onClose on mount', async () => {
+        const onChange = jest.fn();
+        const onOpen = jest.fn();
+        const onClose = jest.fn();
+
+        render(
+            <SearchableDropdownMultiSelectButton
+                id="id"
+                labelledById="labelId"
+                dropdownAttributes={['organizationName', 'organizationNumber']}
+                dropdownList={companies}
+                onChange={onChange}
+                onOpen={onOpen}
+                onClose={onClose}
+                searchAttributes={['organizationName', 'organizationNumber']}
+                locale="nb"
+            />,
+        );
+
+        expect(onOpen).not.toHaveBeenCalled();
+        expect(onClose).not.toHaveBeenCalled();
+    });
+
+    it('runs onOpen when opening dropdown and onClose when closing dropdown', async () => {
+        const onChange = jest.fn();
+        const onOpen = jest.fn();
+        const onClose = jest.fn();
+        const user = userEvent.setup();
+
+        render(
+            <>
+                <button>Knapp</button>
+                <SearchableDropdownMultiSelectButton
+                    id="id"
+                    labelledById="labelId"
+                    dropdownAttributes={['organizationName', 'organizationNumber']}
+                    dropdownList={companies}
+                    onChange={onChange}
+                    onOpen={onOpen}
+                    onClose={onClose}
+                    searchAttributes={['organizationName', 'organizationNumber']}
+                    locale="nb"
+                />
+            </>,
+        );
+
+        const input = screen.getByRole('combobox');
+        const button = screen.getByText('Knapp');
+
+        // Focus combobox to open dropdown
+        await user.click(input);
+        expect(onOpen).toHaveBeenCalledTimes(1);
+
+        // Unfocus combobox to close dropdown
+        await user.click(button);
+        expect(onClose).toHaveBeenCalledTimes(1);
+    });
 });
