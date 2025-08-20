fd5a8a2b4aa532829f155a18a4ff7c135a0448ff
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-08-20 11:10:07 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@0212e1f6e52e54b065ab37c97c7286d0a96702c7 🚀

---
+++ assets/ChartDonut-C308sikQ.js
@@ -0,0 +1,10 @@
+import{j as n,M as p,C as r,a as s}from"./index-Baf5jjcs.js";import{useMDXComponents as m}from"./index-DmqVK_gK.js";import{C as d,S as e,a}from"./ChartDonut.stories-CLIZbOwv.js";import{I as c}from"./InstallImport-a0eNYgkV.js";import"./iframe-CVxcvSut.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./Heading-YepvjHrD.js";import"./index-D2FocPV0.js";import"./Paragraph-7jO2Jnpx.js";const h=["@sb1/ffe-core"];function i(o){const t={h1:"h1",h2:"h2",p:"p",...m(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(p,{of:d}),`
+`,n.jsx(t.h1,{id:"chartdonut",children:"ChartDonut"}),`
+`,n.jsx(t.p,{children:"Diagram i SpareBank 1-drakt."}),`
+`,n.jsx(c,{packageName:"@sb1/ffe-chart-donut-react",dependencies:h}),`
+`,n.jsx(t.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,n.jsx(r,{of:e}),`
+`,n.jsx(s,{of:e}),`
+`,n.jsx(t.p,{children:"Med egentilpasset innhold."}),`
+`,n.jsx(r,{of:a}),`
+`,n.jsx(s,{of:a})]})}function X(o={}){const{wrapper:t}={...m(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(i,{...o})}):i(o)}export{X as default};
