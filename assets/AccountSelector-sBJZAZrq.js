75c68ed9f4456971a563f9b9907a277c068a2117
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-08-29 08:11:47 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@aebde9f5ace3ed2dd1ff5deaa9f37c0e0ac8c9c6 🚀

---
+++ assets/AccountSelector-sBJZAZrq.js
@@ -0,0 +1,32 @@
+import{j as e,M as d,C as t,a as l}from"./index-UvXdqHvp.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{A as a,S as r,a as c,b as m,N as u,L as k,H as p,P as j,C as h,W as x}from"./AccountSelector.stories-BfIDR2nW.js";import{I as v}from"./InstallImport-CVIU8IZe.js";import{d as g}from"./ffe-dependencies-CaqpW2RE.js";import"./iframe-DeV6odNx.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-D2FocPV0.js";import"./texts-w4hjI5lD.js";import"./index-DSyfhXNB.js";import"./index-D9tCDUwu.js";import"./Paragraph-7jO2Jnpx.js";import"./Icon-D_nm5xPy.js";import"./formatNumber-CbrmByI5.js";import"./MicroText-DswQHKl7.js";import"./InputGroup-CsZXON4u.js";import"./Collapse-BVkyKCdf.js";import"./Heading-YepvjHrD.js";function o(s){const n={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:a}),`
+`,e.jsx(n.h1,{id:"accountselector",children:"AccountSelector"}),`
+`,e.jsx(n.p,{children:"Kontovelger brukes som navnet tilsier til å velge konto på de ulike platformene våre. Forskjellen på denne og en vanlig dropdown, er at den er tilpasset situasjoner der man ønsker å velge en konto, siden den vil vise valgt kontonummer og saldo, etter du har valgt et alternativ."}),`
+`,e.jsxs(n.p,{children:["I mobilbanken så er foretrukken løsning ",e.jsx(n.code,{children:"openAccountPicker"})," i stedet for denne."]}),`
+`,e.jsx(v,{packageName:"@sb1/ffe-account-selector-react",dependencies:g}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(t,{of:r}),`
+`,e.jsx(l,{of:r}),`
+`,e.jsx(n.h2,{id:"saldo",children:"Saldo"}),`
+`,e.jsx(n.p,{children:"Kontovelger som også viser saldo/beløp"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(n.h2,{id:"tillate-å-skrive-eget",children:"Tillate å skrive eget"}),`
+`,e.jsxs(n.p,{children:["Trenger du å tillate at brukere kan skrive inn sitt eget kontonummer kan du aktivere dette ved å sende med ",e.jsx(n.code,{children:"allowCustomAccount={true}"}),"."]}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(n.h2,{id:"ingen-formattering",children:"Ingen formattering"}),`
+`,e.jsxs(n.p,{children:["Hvis du ikke ønsker at kontonummeret en bruker skriver inn skal formatteres, kan du skru det av med ",e.jsx(n.code,{children:"formatAccountNumber={false}"}),"."]}),`
+`,e.jsx(t,{of:u}),`
+`,e.jsx(n.h2,{id:"egendefinert-listelement",children:"Egendefinert listElement"}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å gjøre endringer på selve listevisningen, kan du gjøre det med ",e.jsx(n.code,{children:"listElementBody"}),"."]}),`
+`,e.jsx(t,{of:k}),`
+`,e.jsx(n.h2,{id:"skjule-kontodetlajer",children:"Skjule kontodetlajer"}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å skjule kontodetaljer, kan du bruke ",e.jsx(n.code,{children:"hideAccountDetails"}),"."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(n.h2,{id:"ekstra-tekst-nederst",children:"Ekstra tekst nederst"}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å ha ekstra tekst på bunnen av lista, kan du bruke ",e.jsx(n.code,{children:"postListElement"}),"."]}),`
+`,e.jsx(t,{of:j}),`
+`,e.jsx(n.h2,{id:"tilpass-visningen-av-valgt-konto",children:"Tilpass visningen av valgt konto"}),`
+`,e.jsxs(n.p,{children:["Dersom du ønsker å vise en annen informasjon enn kontonavn i inputen, så kan du velge hvilken attribute som brukes med ",e.jsx(n.code,{children:"displayAttribute"}),"."]}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(n.h2,{id:"med-beskrivelse",children:"Med beskrivelse"}),`
+`,e.jsxs(n.p,{children:["Du kan bruke ",e.jsx(n.code,{children:"description='tekst her'"})," på ",e.jsx(n.code,{children:"InputGroup"})," for å legge til en beskrivelse mellom label og AccountSelector."]}),`
+`,e.jsx(t,{of:x})]})}function G(s={}){const{wrapper:n}={...i(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(o,{...s})}):o(s)}export{G as default};
---
+++ assets/SmallText-Cvt3iErz.js
@@ -0,0 +1,5 @@
+import{j as t,M as a,C as m,a as i}from"./index-UvXdqHvp.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{S as l,a as e}from"./SmallText.stories-VD-d2w-C.js";import"./iframe-DeV6odNx.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-D2FocPV0.js";function r(n){const o={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:l}),`
+`,t.jsx(o.h1,{id:"smalltext",children:"SmallText"}),`
+`,t.jsx(o.p,{children:"En tekst som skal være mindre enn den vanlige skriftstørrelsen."}),`
+`,t.jsx(m,{of:e}),`
+`,t.jsx(i,{of:e})]})}function S(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{S as default};
