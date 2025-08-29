bc7eaea3e7d599698b8032458d5cff9c66c1f89d
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-08-29 13:17:12 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@eb1b5599aab8f13e0af8c697eadf6f50cef9fec9 🚀

--- assets/Paragraph-2Qvfkd-t.js
+++
@@ -1,5 +0,0 @@
-import{j as t,M as e,C as i,a as m}from"./index-UvXdqHvp.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{P as p,S as r}from"./Paragraph.stories-CZWh3DBg.js";import"./iframe-DeV6odNx.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-D2FocPV0.js";function a(n){const o={h1:"h1",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
-`,t.jsx(o.h1,{id:"paragraph",children:"Paragraph"}),`
-`,t.jsx(o.p,{children:"Et vanlig tekstavsnitt"}),`
-`,t.jsx(i,{of:r}),`
-`,t.jsx(m,{of:r})]})}function g(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(a,{...n})}):a(n)}export{g as default};
