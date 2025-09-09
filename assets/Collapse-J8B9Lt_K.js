c2c971e67e86ef43261e662fd233599e6b977bd2
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-09-09 10:47:14 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@93a1f4997f5202a159d0dd30ff0160f9c3636070 🚀

--- assets/Collapse-J8B9Lt_K.js
+++
@@ -1,8 +0,0 @@
-import{j as e,M as i,C as a,a as p}from"./index-BtMYWq8G.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{C as m,S as r}from"./Collapse.stories-rkPbNhmo.js";import{I as c}from"./InstallImport-DACXjcPb.js";import"./iframe-tqXtvfUp.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-D2FocPV0.js";import"./ExpandButton-BW-fbTLH.js";import"./fixedForwardRef-Dm1tRvsG.js";import"./Icon-D_nm5xPy.js";import"./Heading-YepvjHrD.js";import"./Paragraph-7jO2Jnpx.js";const d=["@sb1/ffe-core"];function t(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:m}),`
-`,e.jsx(n.h1,{id:"collapse",children:"Collapse"}),`
-`,e.jsxs(n.p,{children:["Komponent for å enkelt lage ekspanderbart område. Hvorvidt området er ekspandert eller ikke styres av konsumenten via property ",e.jsx(n.code,{children:"isOpen"}),`.
-Et callback kan gis via property `,e.jsx(n.code,{children:"onRest"})," som kjøres når transisjonen er ferdig."]}),`
-`,e.jsx(c,{packageName:"@sb1/ffe-collapse-react",dependencies:d}),`
-`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
-`,e.jsx(a,{of:r}),`
-`,e.jsx(p,{of:r})]})}function X(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{X as default};
