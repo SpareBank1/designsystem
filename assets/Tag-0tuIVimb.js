c42e4d34413761a6a66eebeb75bad1fb22fd8474
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-09-18 11:11:06 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@3a6c11f03c670f69866e40b545e6dfab546da310 🚀

---
+++ assets/Tag-0tuIVimb.js
@@ -0,0 +1,19 @@
+import{j as e,M as c,C as i,a as d}from"./index-DqSvDkn9.js";import{useMDXComponents as t}from"./index-DmqVK_gK.js";import{T as a,E as l,S as h,W as r,D as m}from"./Tag.stories-CiugoykO.js";import{I as j}from"./InstallImport-Ckl6ChH0.js";import"./iframe-C_x4MhpQ.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-D2FocPV0.js";import"./Icon-D_nm5xPy.js";import"./Heading-YepvjHrD.js";import"./Paragraph-7jO2Jnpx.js";const x=["@sb1/ffe-core","@sb1/ffe-icons-react","@sb1/ffe-tags","@sb1/ffe-icons"];function o(s){const n={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...t(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:a}),`
+`,e.jsx(n.h1,{id:"tags",children:"Tags"}),`
+`,e.jsxs(n.p,{children:[`En Tag er en liten, fleksibel komponent som brukes til å kategorisere, merke eller gi ekstra kontekst til innhold.
+Den finnes i 6 fargevarianter; `,e.jsx(n.code,{children:"neutral"}),", ",e.jsx(n.code,{children:"info"}),", ",e.jsx(n.code,{children:"success"}),", ",e.jsx(n.code,{children:"warning"}),", ",e.jsx(n.code,{children:"critical"})," og ",e.jsx(n.code,{children:"tip"}),`. Hver farge kommer i
+to varianter; `,e.jsx(n.code,{children:"emphasis"})," og ",e.jsx(n.code,{children:"subtle"}),"."]}),`
+`,e.jsx(j,{packageName:"@sb1/ffe-tags-react",dependencies:x}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(n.h3,{id:"emphasis",children:"Emphasis"}),`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(n.h3,{id:"subtle",children:"Subtle"}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(n.h2,{id:"med-ikon",children:"Med Ikon"}),`
+`,e.jsxs(n.p,{children:["En Tag kan brukes med eller uten ikon. Ikonet sendes inn som en child til Tag komponenten ved bruk av ",e.jsx(n.code,{children:"@sb1/ffe-icons-react"}),` pakken.
+Du må installere pakken og importere stylingen og ikonet separat.`]}),`
+`,e.jsx(i,{of:r}),`
+`,e.jsx(d,{of:r}),`
+`,e.jsx(n.h2,{id:"forskjellige-størrelser",children:"Forskjellige størrelser"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"Tag"})," tilbyr ",e.jsx(n.code,{children:"size"})," i ",e.jsx(n.code,{children:"sm"}),", ",e.jsx(n.code,{children:"md"})," og ",e.jsx(n.code,{children:"lg"}),". Default er md."]}),`
+`,e.jsx(i,{of:m})]})}function y(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(o,{...s})}):o(s)}export{y as default};
