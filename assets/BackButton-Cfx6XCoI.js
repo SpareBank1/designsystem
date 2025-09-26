1a3c9de39b26e00dcc069428907c5f58e09e281d
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-09-26 08:13:22 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@66622f3e573b7ae7211f79ed79e32d46bdfcbcfe 🚀

---
+++ assets/BackButton-Cfx6XCoI.js
@@ -0,0 +1,6 @@
+import{j as t,M as s,C as a,a as m}from"./index-C6_L7qtr.js";import{useMDXComponents as e}from"./index-DmqVK_gK.js";import{B as p,S as r}from"./BackButton.stories-Crnmk0Ax.js";import"./iframe-i0HVUDda.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./InlineBaseButton-CBnMDeqF.js";import"./fixedForwardRef-DqyCgkTx.js";import"./index-D2FocPV0.js";import"./Icon-D_nm5xPy.js";function i(n){const o={h1:"h1",h2:"h2",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(o.h1,{id:"backbutton",children:"BackButton"}),`
+`,t.jsx(o.p,{children:"Brukes der du vil gi brukerne muligheten til å navigere tilbake dit de var."}),`
+`,t.jsx(o.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,t.jsx(a,{of:r}),`
+`,t.jsx(m,{of:r})]})}function M(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(i,{...n})}):i(n)}export{M as default};
---
+++ assets/EmphasizedText-8Ximy9II.js
@@ -0,0 +1,5 @@
+import{j as t,M as i,C as m,a}from"./index-C6_L7qtr.js";import{useMDXComponents as r}from"./index-DmqVK_gK.js";import{E as p,S as o}from"./EmphasizedText.stories-BhvsdUOa.js";import"./iframe-i0HVUDda.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-D2FocPV0.js";function s(e){const n={h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(n.h1,{id:"emphasizedtext",children:"EmphasizedText"}),`
+`,t.jsx(n.p,{children:"Kursiv tekst som brukes der vi vil utheve et ord eller en setning i en lengre tekst."}),`
+`,t.jsx(m,{of:o}),`
+`,t.jsx(a,{of:o})]})}function v(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(s,{...e})}):s(e)}export{v as default};
