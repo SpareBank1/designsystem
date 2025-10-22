19de10d117b21af09ec51e52626ef5103e551fa7
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-10-22 08:13:42 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@cc94f0fd7b6c361fb2811d37fa3158a80b99d57e 🚀

---
+++ assets/AccountSelectorMulti-Tqs-GDgZ.js
@@ -0,0 +1,10 @@
+import{j as e,M as c,C as r,a as m}from"./index-Bp0NURbs.js";import{useMDXComponents as s}from"./index-DmqVK_gK.js";import{A as p,S as n,W as l}from"./AccountSelectorMulti.stories-sDqHIDKo.js";import{I as a}from"./InstallImport-DZMlLJTw.js";import{d}from"./ffe-dependencies-CaqpW2RE.js";import"./iframe-Debu0IxV.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./texts-w4hjI5lD.js";import"./index-DSyfhXNB.js";import"./index-D2FocPV0.js";import"./index-D9tCDUwu.js";import"./Paragraph-7jO2Jnpx.js";import"./Icon-D_nm5xPy.js";import"./formatNumber-CbrmByI5.js";import"./ChipRemovable-KNg7V90m.js";import"./InputGroup-CsZXON4u.js";import"./Collapse-BVkyKCdf.js";import"./TertiaryButton-Bljp0IOX.js";import"./fixedForwardRef-Dm1tRvsG.js";import"./Heading-YepvjHrD.js";function i(t){const o={code:"code",h1:"h1",h2:"h2",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:p}),`
+`,e.jsx(o.h1,{id:"accountselectormulti",children:"AccountSelectorMulti"}),`
+`,e.jsx(o.p,{children:"Kontovelger med støtte for å velge flere kontoer på én gang."}),`
+`,e.jsx(a,{packageName:"@sb1/ffe-account-selector-react",dependencies:d}),`
+`,e.jsx(o.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(r,{of:n}),`
+`,e.jsx(m,{of:n}),`
+`,e.jsx(o.h2,{id:"med-beskrivelse",children:"Med beskrivelse"}),`
+`,e.jsxs(o.p,{children:["Du kan bruke ",e.jsx(o.code,{children:"description='tekst her'"})," på ",e.jsx(o.code,{children:"InputGroup"})," for å legge til en beskrivelse mellom label og AccountSelectorMulti."]}),`
+`,e.jsx(r,{of:l})]})}function L(t={}){const{wrapper:o}={...s(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(i,{...t})}):i(t)}export{L as default};
