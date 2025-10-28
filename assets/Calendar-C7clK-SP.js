650cd69f7754237aac93f0e20d52542a6e8adee6
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-10-28 11:11:50 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@f2b52cb986c04259c56d342893325bcc11f5e29b 🚀

---
+++ assets/Calendar-C7clK-SP.js
@@ -0,0 +1,10 @@
+import{j as e,M as d,C as t,a}from"./index-Bqw3HZ8L.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{C as m,S as o,W as l}from"./Calendar.stories-DT8OzVlg.js";import"./iframe-C3n7FY_l.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./Calendar-D5vWCd0d.js";import"./index-D2FocPV0.js";import"./Icon-D_nm5xPy.js";import"./Dropdown-DU2fHO9Y.js";import"./v4-CtRu48qb.js";function s(n){const r={code:"code",h1:"h1",h2:"h2",p:"p",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:m}),`
+`,e.jsx(r.h1,{id:"calendar",children:"Calendar"}),`
+`,e.jsx(r.p,{children:"Kalender-delen av Datepicker kan brukes for seg om man trenger det."}),`
+`,e.jsx(r.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(t,{of:o}),`
+`,e.jsx(a,{of:o}),`
+`,e.jsx(r.h2,{id:"med-nedtrekkslister-for-måned-og-år",children:"Med nedtrekkslister for måned og år"}),`
+`,e.jsxs(r.p,{children:["Kalenderen støtter nedtrekkslister for måned- og årsvalg ved å sette ",e.jsx(r.code,{children:"dropdownCaption={true}"}),". Dette gjør det mulig for brukeren å navigere raskt til spesifikke måneder og år uten å måtte klikke seg gjennom kalenderen steg for steg."]}),`
+`,e.jsx(r.p,{children:"Dette er spesielt nyttig når brukeren trenger å velge datoer langt frem eller tilbake i tid."}),`
+`,e.jsx(t,{of:l})]})}function w(n={}){const{wrapper:r}={...i(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(s,{...n})}):s(n)}export{w as default};
