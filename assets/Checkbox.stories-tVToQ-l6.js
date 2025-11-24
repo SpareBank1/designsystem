13fea5add043542ca2b1bcca93be3cb3da3e266d
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-11-24 11:12:02 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@beda772b3959b2d2e56c8fd641d0a4ffaeee9e30 🚀

---
+++ assets/Checkbox.stories-tVToQ-l6.js
@@ -0,0 +1,84 @@
+import{R as e,r as J}from"./index-DQDNmYQF.js";import{c as u}from"./index-D2FocPV0.js";import{I as M}from"./InputGroup-BBDrD5qR.js";const r=e.forwardRef(({children:a,hiddenLabel:s,inline:D=!0,noMargins:G,id:V,checked:j,onChange:P,disabled:i,...L},T)=>{const q=J.useId(),m=V??q,p={className:u({"ffe-checkbox":!0,"ffe-checkbox--inline":D,"ffe-checkbox--no-margin":G,"ffe-checkbox--hidden-label":s}),htmlFor:m};return e.createElement(e.Fragment,null,e.createElement("input",{ref:T,className:"ffe-hidden-checkbox",id:m,type:"checkbox",checked:j,disabled:i,onChange:i?void 0:P,...L}),typeof a=="function"?a(p):e.createElement("label",{...p},e.createElement("span",{className:u("ffe-checkbox__content",{"ffe-screenreader-only":s})},a)))});r.__docgenInfo={description:"",methods:[],displayName:"Checkbox",props:{noMargins:{required:!1,tsType:{name:"boolean"},description:"Removes vertical margins from the checkbox"},hiddenLabel:{required:!1,tsType:{name:"boolean"},description:"If you plan to render the checkbox without a visible label"},inline:{required:!1,tsType:{name:"boolean"},description:"Display inline",defaultValue:{value:"true",computed:!1}},children:{required:!0,tsType:{name:"union",raw:`| React.ReactNode
+| ((labelProps: {
+      className: string;
+      htmlFor: string;
+  }) => React.ReactNode)`,elements:[{name:"ReactReactNode",raw:"React.ReactNode"},{name:"unknown"}]},description:""},onColoredBg:{required:!1,tsType:{name:"never"},description:`@deprecated as part of update to Semantic Colors
+
+Use the \`ffe-accent-color\` class on the component or on the container of the component instead
+[Read more in the upgrade guide](https://sparebank1.github.io/designsystem/?path=/docs/introduksjon-changelog--docs#2025---februar---semantiske-farger)`}},composes:["Omit"]};const O={title:"Komponenter/Form/Checkbox",component:r},n={args:{noMargins:!1,hiddenLabel:!1,inline:!0},render:a=>e.createElement("fieldset",{className:"ffe-input-group"},e.createElement("legend",{className:"ffe-form-label ffe-form-label--block"},"Hvilke aviser leser du?"),e.createElement(r,{...a,name:"newspapers",value:"vg"},"VG"),e.createElement(r,{...a,name:"newspapers",value:"dagbladet"},"Dagbladet"))},t={...n,args:{...n.args,inline:!1},render:a=>e.createElement("fieldset",{className:"ffe-input-group"},e.createElement("legend",{className:"ffe-form-label ffe-form-label--block"},"Hvilke aviser leser du?"),e.createElement(r,{...a,name:"newspapers",value:"vg"},"VG"),e.createElement(r,{...a,name:"newspapers",value:"dagbladet"},"Dagbladet"))},l={...n,args:{defaultChecked:!0,hiddenLabel:!0,inline:!1},render:a=>e.createElement(r,{...a},"Jeg har en ingen label")},o={...n,args:{"aria-invalid":!0},render:a=>e.createElement(r,{...a},"Ja, jeg vil gjerne motta reklame!")},c={...n,args:{checked:!0},render:a=>e.createElement(r,{...a},s=>e.createElement("label",{htmlFor:s.htmlFor,className:s.className},"Her benyttes render props"))},d={args:{...n.args,inline:!1},render:a=>e.createElement(M,{label:"Hva pleier du å lese?",description:"Velg en eller flere aviser"},()=>e.createElement(e.Fragment,null,e.createElement(r,{...a,name:"newspapers",value:"vg"},"VG"),e.createElement(r,{...a,name:"newspapers",value:"dagbladet"},"Dagbladet"),e.createElement(r,{...a,name:"newspapers",value:"nrk"},"NRK")))};var g,f,b;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    noMargins: false,
+    hiddenLabel: false,
+    inline: true
+  },
+  render: args => <fieldset className="ffe-input-group">
+            <legend className="ffe-form-label ffe-form-label--block">
+                Hvilke aviser leser du?
+            </legend>
+            <Checkbox {...args} name="newspapers" value="vg">
+                VG
+            </Checkbox>
+            <Checkbox {...args} name="newspapers" value="dagbladet">
+                Dagbladet
+            </Checkbox>
+        </fieldset>
+}`,...(b=(f=n.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var h,k,v;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  ...Standard,
+  args: {
+    ...Standard.args,
+    inline: false
+  },
+  render: args => <fieldset className="ffe-input-group">
+            <legend className="ffe-form-label ffe-form-label--block">
+                Hvilke aviser leser du?
+            </legend>
+            <Checkbox {...args} name="newspapers" value="vg">
+                VG
+            </Checkbox>
+            <Checkbox {...args} name="newspapers" value="dagbladet">
+                Dagbladet
+            </Checkbox>
+        </fieldset>
+}`,...(v=(k=t.parameters)==null?void 0:k.docs)==null?void 0:v.source}}};var x,C,N;l.parameters={...l.parameters,docs:{...(x=l.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  ...Standard,
+  args: {
+    defaultChecked: true,
+    hiddenLabel: true,
+    inline: false
+  },
+  render: args => <Checkbox {...args}>Jeg har en ingen label</Checkbox>
+}`,...(N=(C=l.parameters)==null?void 0:C.docs)==null?void 0:N.source}}};var E,w,S;o.parameters={...o.parameters,docs:{...(E=o.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  ...Standard,
+  args: {
+    'aria-invalid': true
+  },
+  render: args => <Checkbox {...args}>Ja, jeg vil gjerne motta reklame!</Checkbox>
+}`,...(S=(w=o.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};var R,y,I;c.parameters={...c.parameters,docs:{...(R=c.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  ...Standard,
+  args: {
+    checked: true
+  },
+  render: args => <Checkbox {...args}>
+            {labelProps => <label htmlFor={labelProps.htmlFor} className={labelProps.className}>
+                    Her benyttes render props
+                </label>}
+        </Checkbox>
+}`,...(I=(y=c.parameters)==null?void 0:y.docs)==null?void 0:I.source}}};var F,H,_;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    ...Standard.args,
+    inline: false
+  },
+  render: args => <InputGroup label="Hva pleier du å lese?" description="Velg en eller flere aviser">
+            {() => <>
+                    <Checkbox {...args} name="newspapers" value="vg">
+                        VG
+                    </Checkbox>
+                    <Checkbox {...args} name="newspapers" value="dagbladet">
+                        Dagbladet
+                    </Checkbox>
+                    <Checkbox {...args} name="newspapers" value="nrk">
+                        NRK
+                    </Checkbox>
+                </>}
+        </InputGroup>
+}`,...(_=(H=d.parameters)==null?void 0:H.docs)==null?void 0:_.source}}};const A=["Standard","InlineFalse","HiddenLabel","AriaInvalid","RenderProps","Description"],U=Object.freeze(Object.defineProperty({__proto__:null,AriaInvalid:o,Description:d,HiddenLabel:l,InlineFalse:t,RenderProps:c,Standard:n,__namedExportsOrder:A,default:O},Symbol.toStringTag,{value:"Module"}));export{o as A,U as C,l as H,t as I,c as R,n as S};
---
+++ assets/Input-C4eJCgYp.js
@@ -0,0 +1,17 @@
+import{j as e,M as a,C as r,a as t}from"./index-B9ybg0th.js";import{useMDXComponents as d}from"./index-DmqVK_gK.js";import{I as m,S as s,F as o}from"./Input.stories-CVoHJlk7.js";import"./iframe-clYGiD5J.js";import"./index-DQDNmYQF.js";import"./index-CV7naf_y.js";import"./index-BuA_lF3S.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./Input-XcOPKIal.js";import"./index-D2FocPV0.js";import"./InputGroup-BBDrD5qR.js";import"./Label-CbZQE9c6.js";import"./Tooltip-Dx-tWuFa.js";import"./Icon-D_nm5xPy.js";import"./Collapse-BVkyKCdf.js";import"./ErrorFieldMessage-DIT7W522.js";import"./BaseFieldMessage-WVjH22u3.js";function l(i){const n={code:"code",h1:"h1",h2:"h2",li:"li",ol:"ol",p:"p",...d(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:m}),`
+`,e.jsx(n.h1,{id:"input",children:"Input"}),`
+`,e.jsx(n.p,{children:"The bread and butter av skjemaene våre."}),`
+`,e.jsx(n.p,{children:"Vi har tre varianter av inputfelt:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"standard"}),`
+`,e.jsx(n.li,{children:"inline"}),`
+`,e.jsx(n.li,{children:"text-like"}),`
+`]}),`
+`,e.jsx(n.p,{children:"Skillet på Standard og inline er mest relevant for utviklere. De ser helt like ut, men oppfører seg annerledes i samspillet med andre elementer på siden, for eksempel tooltip."}),`
+`,e.jsx(n.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(r,{of:s}),`
+`,e.jsx(t,{of:s}),`
+`,e.jsx(n.h2,{id:"med-feilmelding",children:"Med feilmelding"}),`
+`,e.jsxs(n.p,{children:["Sender man inn en string eller et ",e.jsx(n.code,{children:"<ErrorFieldMessage>"}),"-element til ",e.jsx(n.code,{children:"fieldMessage"})," på ",e.jsx(n.code,{children:"<InputGroup>"})," vil input-feltet rendres med ",e.jsx(n.code,{children:"aria-invalid"})," og en feilmelding."]}),`
+`,e.jsx(r,{of:o}),`
+`,e.jsx(t,{of:o})]})}function E(i={}){const{wrapper:n}={...d(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(l,{...i})}):l(i)}export{E as default};
