981c572a99e72dc41ec9cf36c03c5b97fd49afa9
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2025-12-19 13:39:56 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@b02555e9fb9c51ad6a7813ecb6335676c07db486 🚀

---
+++ assets/Skeleton.stories-BXf_m2ZA.js
@@ -0,0 +1,4 @@
+import{R as s}from"./index-DQDNmYQF.js";import{S as o}from"./Skeleton-Len5f0hZ.js";import"./index-D2FocPV0.js";const p={title:"Komponenter/Skeleton/Skeleton",component:o,args:{width:"100%",height:"1em"}},e={args:{},render:n=>s.createElement(o,{...n})};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: args => <Skeleton {...args}></Skeleton>
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const l=["Standard"];export{e as Standard,l as __namedExportsOrder,p as default};
---
+++ assets/SkeletonCircle.stories-CWQNsJY-.js
@@ -0,0 +1,6 @@
+import{R as s}from"./index-DQDNmYQF.js";import{S as a}from"./SkeletonCircle-BAvV3Ff2.js";import"./index-D2FocPV0.js";const d={title:"Komponenter/Skeleton/SkeletonCircle",component:a},e={args:{size:"lg"},render:n=>s.createElement(a,{...n})};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    size: 'lg'
+  },
+  render: args => <SkeletonCircle {...args}></SkeletonCircle>
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const i=["Standard"];export{e as Standard,i as __namedExportsOrder,d as default};
