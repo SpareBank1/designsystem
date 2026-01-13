c43ac58fd953091bd228b8766cd3ea73879db5d0
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2026-01-13 15:15:45 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@086657131ab717d994af43c303db6baef122b15c 🚀

---
+++ assets/Tailwind-VviYvNxq.js
@@ -0,0 +1,22 @@
+import{j as e,M as o}from"./index-Cyle30Y4.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import"./iframe--OCoIXnN.js";import"./index-DQDNmYQF.js";import"./index-1rxX2T2W.js";import"./index-CFFhkS_s.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function s(r){const n={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Design/Farger/Tailwind"}),`
+`,e.jsx(n.h1,{id:"tailwind",children:"Tailwind"}),`
+`,e.jsx(n.p,{children:`Vi anbefaler ikke å bruke Tailwind, men vet det er mange som gjør det.
+Vi tilbyr derfor farge tokens for Tailwind også.`}),`
+`,e.jsx(n.h2,{id:"versjon-4",children:"Versjon 4"}),`
+`,e.jsxs(n.p,{children:["Tailwind versjon 4 bruker css layers per default. På grunn av hvilke ",e.jsx(n.a,{href:"https://design.sparebank1.no/grunnleggende/nettleserversjoner/",rel:"nofollow",children:"nettleserversjoner"})," vi må støtte i dag (sept 2025) støtter vi ikke CSS layers. Vi anbefaler å bruke Tailwind 4 med en måte som importerer fargene uten CSS layers, som du kan lese mer om her: ",e.jsx(n.a,{href:"https://github.com/tailwindlabs/tailwindcss/discussions/13188",rel:"nofollow",children:"tailwindlabs/tailwindcss#13188"}),`
+Importer følgende css fil for å få semantiske farger i Tailwind versjon 4:`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-css",children:`@import '@sb1/ffe-core/css/colors-semantic-tailwind-4.css';
+`})}),`
+`,e.jsx(n.h2,{id:"versjon-3",children:"Versjon 3"}),`
+`,e.jsxs(n.p,{children:["For å bruke semantiske farger med Tailwind Versjon 3 kan du importere fargene fra ",e.jsx(n.code,{children:"@sb1/ffe-core/lib/semanticColorsTailwind"}),"."]}),`
+`,e.jsxs(n.p,{children:["Her et eksempel på hvordan et utsnitt av din ",e.jsx(n.code,{children:"tailwind.config.js"})," kan se ut for å sette opp til bruk av de semantske fagene"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import * as colors from '@sb1/ffe-core/lib/semanticColorsTailwind';
+module.exports = {
+    theme: {
+        colors: {
+            ...colors,
+        },
+    },
+};
+`})}),`
+`,e.jsxs(n.p,{children:["Da blir fargene tilgjengelige i kebab-case uten ",e.jsx(n.code,{children:"--ffe-color"}),". Feks ",e.jsx(n.code,{children:"--ffe-color-background-default"})," -> ",e.jsx(n.code,{children:"background-default"})]})]})}function j(r={}){const{wrapper:n}={...i(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(s,{...r})}):s(r)}export{j as default};
---
+++ assets/TaskButton-zI4ASwQ2.js
@@ -0,0 +1,6 @@
+import{j as n,M as i,C as a,a as m}from"./index-Cyle30Y4.js";import{useMDXComponents as e}from"./index-DmqVK_gK.js";import{T as p,S as r}from"./TaskButton.stories-D1BmNhkL.js";import"./iframe--OCoIXnN.js";import"./index-DQDNmYQF.js";import"./index-1rxX2T2W.js";import"./index-CFFhkS_s.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./BaseButton-C7ZIg8iM.js";import"./fixedForwardRef-DqyCgkTx.js";import"./index-D2FocPV0.js";import"./Icon-D_nm5xPy.js";function s(t){const o={h1:"h1",h2:"h2",p:"p",...e(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:p}),`
+`,n.jsx(o.h1,{id:"taskbutton",children:"TaskButton"}),`
+`,n.jsx(o.p,{children:"Kan brukes som et alternativ til en kommandoknapp i scenarioer der man ønsker å legge til noe."}),`
+`,n.jsx(o.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,n.jsx(a,{of:r}),`
+`,n.jsx(m,{of:r})]})}function D(t={}){const{wrapper:o}={...e(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(s,{...t})}):s(t)}export{D as default};
