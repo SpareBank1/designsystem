445cca9b6d627a73d7c0539cfb7ab340f60c851f
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2026-02-11 13:41:43 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@1fbe3fa8ca2ceff786e5ca318ff12177f4c410f9 🚀

---
+++ assets/Datepicker-e_JKpo0l.js
@@ -0,0 +1,47 @@
+import{j as e,M as a,C as r,a as d}from"./index-CmNcSkZs.js";import{useMDXComponents as o}from"./index-DmqVK_gK.js";import{D as c,S as i,W as p,a as l}from"./Datepicker.stories-ByU4Ks7K.js";import{I as m}from"./InstallImport-Blfwm1ZT.js";import"./iframe-BR2lhmrU.js";import"./index-DQDNmYQF.js";import"./index-1rxX2T2W.js";import"./index-CFFhkS_s.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./Calendar-GdpSWRup.js";import"./index-D2FocPV0.js";import"./Icon-D_nm5xPy.js";import"./Dropdown-DU2fHO9Y.js";import"./v4-CtRu48qb.js";import"./index-D9tCDUwu.js";import"./InputGroup-CsZXON4u.js";import"./Collapse-BVkyKCdf.js";import"./SecondaryButton-Boh02aDD.js";import"./BaseButton-By4eKdy_.js";import"./fixedForwardRef-Dm1tRvsG.js";import"./Input-D-am7U86.js";import"./Heading-YepvjHrD.js";import"./Paragraph-7jO2Jnpx.js";const f=["@sb1/ffe-datepicker","@sb1/ffe-dropdown-react","@sb1/ffe-form","@sb1/ffe-form-react","@sb1/ffe-icons-react","@sb1/ffe-core","@sb1/ffe-buttons","@sb1/ffe-collapse-react","@sb1/ffe-icons"];function s(n){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...o(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:c}),`
+`,e.jsx(t.h1,{id:"datepicker",children:"Datepicker"}),`
+`,e.jsx(m,{packageName:"@sb1/ffe-datepicker-react",dependencies:f}),`
+`,e.jsx(t.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(r,{of:i}),`
+`,e.jsx(t.h2,{id:"med-nedtrekksmeny-for-måned-og-år",children:"Med nedtrekksmeny for måned og år"}),`
+`,e.jsxs(t.p,{children:["Du kan aktivere nedtrekksmeny for måned og år ved å sette ",e.jsx(t.code,{children:"dropdownCaption={true}"}),". Dette lar brukeren hoppe raskt til en spesifikk måned eller år i kalenderen, i stedet for å bla gjennom månedene én for én."]}),`
+`,e.jsx(r,{of:p}),`
+`,e.jsx(d,{of:i}),`
+`,e.jsx(t.h2,{id:"med-beskrivelse",children:"Med beskrivelse"}),`
+`,e.jsxs(t.p,{children:["Du kan bruke ",e.jsx(t.code,{children:"description='tekst her'"})," på ",e.jsx(t.code,{children:"InputGroup"})," for å legge til en beskrivelse mellom label og Datepicker."]}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(t.h2,{id:"testing-med-react-testing-library",children:"Testing med React Testing Library"}),`
+`,e.jsxs(t.p,{children:["Testing av ",e.jsx(t.code,{children:"Datepicker"}),` krever litt ekstra kode, så vi har gjort det enklere ved å tilby en funksjon som
+skal gjøre det enklere å teste komponenten med React Testing Library.`]}),`
+`,e.jsxs(t.p,{children:["Funksjonen er avhengig av ",e.jsx(t.code,{children:"@testing-library/react"}),", så dette biblioteket må installeres."]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-sh",children:`npm i --save-dev @testing-library/react
+`})}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`async function getDatepickerByLabelText(
+    label: string,
+    index = 0,
+): {
+    /** The datepicker element */
+    element: Element;
+    /** Function to get the value of the datepicker
+     * @returns string in the format 'dd.mm.yyyy' or <empty string> if the datepicker is empty */
+    getValue: () => string ;
+    /**Function to set the value of the datepicker
+     * @param value string in the format 'dd.mm.yyyy'
+     * @returns void
+     */
+    setValue: (value: string) => Promise<void>;
+};
+`})}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`describe('Datepicker test', () => {
+    it('datepicker provides methods to simplify testing', async () => {
+        // Code for rendering your page / component
+
+        const datepicker = await getDatepickerByLabelText('Dato velger');
+
+        expect(datepicker.getValue()).toStrictEqual('01.02.2024');
+
+        await datepicker.setValue('6.5.2024');
+        expect(datepicker.getValue()).toStrictEqual('06.05.2024');
+    });
+});
+`})})]})}function q(n={}){const{wrapper:t}={...o(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(s,{...n})}):s(n)}export{q as default};
---
+++ assets/RadioBlock-DE5DekTO.js
@@ -0,0 +1,44 @@
+import{j as e,M as a,S as s,C as l,a as d}from"./index-CmNcSkZs.js";import{useMDXComponents as i}from"./index-DmqVK_gK.js";import{R as p,S as n}from"./RadioBlock.stories-BfLKN62R.js";import"./iframe-BR2lhmrU.js";import"./index-DQDNmYQF.js";import"./index-1rxX2T2W.js";import"./index-CFFhkS_s.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./RadioBlock-MtxcmPo-.js";import"./index-D2FocPV0.js";import"./RadioButtonInputGroup-BnP6J3oD.js";import"./Tooltip-Dx-tWuFa.js";import"./Icon-D_nm5xPy.js";import"./Collapse-BVkyKCdf.js";import"./ErrorFieldMessage-DIT7W522.js";import"./BaseFieldMessage-WVjH22u3.js";function t(r){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:p}),`
+`,e.jsx(o.h1,{id:"radioblock",children:"RadioBlock"}),`
+`,e.jsx(o.p,{children:"Radioblokker brukes når vi ber brukeren om å ta valg som krever en del."}),`
+`,e.jsx(o.h2,{id:"kode",children:"Kode"}),`
+`,e.jsx(s,{code:`
+<RadioButtonInputGroup
+    label="Hvem eier bilen du skal forsikre?"
+    name="owner"
+    onChange={e => setSelected(e.target.value)}
+    selectedValue={selected}
+>
+    {inputProps => (
+        <>
+            <RadioBlock
+                {...inputProps}
+                label="Ektefelle, samboer eller registrert partner"
+                value="partner"
+            >
+                Da må ektefelle, samboer eller registrert partner
+                skrive inn detaljene sine under.
+            </RadioBlock>
+            <RadioBlock
+                {...inputProps}
+                label="Du"
+                value="you"
+            >
+                Da må du skrive inn detaljene dine under.
+            </RadioBlock>
+            <RadioBlock
+                {...inputProps}
+                label="Leasingselskap"
+                value="leasing-company"
+            >
+                Da må leasingselskapet gi deg noen detaljer som du
+                må skrive inn under.
+            </RadioBlock>
+        </>
+    )}
+</RadioButtonInputGroup>
+`}),`
+`,e.jsx(o.h2,{id:"forhåndsvisning",children:"Forhåndsvisning"}),`
+`,e.jsx(l,{of:n}),`
+`,e.jsx(d,{of:n}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{})})]})}function P(r={}){const{wrapper:o}={...i(),...r.components};return o?e.jsx(o,{...r,children:e.jsx(t,{...r})}):t(r)}export{P as default};
