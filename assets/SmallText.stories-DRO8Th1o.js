3a369d18b6f03dc44756799ae879c16aba8efd46
sb1-designsystem <37370366+sb1-designsystem@users.noreply.github.com>
2026-03-31 11:28:48 +0000

    Deploying to gh-pages from @ SpareBank1/designsystem@caff50ed07c09455564fd03b7d93fc25821fcffb 🚀

---
+++ assets/SmallText.stories-DRO8Th1o.js
@@ -0,0 +1,7 @@
+import{R as o}from"./index-DQDNmYQF.js";import{c as i}from"./index-D2FocPV0.js";function a(r){const{as:l="span",className:m,underline:d=!1,...c}=r;return o.createElement(l,{className:i("ffe-small-text",{"ffe-small-text--underline":d},m),...c})}a.__docgenInfo={description:"",methods:[],displayName:"SmallText",props:{as:{required:!1,tsType:{name:"As"},description:""},underline:{required:!1,tsType:{name:"boolean"},description:""}}};const p={title:"Komponenter/Core/SmallText",component:a},e={args:{underline:!1,children:"Some text"},render:r=>o.createElement(a,{...r})};var t,s,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    underline: false,
+    children: 'Some text'
+  },
+  render: args => <SmallText {...args} />
+}`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["Standard"],x=Object.freeze(Object.defineProperty({__proto__:null,Standard:e,__namedExportsOrder:f,default:p},Symbol.toStringTag,{value:"Module"}));export{x as S,e as a};
